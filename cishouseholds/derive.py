08144ec5fe7349cad062ff639cc7e80ad52ebc06
Rowan Hemsi <rowan.hemsi@ons.gov.uk>
2021-07-06 16:21:34 +0100

    Refactor derive_ctpattern

--- cishouseholds/derive.py
+++ cishouseholds/derive.py
@@ -46,25 +46,46 @@ def substring_column(df: DataFrame, new_column_name, column_to_substr, start_pos
 
 
 def derive_ctpattern(df: DataFrame, spark_session):
-    #    lookup_df = spark_session.createDataFrame(
-    #        data=[
-    #            ("0", "0", "0", "NULL"),
-    #            ("1", "0", "0", "OR only"),
-    #            ("0", "1", "0", "N only"),
-    #            ("0", "0", "1", "S only"),
-    #            ("1", "1", "0", "OR+N"),
-    #            ("1", "0", "1", "OR+S"),
-    #            ("0", "1", "1", "N+S"),
-    #            ("1", "1", "1", "OR+N+S"),
-    #        ],
-    #        schema=["indicator_ct_or", "indicator_ct_n", "indicator_ct_s", "ct_pattern"],
-    #    )
+    """
+    Derive a new column containing string of pattern in
+    ["N only", "OR only", "S only", "OR+N", "OR+S", "N+S", "OR+N+S", NULL]
+    indicating which ct_* columns indicate a positive result.
+    From households_aggregate_processes.xlsx, derivation number 7.
 
-    df = (
-        df.withColumn("indicator_ct_or", F.when(F.col("cf_or") > 0, "1").otherwise("0"))
-        .withColumn("indicator_ct_n", F.when(F.col("cf_n") > 0, "1").otherwise("0"))
-        .withColumn("indicator_ct_s", F.when(F.col("cf_s") > 0, "1").otherwise("0"))
+    Parameters
+    ----------
+    spark_session: pyspark.sql.SparkSession
+    df: pyspark.sql.DataFrame
+
+    Return
+    ------
+    df: pyspark.sql.DataFrame
+    """
+    lookup_df = spark_session.createDataFrame(
+        data=[
+            ("0", "0", "0", "NULL"),
+            ("1", "0", "0", "OR only"),
+            ("0", "1", "0", "N only"),
+            ("0", "0", "1", "S only"),
+            ("1", "1", "0", "OR+N"),
+            ("1", "0", "1", "OR+S"),
+            ("0", "1", "1", "N+S"),
+            ("1", "1", "1", "OR+N+S"),
+        ],
+        schema=["indicator_ct_or", "indicator_ct_n", "indicator_ct_s", "ctpattern"],
     )
+
+    on_condition = []
+    for ct_gene in ["or", "n", "s"]:
+        df = df.withColumn(f"indicator_ct_{ct_gene}", F.when(F.col(f"cf_{ct_gene}") > 0, "1").otherwise("0"))
+
+        on_condition.append(f"indicator_ct_{ct_gene}")
+
+    df = df.join(F.broadcast(lookup_df), on=on_condition, how="left").drop(
+        "indicator_ct_or", "indicator_ct_n", "indicator_ct_s"
+    )
+
+    return df
 
 
 def derive_ctpattern2(df: DataFrame):
