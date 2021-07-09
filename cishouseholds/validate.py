bd3aef00538c2dfd272eefcbf916e5d75db097af
Osborne <Jack.Osborne@ons.gov.uk>
2021-07-09 13:40:52 +0100

    Add validation for csv and test

---
+++ cishouseholds/validate.py
@@ -0,0 +1,59 @@
+import csv
+
+class InvalidFileError(Exception):
+    pass
+
+
+def validate_csv_fields(csv_file:str, delimiter:str=","):
+    """
+    Function to validate the number of fields within records of a csv file.
+
+    Parameters
+    ----------
+    csv_file
+        File path for csv file to be validated    
+    delimiter
+        Delimiter used in csv file, default as ','
+    """
+    row_errors = []
+    with open(csv_file) as f:
+        reader = csv.reader(f,delimiter=delimiter)
+        n_fields = len(next(reader))
+        
+        for line_num, row in enumerate(reader):
+            row_fields = len(row)
+            if row_fields!=n_fields:
+                row_errors.append(f"{line_num+1}")
+        
+    if row_errors:
+        raise InvalidFileError(f"Expected number of fields in each row is {n_fields}",
+                               f"Rows not matching this are: {', '.join(row_errors)}")
+    return True
+
+def validate_csv_header(csv_file:str, expected_header:str, delimiter:str=","):
+    """
+    Function to validate header in csv file matches expected header.
+
+    Parameters
+    ----------
+    csv_file
+        File path for csv file to be validated
+    expected_header
+        Exact header expected in csv file
+    delimiter
+        Delimiter used in csv file, default as ','
+    """
+
+    with open(csv_file) as f:
+        header = f.readline()
+
+    if expected_header is not None:
+            is_match = (expected_header==header)
+            if is_match is False:
+                raise InvalidFileError(f"Header of csv file {csv_file} does not match expected header",
+                                       f"Actual header: {header}",
+                                       f"Expected header: {expected_header}")
+    return True
+
+if __name__=="__main__":
+    pass
---
+++ tests/validate/test_validate_csv.py
@@ -0,0 +1,35 @@
+import pytest
+import validate_csv
+from validate_csv import InvalidFileError
+
+@pytest.fixture
+def gen_tmp_file(tmp_path):
+    def _(file_name,file_content):
+        test_file_path = tmp_path / file_name
+        test_file_path.touch()
+        test_file_path.write_text(file_content)
+        return test_file_path
+    return _
+
+def test_validate_csv_header(gen_tmp_file):
+    test_text = '"field_1"|"field,2"|"field_3"'
+    test_file_path = gen_tmp_file("test_file.csv", test_text)
+    assert validate_csv.validate_csv_header(test_file_path.as_posix(),expected_header=test_text,delimiter="|")
+
+def test_validate_csv_header_error(gen_tmp_file):
+    test_text = '"field_1"|"field,2"|"field_3"'
+    test_text_error = '"field_1"|"field_2"|"field_3"'
+    test_file_path = gen_tmp_file("test_file.csv", test_text)
+    with pytest.raises(InvalidFileError):
+        validate_csv.validate_csv_header(test_file_path.as_posix(),expected_header=test_text_error,delimiter="|")
+
+def test_validate_csv_fields(gen_tmp_file):
+    test_text = '"field_1"|"field,2"|"field_3"\n"entry_1"|"entry_2"|"entry_3"\n"entry_4"|"entry_5"|"entry_6"'
+    test_file_path = gen_tmp_file("test_file.csv", test_text)
+    assert validate_csv.validate_csv_fields(test_file_path.as_posix(),delimiter="|")
+
+def test_validate_csv_fields_error(gen_tmp_file):
+    test_text_error = '"field_1"|"field,2"|"field_3"\n"entry_1"|"entry_2"|"entry_3"|"entry_3b"\n"entry_4"|"entry_5"|"entry_6"\n"entry_7"|"entry_8"'
+    test_file_path = gen_tmp_file("test_file.csv", test_text_error)
+    with pytest.raises(InvalidFileError, match="1, 3"):
+        validate_csv.validate_csv_fields(test_file_path.as_posix(),delimiter="|")
