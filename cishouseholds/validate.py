f1b251af21490a42b8d66f62f6704533b900659f
Osborne <Jack.Osborne@ons.gov.uk>
2021-07-12 15:31:49 +0100

    Pre-commit hooks changed

--- cishouseholds/validate.py
+++ cishouseholds/validate.py
@@ -1,36 +1,40 @@
 import csv
 
+
 class InvalidFileError(Exception):
     pass
 
 
-def validate_csv_fields(csv_file:str, delimiter:str=","):
+def validate_csv_fields(csv_file: str, delimiter: str = ","):
     """
     Function to validate the number of fields within records of a csv file.
 
     Parameters
     ----------
     csv_file
-        File path for csv file to be validated    
+        File path for csv file to be validated
     delimiter
         Delimiter used in csv file, default as ','
     """
     row_errors = []
     with open(csv_file) as f:
-        reader = csv.reader(f,delimiter=delimiter)
+        reader = csv.reader(f, delimiter=delimiter)
         n_fields = len(next(reader))
-        
+
         for line_num, row in enumerate(reader):
             row_fields = len(row)
-            if row_fields!=n_fields:
+            if row_fields != n_fields:
                 row_errors.append(f"{line_num+1}")
-        
+
     if row_errors:
-        raise InvalidFileError(f"Expected number of fields in each row is {n_fields}",
-                               f"Rows not matching this are: {', '.join(row_errors)}")
+        raise InvalidFileError(
+            f"Expected number of fields in each row is {n_fields}",
+            f"Rows not matching this are: {', '.join(row_errors)}",
+        )
     return True
 
-def validate_csv_header(csv_file:str, expected_header:str, delimiter:str=","):
+
+def validate_csv_header(csv_file: str, expected_header: str, delimiter: str = ","):
     """
     Function to validate header in csv file matches expected header.
 
@@ -48,12 +52,15 @@ def validate_csv_header(csv_file:str, expected_header:str, delimiter:str=","):
         header = f.readline()
 
     if expected_header is not None:
-            is_match = (expected_header==header)
-            if is_match is False:
-                raise InvalidFileError(f"Header of csv file {csv_file} does not match expected header",
-                                       f"Actual header: {header}",
-                                       f"Expected header: {expected_header}")
+        is_match = expected_header == header
+        if is_match is False:
+            raise InvalidFileError(
+                f"Header of csv file {csv_file} does not match expected header",
+                f"Actual header: {header}",
+                f"Expected header: {expected_header}",
+            )
     return True
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     pass
