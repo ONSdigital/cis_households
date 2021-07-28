f6d2a671deb88b43b72717c042f1ce60dbd50667
Jack Osborne <jack.osborne@ons.gov.uk>
2021-07-28 13:18:39 +0100

    Added function and test to edit sample info prior to design weight calculation

--- cishouseholds/pipeline/sample_delta_ETL.py
+++ cishouseholds/pipeline/sample_delta_ETL.py
@@ -1,3 +1,7 @@
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+
+
 def sample_delta_ETL():
     extract_from_csv()
     validate_sample()
@@ -14,6 +18,68 @@ def extract_from_csv():
 
 def validate_sample():
     pass
+
+
+def edit_sample_file(df: DataFrame, sample_name: str, sample_direct: int) -> DataFrame:
+    """
+    Initial editing of input sample file to prepare for design weight calculation.
+
+    Parameters
+    ----------
+    df
+    sample_name
+        Identifier to apply to all records in (delta) sample file
+    sample_direct
+        (Probably) an identifier to apply to all records indicating if direct
+    """
+
+    df = (
+        df.withColumn("sample", F.lit(sample_name))
+        .withColumn("sample_direct", F.lit(sample_direct))
+        .withColumn(
+            "gor9d",
+            F.when(F.col("custodian_region_code") == "S92000003", "S99999999")
+            .when(F.col("custodian_region_code") == "W92000004", "W99999999")
+            .otherwise(F.col("custodian_region_code")),
+        )
+        .withColumn(
+            "country_sample",
+            F.when(F.col("gor9d") == "W99999999", "Wales")
+            .when(F.col("gor9d") == "S99999999", "Scotland")
+            .when(F.col("gor9d") == "N99999999", "NI")
+            .otherwise(F.lit("England")),
+        )
+        .withColumn("rgn", F.when(F.col("rgn").isNull(), F.col("gor9d")).otherwise(F.col("rgn")))
+        .drop("custodian_region_code")
+    )
+
+    northwest_boost_list = [
+        "E07000117",
+        "E07000120",
+        "E07000122",
+        "E07000123",
+        "E07000125",
+        "E08000001",
+        "E08000002",
+        "E08000003",
+        "E08000004",
+        "E08000005",
+        "E08000006",
+        "E08000007",
+        "E08000008",
+        "E08000009",
+        "E08000010",
+    ]
+    yorkshire_boost_list = ["E08000032", "E08000033", "E08000034"]
+    df = df.withColumn(
+        "gor9d_recoded",
+        F.when((F.col("gor9d") == "E12000002") & (F.col("laua").isin(*northwest_boost_list)), "E12000002_boost")
+        .when((F.col("gor9d") == "E12000002") & (~(F.col("laua").isin(*northwest_boost_list))), "E12000002_nonboost")
+        .when((F.col("gor9d") == "E12000003") & (F.col("laua").isin(*yorkshire_boost_list)), "E12000003_boost")
+        .when((F.col("gor9d") == "E12000003") & (~(F.col("laua").isin(*yorkshire_boost_list))), "E12000003_boost")
+        .otherwise(F.col("gor9d")),
+    )
+    return df
 
 
 def calculate_design_weights():
---
+++ tests/pipeline/test_calculate_design_weights.py
@@ -0,0 +1,35 @@
+from chispa import assert_df_equality
+
+from cishouseholds.pipeline.sample_delta_ETL import edit_sample_file
+
+
+def test_edit_sample_info(spark_session):
+
+    input_schema = "custodian_region_code string, laua string, rgn string"
+    input_df = spark_session.createDataFrame(
+        data=[
+            ("S92000003", "S00001111", None),
+            ("W92000004", "W00001111", "W92000004"),
+            ("N99999999", "N00001111", "N92000001"),
+            ("E12000002", "E07000120", None),
+        ],
+        schema=input_schema,
+    )
+
+    expected_schema = (
+        "laua string, rgn string, sample string, sample_direct integer,"
+        "gor9d string, country_sample string, gor9d_recoded string"
+    )
+    expected_df = spark_session.createDataFrame(
+        data=[
+            ("S00001111", "S99999999", "samp1", 1, "S99999999", "Scotland", "S99999999"),
+            ("W00001111", "W92000004", "samp1", 1, "W99999999", "Wales", "W99999999"),
+            ("N00001111", "N92000001", "samp1", 1, "N99999999", "NI", "N99999999"),
+            ("E07000120", "E12000002", "samp1", 1, "E12000002", "England", "E12000002_boost"),
+        ],
+        schema=expected_schema,
+    )
+
+    output_df = edit_sample_file(input_df, "samp1", 1)
+
+    assert_df_equality(output_df, expected_df, ignore_nullable=True)
