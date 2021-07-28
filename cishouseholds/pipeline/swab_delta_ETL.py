064d76ebccb8b3603825127d70a389a6b3d671ed
Rowan Hemsi <rowan.hemsi@ons.gov.uk>
2021-07-28 11:12:52 +0100

    Add D13 and D7 to transform_swabs_delta

--- cishouseholds/pipeline/swab_delta_ETL.py
+++ cishouseholds/pipeline/swab_delta_ETL.py
@@ -1,3 +1,10 @@
+from ..derive import assign_column_convert_to_date
+from ..derive import derive_ctpattern
+
+# from ..derive import assign_from_lookup
+# from ..derive import mean_across_columns
+
+
 def swabs_delta_ETL():
     extract_swabs_delta()
     transform_swabs_delta()
@@ -8,8 +15,24 @@ def extract_swabs_delta():
     pass
 
 
-def transform_swabs_delta():
-    pass
+def transform_swabs_delta(df, spark_session):
+    """
+    13: assign_column_convert_to_date
+        derived variable name: result_mk_date_time
+        V1-Vn: result_mk_date_time
+    7: derive_ctpattern
+        derived variable name: ctpattern
+        V1-Vn: ctORF1ab_result,ctNgene_result,ctSgene_result,ctMS2_result
+    9: mean_across_columns
+        dvn: ct_mean
+        V1-Vn: ctpattern,ctORF1ab,ctNgene,ctSgene
+    10: assign_from_lookup
+        dvn: ctonetarget
+        V1-Vn: ctpattern
+    """
+
+    df = assign_column_convert_to_date(df, "result_mk_date_time", "result_mk_date_time")
+    df = derive_ctpattern(df, ["ctORF1ab_result", "ctNgene_result", "ctSgene_result"], spark_session)
 
 
 def load_swabs_delta():
