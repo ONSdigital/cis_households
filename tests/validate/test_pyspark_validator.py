4419f9bb07b6069f979ee878811541f6d0c635a9
David Foster <foster.dev999@gmail.com>
2021-07-28 09:08:32 +0100

    Actually fixed the timestamp test this time

--- tests/validate/test_pyspark_validator.py
+++ tests/validate/test_pyspark_validator.py
@@ -7,9 +7,9 @@ def test_validator_with_timestamp(spark_session):
     schema = {"ts": {"type": "timestamp"}}
     validator = PySparkValidator(schema)
 
-    df = spark_session.createDataFrame(["1966-07-30 15:00:00"], schema="ts string")
-    df = df.withColumn("ts", F.from_unixtime(F.unix_timestamp("ts")))
+    df = spark_session.createDataFrame(["1966-07-30 15:00:00"], "string").toDF("ts")
+    df = df.withColumn("ts", F.from_unixtime(F.unix_timestamp("ts")).cast("timestamp"))
 
-    result = validator(df.rdd.collect()[0])
+    result = validator(df.rdd.collect()[0].asDict())
     assert result
     assert len(validator.errors) == 0
