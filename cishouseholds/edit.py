8d7dbb8fa2a9c86f8eb8b6547ef2415fb6f73bc1
melloj <jonathon.mellor@ons.gov.uk>
2021-08-02 17:17:05 +0100

    null if insufficient editing function creation and test

--- cishouseholds/edit.py
+++ cishouseholds/edit.py
@@ -5,7 +5,24 @@ from pyspark.sql import functions as F
 def assign_null_if_insufficient(
     df: DataFrame, column_name_to_assign: str, first_reference_column: str, second_reference_column: str
 ):
+    """
+    Assign a null values based on bloods insufficient logic, where two columns both have specified values.
+    From households_aggregate_processes.xlsx, edit number 2.
 
+    Parameters
+    ----------
+    df
+    column_name_to_assign
+        Name of column to be assigned
+    first_reference_column
+        First column to check value of for null condition
+    second_reference_column
+        Second column to check value of for null condition
+
+    Return
+    ------
+    pyspark.sql.DataFrame
+    """
     return df.withColumn(
         column_name_to_assign,
         F.when(
