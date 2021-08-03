657c911e4405d84b6e14223e1bb8293ce276345e
Ohi89 <bokth89@gmail.com>
2021-08-03 14:57:06 +0100

    E5_swab_results_test
    add a test following the examples available in households where pytest.fixture is available (test_assign_age_at_date.py).
    
    The test consist of an expected_df function that creates a dataframe with the correct values and input_generator function that creates a dataframe to be fed into the edit_swab_results_single function.
    
    Then these two are compared with chispa function assert_df_equality

--- tests/edit/test_edit.py
+++ tests/edit/test_edit.py
@@ -1,13 +1,30 @@
-# Create an example
 import pytest
+from chispa import assert_df_equality
+from cishouseholds.edit import edit_swab_results_single
 
+
+# Create an example
 @pytest.fixture
 def expected_df(spark_session):
     return spark_session.createDataFrame(
-        data = [
-            (1, 0, 1) # this value should convert to (0, 0, 1)
-        ],
+        data = [(0, 0, 1),
+                (0, 0, 1),
+                (0, 1, 1)],
         schema = ["ctNgene_result","ctNgene","result_mk"]
     )
 
-# Assert
+
+# example df:
+def input_generator(spark_session):
+    return spark_session.createDataFrame(
+            data = [(0, 0, 1),
+                    (1, 0, 1),
+                    (0, 1, 1)],
+            schema = ["ctNgene_result","ctNgene","result_mk"])
+
+
+# Test
+def test_edit_swab_results(spark_session, expected_df):
+    df_input = input_generator(spark_session)
+    actual_df = edit_swab_results_single(df_input, "ctNgene")
+    assert_df_equality(actual_df, expected_df)
