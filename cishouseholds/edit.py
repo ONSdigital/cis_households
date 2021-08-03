43e01a7e5b98b3f311448ae70a6f778c8cc107bd
Ohi89 <bokth89@gmail.com>
2021-08-03 12:08:31 +0100

    E5: Edit Swab Results
    Add a function that edits the Pyspark dataframe results in accordance to the required logic. The result is given in integers as it will be better to pass from 0, 1 to Negative, Positive by another function.

---
+++ cishouseholds/edit.py
@@ -0,0 +1,17 @@
+
+from pyspark.sql.functions import when, col
+
+def edit_swab_results_single(df, V2:str):
+    """
+    Parameters
+    ----------
+    df : Pyspark Dataframe
+    V2 : String with categorical options for "ctOR1ab","ctNgene","ctSgene".
+
+    Returns
+    -------
+    df : Edited Pyspark Dataframe with corrected values in case of wrong result
+        with the following logic: 
+    """
+    return df.withColumn(V2 + '_result', when((col(V2 + '_result') == 1) & (col(V2) <= 0) & (col('result_mk') == 1), 0)\
+                                        .otherwise(col(V2 + '_result')))
