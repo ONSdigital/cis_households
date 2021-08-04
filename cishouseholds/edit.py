65f697065ddd455964fb90744c37b24d2922b3b3
Jonathon Mellor <46898047+jonathonmellor@users.noreply.github.com>
2021-08-04 14:48:12 +0100

    748 blood insufficient sample function created (#29)
    * null if insufficient editing function creation and test
    
    * null if insufficient editing function creation and test
    
    * conflict in edit files completed
    
    Co-authored-by: melloj <jonathon.mellor@ons.gov.uk>

--- cishouseholds/edit.py
+++ cishouseholds/edit.py
@@ -9,7 +9,6 @@ def assign_from_map(df: DataFrame, column_name_to_assign: str, reference_column:
     """
     Assign column with values based on a dictionary map of reference_column.
     From households_aggregate_processes.xlsx, edit number 1.
-
     Parameters
     ----------
     df
@@ -19,11 +18,9 @@ def assign_from_map(df: DataFrame, column_name_to_assign: str, reference_column:
         Name of column of TimeStamp type to be converted
     mapper
         Dictionary of key value pairs to edit values
-
     Return
     ------
     pyspark.sql.DataFrame
-
     Notes
     -----
     Function works if key and value are of the same type and there is a missing key in the mapper
@@ -45,3 +42,30 @@ def assign_from_map(df: DataFrame, column_name_to_assign: str, reference_column:
         )
     else:
         return df.withColumn(column_name_to_assign, mapping_expr[F.col(reference_column)])
+
+
+def assign_null_if_insufficient(
+    df: DataFrame, column_name_to_assign: str, first_reference_column: str, second_reference_column: str
+):
+    """
+    Assign a null values based on bloods insufficient logic, where two columns both have specified values.
+    From households_aggregate_processes.xlsx, edit number 2.
+    Parameters
+    ----------
+    df
+    column_name_to_assign
+        Name of column to be assigned
+    first_reference_column
+        First column to check value of for null condition
+    second_reference_column
+        Second column to check value of for null condition
+    Return
+    ------
+    pyspark.sql.DataFrame
+    """
+    return df.withColumn(
+        column_name_to_assign,
+        F.when(
+            (F.col(first_reference_column) == 0) & (F.col(second_reference_column) == "Insufficient sample"), None
+        ).otherwise(F.col(first_reference_column)),
+    )
