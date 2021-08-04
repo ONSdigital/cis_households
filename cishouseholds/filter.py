04aa5f2784ca82c7220eec7f0a8d0b28d7b28d44
melloj <jonathon.mellor@ons.gov.uk>
2021-08-04 15:35:01 +0100

    Added filter if not null variables function

---
+++ cishouseholds/filter.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from pyspark.sql import DataFrame
+
+
+def filter_not_null(df: DataFrame, reference_columns: List[str]) -> DataFrame:
+    """
+    Filter rows which have NULL values in all the specified columns.
+    From households_aggregate_processes.xlsx, filter number 2.
+
+    Parameters
+    ----------
+    df
+    reference_columns
+        Columns to check for missing values in, all
+        must be missing for the record to be dropped.
+
+    Returns
+    -------
+    pyspark.sql.DataFrame
+    """
+    return df.na.drop(how="all", subset=reference_columns)
