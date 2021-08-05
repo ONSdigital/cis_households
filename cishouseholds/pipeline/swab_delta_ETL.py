36db4dbc275b0919bb15eaed974278e0ccfd49b2
David Foster <foster.dev999@gmail.com>
2021-08-05 10:59:10 +0100

    Add swab cleaning and validation

--- cishouseholds/pipeline/swab_delta_ETL.py
+++ cishouseholds/pipeline/swab_delta_ETL.py
@@ -7,6 +7,7 @@ from cishouseholds.derive import assign_column_convert_to_date
 from cishouseholds.derive import assign_isin_list
 from cishouseholds.derive import derive_ctpattern
 from cishouseholds.derive import mean_across_columns
+from cishouseholds.edit import rename_column_names
 from cishouseholds.pipeline.input_variable_names import swab_variable_name_map
 from cishouseholds.pipeline.validation_schema import swabs_validation_schema
 from cishouseholds.pyspark_utils import create_spark_session
@@ -28,15 +29,9 @@ def swabs_delta_ETL(delta_file_path: str):
 
 def clean_swabs_delta(spark_session: SparkSession, df: DataFrame) -> DataFrame:
     """Clean column names, drop unused data and parse datetime fields on swab delta dataframe."""
-    df = clean_swabs_delta_column_names(df)
+    df = rename_column_names(df, swab_variable_name_map)
     df = df.drop("test_kit")
     df = df.withColumn("swab_pcr_test_date", F.to_timestamp("swab_pcr_test_date", "yyyy-MM-dd HH:mm:ss UTC"))
-
-
-def clean_swabs_delta_column_names(df: DataFrame) -> DataFrame:
-    """Clean column names using dictionary of old to new names."""
-    cleaned_columns = [swab_variable_name_map[old_column_name] for old_column_name in df.columns]
-    return df.toDF(*cleaned_columns)
 
 
 def transform_swabs_delta(spark_session: SparkSession, df: DataFrame) -> DataFrame:
