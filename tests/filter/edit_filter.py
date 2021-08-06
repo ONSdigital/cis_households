0d80cb3eac011239fe085d35d71664175b1203e0
Ohi89 <bokth89@gmail.com>
2021-08-06 11:18:10 +0100

    753-f3_drop-duplicates-close-to-tolerance
    add test function with pytest parametrize

--- tests/filter/edit_filter.py
+++ tests/filter/edit_filter.py
@@ -1,18 +1,40 @@
-from cishouseholds.filter import filter_all_not_null
+import pytest
+from chispa import assert_df_equality
+from cishouseholds.filter import filter_by_cq_diff
 
-# test
-columns = "sample string, date_tested string, cq_value double"
-
-data = [('ONS10948406', '2021-08-13 07:09:41', 12.782375), # Example 1:
-        ('ONS10948406', '2021-08-13 07:09:41', 12.782370), # smaller than 10^-5 - DELETE
+# test data
+@pytest.mark.parametrize(
+    "expected_data",
+    [
+        ('ONS10948406', '2021-08-13 07:09:41', 12.782375), # Example 1:
         ('ONS10948406', '2021-08-13 07:09:41', 12.783275), # larger than 10^-5 - KEEP
         ('ONS10948406', '2021-08-13 07:09:41', 12.983275), # larger than 10^-5 - KEEP
         
         ('ONS74697669', '2021-08-17 07:09:41', 200.782375), # Example 2:
-        ('ONS74697669', '2021-08-17 07:09:41', 200.782370), # smaller than 10^-5 - DELETE
         ('ONS74697669', '2021-08-17 07:09:41', 200.783275), # larger than 10^-5 - KEEP
-]
+    ])
+# test funtion
+def test_filter_by_cq_diff(spark_session, expected_data):
+    expected_schema = "sample string, date_tested string, cq_value double"
+    expected_df = (
+        spark_session.createDataFrame(
+            [expected_data],
+            schema=expected_schema
+        )
+    )
+    # input dataframe
+    df_input = spark_session.createDataFrame(
+        data = [('ONS10948406', '2021-08-13 07:09:41', 12.782375), # Example 1:
+            ('ONS10948406', '2021-08-13 07:09:41', 12.782370), # smaller than 10^-5 - DELETE
+            ('ONS10948406', '2021-08-13 07:09:41', 12.783275), # larger than 10^-5 - KEEP
+            ('ONS10948406', '2021-08-13 07:09:41', 12.983275), # larger than 10^-5 - KEEP
 
-df = spark.createDataFrame(data = data, schema = columns)
+            ('ONS74697669', '2021-08-17 07:09:41', 200.782375), # Example 2:
+            ('ONS74697669', '2021-08-17 07:09:41', 200.782370), # smaller than 10^-5 - DELETE
+            ('ONS74697669', '2021-08-17 07:09:41', 200.783275), # larger than 10^-5 - KEEP
+        ], 
+        schema = "sample string, date_tested string, cq_value double"
+        )
 
-df = filter_all_not_null(df, 'cq_value', 'date_tested')
+    actual_df = filter_by_cq_diff(df_input, 'cq_value', 'date_tested')
+    assert_df_equality(actual_df, expected_df)
