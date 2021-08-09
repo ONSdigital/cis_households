11a9ffd01f4b03ec68201fb6110816f1531f9765
Ohi Bokth <65452503+Ohi89@users.noreply.github.com>
2021-08-09 18:38:51 +0100

    change name of variables, logic, output,
    MOD: change name of variables, correct logic, add extra input for mk_result and change integers 0/1 for strings to Negative/Positive

--- cishouseholds/edit.py
+++ cishouseholds/edit.py
@@ -2,26 +2,35 @@
 import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
 
-def edit_swab_results_single(df:DataFrame, gene_type:str, result_value:str) -> DataFrame:
+
+def edit_swab_results_single(
+                                df:DataFrame, 
+                                gene_result_classification:str, 
+                                gene_result_value:float, 
+                                overall_result_classification:str='result_mk') -> DataFrame:
     """
-    The objective of this function is to correct the result_value from 1 to 0 (Positive to Negative) 
-        in case gene_type is zero.
+    The objective of this function is to edit/correct the gene_result_classification from Positive to Negative or 1 to 0
+        in case gene_result_value is 0.0 or lower and overall_result_classification is Positive or 1.
 
     Parameters
     ----------
     df : Pyspark Dataframe
-    gene_type : String with categorical options "ctOR1ab","ctNgene","ctSgene".
-    result_value : can only be the following discrete integer values 
+    gene_result_classification : String with categorical options "ctOR1ab","ctNgene","ctSgene".
+    gene_result_value : can only be the following discrete integer values 
         (0 - Negative, 1 - Positive, 7 - Rejected, 8 - Inconclusive, 9 - Void, 10 - Insuficient Sample)
 
     Returns
     -------
-    df : Edited Pyspark Dataframe gene_type column with corrected values in case of wrong result_value
-        with the following logic: (result_value == 1) & (gene_type == 0) & (result_mk == 1)
+    df : Edited Pyspark Dataframe gene_result_classification column with corrected values in case of wrong gene_result_value
+        with the following logic: (gene_result_value == 1) & (gene_result_classification == 0) & (result_mk == 1)
     """
-    return df.withColumn(result_value, F.when(
+    return df.withColumn(
+                        gene_result_classification, 
+                        F.when( 
                         # boolean logic:
-                            (F.col(result_value) == 1) & (F.col(gene_type) <= 0) & (F.col('result_mk') == 1), 0
+                            (F.col(gene_result_classification) == 'Positive') & 
+                            (F.col(gene_result_value) <= 0.0) & 
+                            (F.col(overall_result_classification) == 'Positive'), 'Negative'
                         # if boolean condition not met, keep the same value.
-                            ).otherwise(F.col(result_value)))
+                            ).otherwise(F.col(gene_result_classification))) 
 
