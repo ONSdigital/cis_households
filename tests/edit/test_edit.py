843ea8338b1411008675c0f61c5f964c03e4b18e
Ohi Bokth <bokth89@gmail.com>
2021-08-09 20:44:42 +0100

    deleted some repeated dummy data for the pytest

--- tests/edit/test_edit.py
+++ tests/edit/test_edit.py
@@ -4,27 +4,26 @@ from cishouseholds.edit import edit_swab_results_single
 
 
 # Create an example
-@pytest.fixture
-def expected_df(spark_session):
-    return spark_session.createDataFrame(
-        data = [(0, 0, 1),
-                (0, 0, 1),
-                (0, 1, 1)],
-        schema = ["ctNgene_result","ctNgene","result_mk"]
-    )
-
-
-# example df:
-def input_generator(spark_session):
-    return spark_session.createDataFrame(
-            data = [(0, 0, 1),
-                    (1, 0, 1),
-                    (0, 1, 1)],
-            schema = ["ctNgene_result","ctNgene","result_mk"])
-
+@pytest.mark.parametrize(
+    'input_data',
+    [
+        ('Negative', 0.0, 'Positive', 'keep'), # nothing should happen
+        ('Positive', 0.0, 'Positive', 'before'), # this row should be edited
+        ('Negative', 0.0, 'Positive', 'after'), # after editing
+        ('Negative', 1.0, 'Positive', 'keep'), # nothing should happen
+    ]
+)
 
 # Test
-def test_edit_swab_results(spark_session, expected_df):
-    df_input = input_generator(spark_session)
-    actual_df = edit_swab_results_single(df_input, "ctNgene")
+def test_edit_swab_results(spark_session, input_data, edit_swab_results_single):
+    df = spark_session.createDataFrame(
+        data = input_data,
+        schema = ['gene_result_classification', 'gene_result_value', 
+                    'overall_result_classification', 'selection']
+    )
+    
+    input_df = df.filter(df.selection == 'keep' or df.selection == 'before').drop('selection')
+    expected_df = df.filter(df.selection == 'keep' or df.selection == 'after').drop('selection')
+
+    actual_df = edit_swab_results_single(input_df, "gene_result_classification")
     assert_df_equality(actual_df, expected_df)
