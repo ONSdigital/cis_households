88f12cdeeac5d25c080558dee8e2e3201d868272
Ohi Bokth <bokth89@gmail.com>
2021-08-10 18:26:23 +0100

    changed from pytest.mark.parametrize to pytest.fixture and the test works

--- tests/edit/test_edit.py
+++ tests/edit/test_edit.py
@@ -4,25 +4,23 @@ from cishouseholds.edit import edit_swab_results_single
 
 
 # Create an example
-@pytest.mark.parametrize(
-    'input_data',
-    [
-        ('Negative', 0.0, 'Positive', 'keep'), # nothing should happen
+@pytest.fixture
+def dummy_df(spark_session):
+    return spark_session.createDataFrame(
+        data = [
+        ('Negative', 0, 'Positive', 'keep'), # nothing should happen
         ('Positive', 0.0, 'Positive', 'before'), # this row should be edited
         ('Negative', 0.0, 'Positive', 'after'), # after editing
         ('Negative', 1.0, 'Positive', 'keep'), # nothing should happen
-    ]
-)
-
-def test_edit_swab_results(spark_session, input_data):
-    df = spark_session.createDataFrame(
-        data = input_data,
+        ],
         schema = ['gene_result_classification', 'gene_result_value', 
                     'overall_result_classification', 'selection']
-    )
-    
-    input_df = df.filter((df.selection == 'keep') | (df.selection == 'before')).drop('selection')
-    expected_df = df.filter((df.selection == 'keep') | (df.selection == 'after')).drop('selection')
+        )
+
+def test_edit_swab_results(dummy_df):
+
+    input_df = dummy_df.filter((dummy_df.selection == 'keep') | (dummy_df.selection == 'before')).drop('selection')
+    expected_df = dummy_df.filter((dummy_df.selection == 'keep') | (dummy_df.selection == 'after')).drop('selection')
 
     actual_df = edit_swab_results_single(input_df, "gene_result_classification",
                                         "gene_result_value", 'overall_result_classification')
