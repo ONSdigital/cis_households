9ca65b351f68ffa4311f197790e8d9dff8b441d5
David Foster <foster.dev999@gmail.com>
2021-08-10 11:10:59 +0100

    Add double to validator test

--- tests/validate/test_pyspark_validator.py
+++ tests/validate/test_pyspark_validator.py
@@ -4,10 +4,11 @@ from cishouseholds.validate import PySparkValidator
 
 
 def test_validator_with_timestamp(spark_session):
-    schema = {"ts": {"type": "timestamp"}}
+    """Test that validator works with timestamp and double types, which do not exist in super class"""
+    schema = {"ts": {"type": "timestamp"}, "double": {"type": "double"}}
     validator = PySparkValidator(schema)
 
-    df = spark_session.createDataFrame(["1999-07-30 15:00:00"], "string").toDF("ts")
+    df = spark_session.createDataFrame([("1999-07-30 15:00:00", 3.14)], "ts string, double double")
     df = df.withColumn("ts", F.from_unixtime(F.unix_timestamp("ts")).cast("timestamp"))
 
     result = validator(df.rdd.collect()[0].asDict())
