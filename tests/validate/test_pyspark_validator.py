2072ec53625eef43754fcec3d34219f25cc37132
David Foster <foster.dev999@gmail.com>
2021-08-10 11:07:31 +0100

    Fix broken validator test

--- tests/validate/test_pyspark_validator.py
+++ tests/validate/test_pyspark_validator.py
@@ -7,7 +7,7 @@ def test_validator_with_timestamp(spark_session):
     schema = {"ts": {"type": "timestamp"}}
     validator = PySparkValidator(schema)
 
-    df = spark_session.createDataFrame(["1966-07-30 15:00:00"], "string").toDF("ts")
+    df = spark_session.createDataFrame(["1999-07-30 15:00:00"], "string").toDF("ts")
     df = df.withColumn("ts", F.from_unixtime(F.unix_timestamp("ts")).cast("timestamp"))
 
     result = validator(df.rdd.collect()[0].asDict())
