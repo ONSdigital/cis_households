f73e37423b727ce226b14add16a4926864eb5f3c
Ohi Bokth <bokth89@gmail.com>
2021-08-11 09:55:26 +0100

    deleted pytest decorator and added the data inside the test function

--- tests/edit/test_edit.py
+++ tests/edit/test_edit.py
@@ -3,25 +3,27 @@ from chispa import assert_df_equality
 from cishouseholds.edit import edit_swab_results_single
 
 
-# Create an example
-@pytest.fixture
-def dummy_df(spark_session):
-    return spark_session.createDataFrame(
+def test_edit_swab_results(spark_session):
+    # create example data
+    schema = 'gene_result_classification string, gene_result_value double, overall_result_classification string'
+    input_df = spark_session.createDataFrame(
         data = [
-        ('Negative', 0.0, 'Positive', 'keep'), # nothing should happen
-        ('Positive', 0.0, 'Positive', 'before'), # this row should be edited
-        ('Negative', 0.0, 'Positive', 'after'), # after editing
-        ('Negative', 1.0, 'Positive', 'keep'), # nothing should happen
-        ],
-        schema = ['gene_result_classification', 'gene_result_value', 
-                    'overall_result_classification', 'selection']
+                    ('Negative', 0.0, 'Positive'), # nothing should happen
+                    ('Positive', 0.0, 'Positive'), # this row should be edited
+                    ('Negative', 1.0, 'Positive'), # nothing should happen
+                ],
+        schema = schema
         )
-
-def test_edit_swab_results(dummy_df):
-
-    input_df = dummy_df.filter((dummy_df.selection == 'keep') | (dummy_df.selection == 'before')).drop('selection')
-    expected_df = dummy_df.filter((dummy_df.selection == 'keep') | (dummy_df.selection == 'after')).drop('selection')
-
+    
+    expected_df = spark_session.createDataFrame(
+        data = [
+                    ('Negative', 0.0, 'Positive'), # nothing should happen
+                    ('Negative', 0.0, 'Positive'), # after editing
+                    ('Negative', 1.0, 'Positive'), # nothing should happen
+                ],
+        schema = schema
+        )
+    
     actual_df = edit_swab_results_single(input_df, "gene_result_classification",
                                         "gene_result_value", 'overall_result_classification')
     assert_df_equality(actual_df, expected_df)
