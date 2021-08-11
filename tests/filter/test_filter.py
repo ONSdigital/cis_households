872a168dfa8b74cc2d102839f65745c524b853e1
Ohi Bokth <bokth89@gmail.com>
2021-08-11 14:39:31 +0100

    deleted a repeated column intentionally. Review considers it as a unlikely event

--- tests/filter/test_filter.py
+++ tests/filter/test_filter.py
@@ -6,10 +6,10 @@ from cishouseholds.filter import filter_by_cq_diff
 
 def test_filter_by_cq_diff(spark_session):  # test funtion
     expected_data = [
-        ("ONS10948406", "2021-08-13 07:09:41", 12.782375, "keep"),  # Example 1:
-        ("ONS10948406", "2021-08-13 07:09:41", 12.782370, "delete"),  # smaller than 10^-5 - DELETE
-        ("ONS10948406", "2021-08-13 07:09:41", 12.783275, "keep"),  # larger than 10^-5 - KEEP
-        ("ONS10948406", "2021-08-13 07:09:41", 12.983275, "keep"),  # larger than 10^-5 - KEEP
+        ("ONS10948406", "2021-08-13 07:09:41", 0.000005, "keep"),  # Example 1:
+        ("ONS10948406", "2021-08-13 07:09:41", 0.000000, "delete"),  # smaller than 10^-5 - DELETE
+        ("ONS10948406", "2021-08-13 07:09:41", 0.001, "keep"),  # larger than 10^-5 - KEEP
+
         ("ONS74697669", "2021-08-17 07:09:41", 200.782375, "keep"),  # Example 2:
         ("ONS74697669", "2021-08-17 07:09:41", 200.782370, "delete"),  # smaller than 10^-5 - DELETE
         ("ONS74697669", "2021-08-17 07:09:41", 200.783275, "keep"),  # larger than 10^-5 - KEEP
