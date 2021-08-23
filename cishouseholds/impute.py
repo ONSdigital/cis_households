bae935f94e9e708be528a3c6dd1ba69fe43f88a2
Ohi Bokth <bokth89@gmail.com>
2021-08-23 11:06:12 +0100

    added chenges suggested in review and changed assertion bool

--- cishouseholds/impute.py
+++ cishouseholds/impute.py
@@ -1,8 +1,14 @@
+from collections import Counter
+from typing import List
+
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
+from pyspark.sql.window import Window
 
 
-def calculate_imputation_from_mode(df: DataFrame, column_name_to_assign: str, reference_column: str) -> DataFrame:
+def calculate_imputation_from_mode(
+    df: DataFrame, column_name_to_assign: str, reference_column: str, group_column: str
+) -> DataFrame:
     """
     Get imputation value from given column by most repeated value
 
@@ -19,10 +25,62 @@ def calculate_imputation_from_mode(df: DataFrame, column_name_to_assign: str, re
     Function provides a column value for each record that needs to be imputed.
     Where the value does not need to be imputed the column value created will be null.
     """
-    # groupby count() after get all not nulls
-    df_notnulls = df.filter(F.col(reference_column).isNotNull()).groupby(df.white_group).count()
+    # get rid of uac with no nulls
+    df_notnulls = df.filter(F.col(reference_column).isNull()).select(F.col(group_column))
 
-    # dump maximum value
-    mode_imput = df_notnulls.agg({reference_column: "max"}).collect()[0][0]
+    # naming convention
+    list_reference_column = "list_" + reference_column
 
-    return df.withColumn(column_name_to_assign, F.when(F.col(reference_column).isNull(), mode_imput))
+    # window function to collect_list() of all the reference_column items per HH
+    window = Window.partitionBy(group_column)
+    df_window = (
+        df.withColumn(list_reference_column, F.collect_list(F.col(reference_column)).over(window))
+        .filter(F.col(reference_column).isNotNull())
+        .dropDuplicates([group_column, list_reference_column])
+        .drop(reference_column)
+    )
+
+    # JOIN: inner join with the previous df with no nulls filtered
+    df_notnulls = df_notnulls.join(df_window, [group_column])
+
+    # UDF - User defined function to workout the logic for imputation
+    udf_most_common_unique = F.udf(lambda x: most_common_unique_item(x))
+
+    # UDF implementation: with most_common_unique_item() inputation per row is performed
+    df_imputed = df_notnulls.withColumn(
+        column_name_to_assign, udf_most_common_unique(F.col(list_reference_column))
+    ).drop(list_reference_column)
+
+    # JOIN: from initial input df join it with the imputed values for the filtered group_column df
+    # filtered group_column: has None columns, no same number of elements to be imputed
+    actual_df = df.join(df_imputed, [group_column], "outer")
+
+    # Only impute when the cell of the imputed column is null
+    actual_df = actual_df.withColumn(
+        column_name_to_assign,
+        F.when(
+            ((F.col(reference_column) == F.col(column_name_to_assign)) | F.col(reference_column).isNotNull()), None
+        ).otherwise(F.col(column_name_to_assign)),
+    )
+    return actual_df
+
+
+def most_common_unique_item(list_most_common_unrepeated: List[str]):
+    """
+    Works out the most common value in a list ignoring if it is repeated
+
+    Parameters
+    ----------
+    list_most_common_unrepeated
+        List of strings that will be ignored if repeated same amount of times
+        And worked out which is the most common occurence in case it is unique
+    Notes
+    -----
+    This function is implemented as a User Defined Function in Pyspark to carry
+        the imputation for every row.
+    """
+    count = Counter(list_most_common_unrepeated).most_common(2)
+    if len(count) > 1:
+        if count[0][1] - count[1][1] == 0:  # if they are the same, there's a tie
+            return None
+    return Counter(list_most_common_unrepeated).most_common(1)[0][0]
--- tests/impute/test_impute_mode.py
+++ tests/impute/test_impute_mode.py
@@ -1,25 +1,43 @@
 from chispa import assert_df_equality
 
 from cishouseholds.impute import calculate_imputation_from_mode
+from cishouseholds.impute import most_common_unique_item
 
 
-def test_impute_distribution(spark_session):
-
+def test_impute_mode(spark_session):
     expected_df = spark_session.createDataFrame(
         data=[
-            # current stata uses 1, 2 for "male", "female"
-            ("E12000007", "white", "male", None),  # proportion white-male = 1.0 therefore all rnd < 1.0
-            ("E12000007", "white", "male", None),
-            ("E12000007", None, None, "white"),
-            ("E12000007", "white", None, None),
-            ("E12000007", "other", "female", None),  # proportion other-male = 0.0 therefore all rnd > 0.0
-            ("E12000007", "other", "female", None),
-            ("E12000007", None, None, "white"),
+            # if in same hh majority are white/other
+            ("000000000001", "white", None),
+            ("000000000001", None, "white"),
+            ("000000000007", "white", None),
+            ("000000000007", "white", None),
+            ("000000000007", "white", None),
+            ("000000000007", "other", None),
+            ("000000000007", None, "white"),
+            ("222222222222", "other", None),
+            ("222222222222", "other", None),
+            ("222222222222", None, "other"),
+            # there's a tie on the ehtnicity for a specific hh
+            # all of these 3 rows need to be dropped.
+            ("999999999999", "white", None),
+            ("999999999999", "other", None),
+            ("999999999999", None, None),
+            # example where nothing should happen
+            ("XXXXXXXXXXXX", "other", None),
         ],
-        schema="GOR9D string, white_group string, dvsex string, impute_value string",
+        schema="uac_household string, ethnic string, impute_value string",
     )
-
     df_input = expected_df.drop("impute_value")
-    actual_df = calculate_imputation_from_mode(df_input, "impute_value", "white_group")
-
+    actual_df = calculate_imputation_from_mode(df_input, "impute_value", "ethnic", "uac_household")
     assert_df_equality(actual_df, expected_df, ignore_row_order=True, ignore_column_order=True)
+
+
+# Imputation function logic
+def test_mode_logic():
+    assert most_common_unique_item(["a", "a", "b"]) == "a"  # pass
+    assert most_common_unique_item(["a", "a", "b", "b", "c"]) is None  # tie, RETURN: None
+    assert most_common_unique_item(["white"]) == "white"  # pass
+    assert most_common_unique_item(["white", "other"]) is None  # tie, RETURN: None
+    assert most_common_unique_item(["white", "white", "other"]) == "white"  # pass
+    assert most_common_unique_item(["other", "other"]) == "other"  # pass
