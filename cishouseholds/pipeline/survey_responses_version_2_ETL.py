15a30a42a6e83c8cb8e3d9a34e43c6890584d229
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-08-25 16:49:23 +0100

    added test fixes

--- cishouseholds/pipeline/survey_responses_version_2_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_2_ETL.py
@@ -1,22 +1,27 @@
-from cishouseholds.derive import assign_column_regex_match, assign_column_uniform_value, assign_column_convert_to_date, assign_single_column_from_split, assign_consent_code
+from pyspark.accumulators import AddingAccumulatorParam
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+
+from cishouseholds.derive import assign_column_convert_to_date
+from cishouseholds.derive import assign_column_regex_match
+from cishouseholds.derive import assign_column_uniform_value
+from cishouseholds.derive import assign_consent_code
+from cishouseholds.derive import assign_single_column_from_split
+from cishouseholds.extract import read_csv_to_pyspark_df
 from cishouseholds.pipeline.input_variable_names import iqvia_v2_variable_name_map
 from cishouseholds.pipeline.validation_schema import iqvia_v2_validation_schema
-from cishouseholds.pyspark_utils import convert_cerberus_schema_to_pyspark, get_or_create_spark_session
-from cishouseholds.extract import read_csv_to_pyspark_df
-from pyspark.accumulators import AddingAccumulatorParam
+from cishouseholds.pyspark_utils import convert_cerberus_schema_to_pyspark
+from cishouseholds.pyspark_utils import get_or_create_spark_session
 from cishouseholds.validate import validate_and_filter
-from pyspark.sql import SparkSession, DataFrame
+
 
 def survey_responses_version_2_ETL(delta_file_path: str):
     """
     End to end processing of a IQVIA survey responses CSV file.
     """
 
-    print("delta path: ", delta_file_path)
     spark_session = get_or_create_spark_session()
     iqvia_v2_spark_schema = convert_cerberus_schema_to_pyspark(iqvia_v2_validation_schema)
-
-    print("iqvia schema: ",iqvia_v2_spark_schema)
 
     raw_iqvia_v2_data_header = ",".join(iqvia_v2_variable_name_map.keys())
     df = read_csv_to_pyspark_df(
@@ -26,8 +31,6 @@ def survey_responses_version_2_ETL(delta_file_path: str):
         iqvia_v2_spark_schema,
         timestampFormat="yyyy-MM-dd HH:mm:ss 'UTC'",
     )
-    print("print df: ")
-    df.show()
 
     error_accumulator = spark_session.sparkContext.accumulator(
         value=[], accum_param=AddingAccumulatorParam(zero_value=[])
@@ -50,7 +53,7 @@ def transform_survey_responses_version_2_delta(spark_session: SparkSession, df: 
     D13: assign_column_convert_to_date
     D14: assign_single_column_from_split
     D19: assign_consent_code
-    D15: school_year	
+    D15: school_year
     D17: country_barcode
     D18: hh_id_fake
     D19: consent
@@ -71,21 +74,31 @@ def transform_survey_responses_version_2_delta(spark_session: SparkSession, df: 
     df: pyspark.sql.DataFrame
     """
 
-    df = assign_column_uniform_value(df, "dataset", 1) #replace 'n' with chosen value
-    df = assign_column_regex_match(df, "bad_email", "email", r"/^w+[+.w-]*@([w-]+.)*w+[w-]*.([a-z]{2,4}|d+)$/i") #using default email pattern regex to filter 'good' and 'bad' emails
+    df = assign_column_uniform_value(df, "dataset", 1)  # replace 'n' with chosen value
+    df = assign_column_regex_match(
+        df, "bad_email", "email", r"/^w+[+.w-]*@([w-]+.)*w+[w-]*.([a-z]{2,4}|d+)$/i"
+    )  # using default email pattern regex to filter 'good' and 'bad' emails
     df = assign_column_convert_to_date(df, "visit_date", "visit_datetime")
-    df = assign_column_convert_to_date(df, "sample_taken_date",	"samples_taken_datetime")
-    # df = placeholder_for_derivation_number_7-2(df, "week")#derviation number 7 has been used twice - currently associated to ctpatterns
+    df = assign_column_convert_to_date(df, "sample_taken_date", "samples_taken_datetime")
+    # df = placeholder_for_derivation_number_7-2(df, "week")
+    # derviation number 7 has been used twice - currently associated to ctpatterns
     # df = placeholder_for_derivation_number_7-2(df, "month")
-    df = assign_single_column_from_split(df, "outer_postcode", "postcode", " ", 0)# splits on space between postcode segments and gets left half
-    # df = placeholder_for_derivation_number_17(df, "country_barcode", ["swab_barcode_cleaned","blood_barcode_cleaned"], {0:"ONS", 1:"ONW", 2:"ONN", 3:"ONC"})
+    df = assign_single_column_from_split(
+        df, "outer_postcode", "postcode", " ", 0
+    )  # splits on space between postcode segments and gets left half
+    # df = placeholder_for_derivation_number_17(df, "country_barcode", ["swab_barcode_cleaned","blood_barcode_cleaned"],
+    #  {0:"ONS", 1:"ONW", 2:"ONN", 3:"ONC"})
     # df = placeholder_for_derivation_number_18(df, "hh_id_fake",	"ons_household_id")
-    df = assign_consent_code(df, "consent",	["consent_16_visits", "consent_5_visits", "consent_1_visit"])
-    # df = placeholder_for_derivation_number_20(df, "work_healthcare", ["work_healthcare_v1", "work_direct_contact"])
-    # df = placeholder_for_derivation_number_21(df, "work_socialcare", ["work_sector", "work_care_nursing_home" , "work_direct_contact"])
+    df = assign_consent_code(df, "consent", ["consent_16_visits", "consent_5_visits", "consent_1_visit"])
+    # df = placeholder_for_derivation_number_20(df, "work_healthcare",
+    # ["work_healthcare_v1", "work_direct_contact"])
+    # df = placeholder_for_derivation_number_21(df, "work_socialcare",
+    # ["work_sector", "work_care_nursing_home" , "work_direct_contact"])
     # df = placeholder_for_derivation_number_10(df, "self_isolating", "self_isolating_v1")
-    # df = placeholder_for_derivation_number_10(df, "contact_hospital", ["contact_participant_hospital", "contact_other_in_hh_hospital"])
-    # df = placeholder_for_derivation_number_10(df, "contact_carehome", ["contact_participant_carehome", "contact_other_in_hh_carehome"])
+    # df = placeholder_for_derivation_number_10(df, "contact_hospital",
+    # ["contact_participant_hospital", "contact_other_in_hh_hospital"])
+    # df = placeholder_for_derivation_number_10(df, "contact_carehome",
+    # ["contact_participant_carehome", "contact_other_in_hh_carehome"])
     # df = placeholder_for_derivation_number_22(df, "age_at_visit", "visit_date,dob")
     # df = placeholder_for_derivation_number_23(df, "work_status", ["work_status_v1", "work_status_v2"])
 
