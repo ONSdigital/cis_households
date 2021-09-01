c11978dfbf99649992fac4a29b3558de15e6ff1b
Rowan Hemsi <70013144+rowanhemsi@users.noreply.github.com>
2021-09-01 14:42:53 +0100

    876 Add assign_merge_process_group_flag with test (#57)
    * Add assign_merge_process_group_flag with test
    
    * Edit docstring
    
    * Address comments
    
    Co-authored-by: Jonathon Mellor <jonathon.mellor@ons.gov.uk>
    Co-authored-by: George Feldman <george.feldman@ons.gov.uk>

--- cishouseholds/merge.py
+++ cishouseholds/merge.py
@@ -74,3 +74,45 @@ def join_dataframes(df1: DataFrame, df2: DataFrame, reference_column: str, join_
         Specify join type to apply to .join() method
     """
     return df1.join(df2, on=reference_column, how=join_type)
+
+
+def assign_merge_process_group_flag(
+    df: DataFrame,
+    column_name_to_assign: str,
+    out_of_date_range_flag: str,
+    count_barcode_labs_column_name: str,
+    count_barcode_labs_condition: str,
+    count_barcode_voyager_column_name: str,
+    count_barcode_voyager_condition: str,
+):
+    """
+    Combine three conditions to create flag indicating record to be processed in forthcoming matching process.
+    This is run for each of 1:1, 1:many, many:1 and many:many to identify the relevant processing group.
+
+    Parameters
+    ----------
+    df
+    column_name_to_assign
+    out_of_date_range_flag
+    count_barcode_labs_column_name
+    count_barcode_labs_condition
+    count_barcode_voyager_column_name
+    count_barcode_voyager_condition
+    """
+
+    count_barcode_labs_condition = F.expr(f"{count_barcode_labs_column_name} {count_barcode_labs_condition}")
+    count_barcode_voyager_condition = F.expr(f"{count_barcode_voyager_column_name} {count_barcode_voyager_condition}")
+
+    df = df.withColumn("count_barcode_labs_flag", F.when(count_barcode_labs_condition, 1))
+    df = df.withColumn("count_barcode_voyager_flag", F.when(count_barcode_voyager_condition, 1))
+
+    return df.withColumn(
+        column_name_to_assign,
+        F.when(
+            (
+                F.col(out_of_date_range_flag).isNull()
+                & (F.col("count_barcode_labs_flag") + F.col("count_barcode_voyager_flag") == 2)
+            ),
+            1,
+        ).otherwise(None),
+    ).drop("count_barcode_labs_flag", "count_barcode_voyager_flag")
