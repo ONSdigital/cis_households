9154ce0b0aeac059d641d37b4e8880c9e1915b37
Rowan Hemsi <rowan.hemsi@ons.gov.uk>
2021-09-01 12:56:31 +0100

    Add assign_merge_process_group_flag with test

--- cishouseholds/merge.py
+++ cishouseholds/merge.py
@@ -53,3 +53,39 @@ def join_dataframes(df1: DataFrame, df2: DataFrame, reference_column: str, join_
         Specify join type to apply to .join() method
     """
     return df1.join(df2, on=reference_column, how=join_type)
+
+
+def assign_merge_process_group_flag(
+    df: DataFrame,
+    column_name_to_assign: str,
+    out_of_date_range_flag,
+    count_barcode_labs_column_name,
+    count_barcode_labs_condition,
+    count_barcode_voyager_column_name,
+    count_barcode_voyager_condition,
+):
+    """
+    Combine three conditions to create flag indicating record to be processed in forthcoming matching process.
+
+    Parameters
+    ----------
+    df
+    column_name_to_assign
+    """
+
+    count_barcode_labs_condition = F.expr(f"{count_barcode_labs_column_name} {count_barcode_labs_condition}")
+    count_barcode_voyager_condition = F.expr(f"{count_barcode_voyager_column_name} {count_barcode_voyager_condition}")
+
+    df = df.withColumn("count_barcode_labs_flag", F.when(count_barcode_labs_condition, 1))
+    df = df.withColumn("count_barcode_voyager_flag", F.when(count_barcode_voyager_condition, 1))
+
+    return df.withColumn(
+        column_name_to_assign,
+        F.when(
+            (
+                F.col(out_of_date_range_flag).isNull()
+                & (F.col("count_barcode_labs_flag") + F.col("count_barcode_voyager_flag") == 2)
+            ),
+            1,
+        ).otherwise(None),
+    ).drop("count_barcode_labs_flag", "count_barcode_voyager_flag")
