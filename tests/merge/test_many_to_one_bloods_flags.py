c34bd77f99e2ebe1f9e01457896a18310075508b
Jamie Howells <jamie.howells@ons.gov.uk>
2021-09-02 12:16:55 +0100

    changes to many-to-one function post-review

--- tests/merge/test_many_to_one_bloods_flags.py
+++ tests/merge/test_many_to_one_bloods_flags.py
@@ -1,9 +1,9 @@
 from chispa import assert_df_equality
 
-from cishouseholds.merge import many_to_one_bloods_flag
+from cishouseholds.merge import many_to_one_antibody_flag
 
 
-def test_many_to_one_bloods_flag(spark_session):
+def test_many_to_one_antibody_flag(spark_session):
 
     expected_df = spark_session.createDataFrame(
         data=[
@@ -14,12 +14,12 @@ def test_many_to_one_bloods_flag(spark_session):
             ("ABC789", 1, 1, 2, None),
             ("ABC789", 1, 1, 2, None),
         ],
-        schema="blood_barcode_cleaned string, out_of_date_range_bloods integer, count_barcode_blood integer, \
-               count_barcode_v2 integer,drop_many_to_one_bloods_flag integer",
+        schema="antibody_barcode_cleaned string, out_of_date_range_antibody integer, count_barcode_antibody integer, \
+               count_barcode_voyager integer, drop_many_to_one_antibody_flag integer",
     )
 
-    input_df = expected_df.drop("drop_many_to_one_bloods_flag")
+    input_df = expected_df.drop("drop_many_to_one_antibody_flag")
 
-    output_df = many_to_one_bloods_flag(input_df, "drop_many_to_one_bloods_flag")
+    output_df = many_to_one_antibody_flag(input_df, "drop_many_to_one_antibody_flag", "antibody_barcode_cleaned")
 
     assert_df_equality(output_df, expected_df, ignore_row_order=True)
