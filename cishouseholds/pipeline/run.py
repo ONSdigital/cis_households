7e31f96654fb84baec1ce65372996ba3806d3b90
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-09-27 10:17:52 +0100

    added run config

--- cishouseholds/pipeline/run.py
+++ cishouseholds/pipeline/run.py
@@ -1,23 +1,26 @@
 import os
 
 import yaml
-
-# import bloods_delta_ETL
-# import sample_delta_ETL
-# import survey_responses_version_2_ETL
+from bloods_delta_ETL import bloods_delta_ETL  # noqa F401
+from sample_delta_ETL import sample_delta_ETL  # noqa F401
+from survey_responses_version_2_ETL import survey_responses_version_2_ETL  # noqa F401
 
 # Load YAML data from the file
 
 # Print current working directory
-print("Current working dir : {}".format(os.getcwd()))
 
-with open(os.path.join(os.getcwd(), "cishouseholds/pipeline/config.yaml")) as fh:
 
-    read_data = yaml.load(fh, Loader=yaml.FullLoader)
+def run_from_config(config_location: str):
+    with open(config_location) as fh:
 
-print(read_data)
+        read_data = yaml.load(fh, Loader=yaml.FullLoader)
 
-for ETL in read_data:
-    if ETL["run"]:
-        print("to run...", ETL["function"])
-        ETL["function"](ETL["resource_path"])
+    print(read_data)
+    print("locales: ", locals())
+
+    for ETL in read_data:
+        if ETL["run"]:
+            locals()[ETL["function"]](ETL["resource_path"])
+
+
+run_from_config(os.path.join(os.getcwd(), "cishouseholds/pipeline/config.yaml"))
