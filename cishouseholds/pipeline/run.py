a975d6e8f5552055f809a511e8376e353ed4c3eb
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-09-28 12:39:23 +0100

    pytest conflict

--- cishouseholds/pipeline/run.py
+++ cishouseholds/pipeline/run.py
@@ -2,10 +2,10 @@ import os
 
 import yaml
 
+from cishouseholds.pipeline.a_test_ETL import test_ETL  # noqa F401
 from cishouseholds.pipeline.bloods_delta_ETL import bloods_delta_ETL  # noqa F401
 from cishouseholds.pipeline.sample_delta_ETL import sample_delta_ETL  # noqa F401
 from cishouseholds.pipeline.survey_responses_version_2_ETL import survey_responses_version_2_ETL  # noqa F401
-from cishouseholds.pipeline.test_ETL import test_ETL  # noqa F401
 
 
 def run_from_config(config_location: str):
