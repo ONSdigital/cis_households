1b8f7d8d0e63129f6128e64f2aafed00ebc4ffd3
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2021-10-05 09:42:34 +0000

    test

--- cishouseholds/pipeline/survey_responses_version_2_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_2_ETL.py
@@ -37,7 +37,7 @@ def survey_responses_version_2_ETL(delta_file_path: str):
     )
 
     df = validate_and_filter(df, iqvia_v2_validation_schema, error_accumulator)
-    df = transform_survey_responses_version_2_delta(spark_session, df)
+    # df = transform_survey_responses_version_2_delta(spark_session, df)
     df = load_survey_responses_version_2_delta(spark_session, df)
     return df
 
