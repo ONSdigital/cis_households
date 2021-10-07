c924a872fc2d0c80d06b7561080ee5d6f2c86336
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-10-07 16:20:53 +0100

    done

---
+++ tests/edit/test_convert_null_if_not_in_list.py
@@ -0,0 +1,25 @@
+import pytest
+from chispa import assert_df_equality
+
+from cishouseholds.edit import convert_null_if_not_in_list
+
+
+@pytest.fixture
+def input_df(spark_session):
+    return spark_session.createDataFrame(
+        data=[("male", 1), ("female", 2), ("helicopter", 3), ("dont know", 4)],
+        schema="""sex string, id integer""",
+    )
+
+
+@pytest.fixture
+def expected_df(spark_session):
+    return spark_session.createDataFrame(
+        data=[("male", 1), ("female", 2), (None, 3), (None, 4)],
+        schema="""sex string, id integer""",
+    )
+
+
+def test_convert_null_if_not_in_list(input_df, expected_df):
+    output_df = convert_null_if_not_in_list(input_df, "sex", ["male", "female"])
+    assert_df_equality(output_df, expected_df)
