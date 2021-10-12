16bd57a342519f427d6996572aefc1a25bff49bb
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-10-12 11:45:18 +0100

    refactored test

--- tests/edit/test_format_string_upper_and_clean.py
+++ tests/edit/test_format_string_upper_and_clean.py
@@ -1,29 +1,22 @@
-import pytest
 from chispa import assert_df_equality
 
 from cishouseholds.edit import format_string_upper_and_clean
 
 
-@pytest.fixture
-def input_df(spark_session):
-    return spark_session.createDataFrame(
+def test_format_string_upper_and_clean(spark_session):
+    input_df = spark_session.createDataFrame(
         data=[
             ("       this is a   string          .   ", 1),
         ],
         schema="""ref string, id integer""",
     )
 
-
-@pytest.fixture
-def expected_df(spark_session):
-    return spark_session.createDataFrame(
+    expected_df = spark_session.createDataFrame(
         data=[
             ("THIS IS A STRING", 1),
         ],
         schema="""ref string, id integer""",
     )
 
-
-def test_format_string_upper_and_clean(expected_df, input_df):
     output_df = format_string_upper_and_clean(input_df, "ref")
     assert_df_equality(expected_df, output_df)
