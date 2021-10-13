5780017e18a92a93a0e1fc7733896b6115114658
fosted1 <David.Foster@ons.gov.uk>
2021-10-13 09:52:30 +0000

    Update blood delta ETL to separate load

--- cishouseholds/pipeline/blood_delta_ETL.py
+++ cishouseholds/pipeline/blood_delta_ETL.py
@@ -1,5 +1,4 @@
 from itertools import chain
-
 from pyspark.accumulators import AddingAccumulatorParam
 from pyspark.sql import DataFrame
 
@@ -8,11 +7,11 @@ from cishouseholds.derive import substring_column
 from cishouseholds.edit import convert_columns_to_timestamps
 from cishouseholds.edit import update_schema_types
 from cishouseholds.extract import read_csv_to_pyspark_df
-from cishouseholds.pipeline.input_variable_names import bloods_variable_name_map
+from cishouseholds.pipeline.input_variable_names import blood_variable_name_map
 from cishouseholds.pipeline.load import update_table
 from cishouseholds.pipeline.pipeline_stages import register_pipeline_stage
 from cishouseholds.pipeline.timestamp_map import antibody_time_map
-from cishouseholds.pipeline.validation_schema import bloods_validation_schema
+from cishouseholds.pipeline.validation_schema import blood_validation_schema
 from cishouseholds.pyspark_utils import convert_cerberus_schema_to_pyspark
 from cishouseholds.pyspark_utils import get_or_create_spark_session
 from cishouseholds.validate import validate_and_filter
@@ -20,39 +19,49 @@ from cishouseholds.validate import validate_and_filter
 # from cishouseholds.compare import prepare_for_union
 
 
-@register_pipeline_stage("bloods_delta_ETL")
-def bloods_delta_ETL(resource_path: str):
-    spark_session = get_or_create_spark_session()
-    bloods_spark_schema = convert_cerberus_schema_to_pyspark(bloods_validation_schema)
-    # ref_file_path = "" # reference to parquet file path
+@register_pipeline_stage("blood_delta_ETL")
+def blood_delta_ETL(resource_path: str):
+    df = extract_validate_transform_blood_delta(resource_path)
+    update_table(df, "processed_blood_test_results")
+    return df
 
-    raw_bloods_delta_header = ",".join(bloods_variable_name_map.keys())
+
+def extract_validate_transform_blood_delta(resource_path: str):
+    spark_session = get_or_create_spark_session()
+
+    error_accumulator = spark_session.sparkContext.accumulator(
+        value=[], accum_param=AddingAccumulatorParam(zero_value=[])
+    )
+    df = extract_blood_delta(spark_session, resource_path)
+    df = convert_columns_to_timestamps(df, antibody_time_map)
+    antibody_time_map_list = list(chain(*list(antibody_time_map.values())))
+    _blood_validation_schema = update_schema_types(
+        blood_validation_schema, antibody_time_map_list, {"type": "timestamp"}
+    )
+
+    df = validate_and_filter(df, _blood_validation_schema, error_accumulator)
+    df = transform_blood_delta(df)
+    # df = prepare_for_union(df, None)
+    # df = load_bloods_delta(df)
+    return df
+
+
+def extract_blood_delta(spark_session, resource_path: str):
+    bloods_spark_schema = convert_cerberus_schema_to_pyspark(blood_validation_schema)
+
+    raw_bloods_delta_header = ",".join(blood_variable_name_map.keys())
     df = read_csv_to_pyspark_df(
         spark_session,
         resource_path,
         raw_bloods_delta_header,
         bloods_spark_schema,
     )
-
-    error_accumulator = spark_session.sparkContext.accumulator(
-        value=[], accum_param=AddingAccumulatorParam(zero_value=[])
-    )
-    df = convert_columns_to_timestamps(df, antibody_time_map)
-    antibody_time_map_list = list(chain(*list(antibody_time_map.values())))
-    _bloods_validation_schema = update_schema_types(
-        bloods_validation_schema, antibody_time_map_list, {"type": "timestamp"}
-    )
-    df = validate_and_filter(df, _bloods_validation_schema, error_accumulator)
-    df = transform_bloods_delta(df)
-    # df = prepare_for_union(df, None)
-    # df = load_bloods_delta(df)
-    update_table(df, "processed_blood_test_results")
     return df
 
 
-def transform_bloods_delta(df: DataFrame) -> DataFrame:
+def transform_blood_delta(df: DataFrame) -> DataFrame:
     """
-    Call functions to process input for bloods deltas.
+    Call functions to process input for blood deltas.
     D1: substring_column
     D11: assign_column_uniform_value
 
--- tests/pipeline/test_blood_deltas.py
+++ tests/pipeline/test_blood_deltas.py
@@ -2,12 +2,12 @@ import pandas as pd
 import pytest
 from mimesis.schema import Schema
 
-from cishouseholds.pipeline.bloods_delta_ETL import bloods_delta_ETL
+from cishouseholds.pipeline.blood_delta_ETL import extract_validate_transform_blood_delta
 from dummy_data_generation.schemas import get_blood_data_description
 
 
 @pytest.fixture
-def bloods_dummy_df(mimesis_field):
+def blood_dummy_df(mimesis_field):
     """
     Generate lab bloods file.
     """
@@ -16,10 +16,10 @@ def bloods_dummy_df(mimesis_field):
     return pandas_df
 
 
-def test_bloods_delta_ETL_end_to_end(regression_test_df, bloods_dummy_df, pandas_df_to_temporary_csv):
+def test_blood_delta_ETL_end_to_end(regression_test_df, blood_dummy_df, pandas_df_to_temporary_csv):
     """
     Test that valid example data flows through the ETL from a csv file.
     """
-    csv_file = pandas_df_to_temporary_csv(bloods_dummy_df)
-    processed_df = bloods_delta_ETL(csv_file.as_posix())
+    csv_file = pandas_df_to_temporary_csv(blood_dummy_df)
+    processed_df = extract_validate_transform_blood_delta(csv_file.as_posix())
     regression_test_df(processed_df, "blood_sample_barcode", "processed_blood")
