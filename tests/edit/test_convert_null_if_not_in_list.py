fa087ce854e0e151fc77895585927de8f946e44f
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-10-14 11:16:10 +0100

    revised test

--- tests/edit/test_convert_null_if_not_in_list.py
+++ tests/edit/test_convert_null_if_not_in_list.py
@@ -5,12 +5,12 @@ from cishouseholds.edit import convert_null_if_not_in_list
 
 def test_convert_null_if_not_in_list(spark_session):
     expected_df = spark_session.createDataFrame(
-        data=[("male", 1), ("female", 2), (None, 3), (None, 4)],
-        schema="""sex string, id integer""",
+        data=[("Renault", 1), ("Fiat", 2), (None, 3), (None, 4)],
+        schema="""french_cars string, id integer""",
     )
     input_df = spark_session.createDataFrame(
-        data=[("male", 1), ("female", 2), ("X", 3), ("dont know", 4)],
-        schema="""sex string, id integer""",
+        data=[("Renault", 1), ("Fiat", 2), ("Kia", 3), ("Honda", 4)],
+        schema="""french_cars string, id integer""",
     )
-    output_df = convert_null_if_not_in_list(input_df, "sex", ["male", "female"])
+    output_df = convert_null_if_not_in_list(input_df, "french_cars", ["Renault", "Fiat"])
     assert_df_equality(output_df, expected_df)
