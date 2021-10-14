0b2d2d1eb132f3acfa9d007ecb5aca6d746256bd
Ohi Bokth <bokth89@gmail.com>
2021-10-14 10:03:10 +0100

    for loop, and adding final filtering steps of sucessful merge, not merged and failed to merge

--- cishouseholds/pipeline/merge_process.py
+++ cishouseholds/pipeline/merge_process.py
@@ -5,7 +5,7 @@ from cishouseholds.validate import validate_merge_logic
 def execute_and_resolve_flags_merge_specific_swabs(survey_df, labs_df, column_name_date_visit):
     """ """
 
-    outer_df = execute_and_resolve_flags_merge_part1(survey_df, labs_df)
+    outer_df = merge_process_perparation(survey_df, labs_df)
 
     window_columns = [
         "abs_offset_diff_vs_visit_hr",
@@ -41,14 +41,18 @@ def execute_and_resolve_flags_merge_specific_swabs(survey_df, labs_df, column_na
         process_type="swab",
         failed_flag_column_name_to_assign="failed_flag_many_to_many_swabs",
     )
-    outer_df = execute_and_resolve_flags_merge_part2(outer_df)
+    outer_df = merge_process_validation(outer_df)
 
-    return outer_df
+    return merge_process_filtering(outer_df)
 
 
 def execute_and_resolve_flags_merge_specific_antibody(survey_df, labs_df, column_name_date_visit):
     """ """
-    outer_df = execute_and_resolve_flags_merge_part1(survey_df, labs_df)
+    survey_df = M.assign_unique_identifier_column(survey_df, "unique_id_voyager", ordering_columns=["barcode"])
+
+    survey_df = M.assign_count_of_occurrences_column(survey_df, "barcode", "count_barcode_voyager")
+
+    outer_df = merge_process_perparation(survey_df, labs_df)
 
     window_columns = [
         "abs_offset_diff_vs_visit_hr",
@@ -82,12 +86,12 @@ def execute_and_resolve_flags_merge_specific_antibody(survey_df, labs_df, column
         failed_flag_column_name_to_assign="failed_flag_many_to_many_antibody",
     )
 
-    outer_df = execute_and_resolve_flags_merge_part2(outer_df)
+    outer_df = merge_process_filtering(outer_df, "antibody")
 
-    return outer_df
+    return merge_process_filtering(outer_df)
 
 
-def execute_and_resolve_flags_merge_part1(survey_df, labs_df):
+def merge_process_perparation(survey_df, labs_df):
     """ """
     labs_df = M.assign_unique_identifier_column(labs_df, "unique_id_swab", ordering_columns=["barcode"])
     labs_df = M.assign_count_of_occurrences_column(labs_df, "barcode", "count_barcode_swab")
@@ -109,36 +113,37 @@ def execute_and_resolve_flags_merge_part1(survey_df, labs_df):
     return outer_df
 
 
-def execute_and_resolve_flags_merge_part2(outer_df):
+def merge_process_validation(outer_df, merge_type: str):
     """ """
     # make a FOR loop so that the assign_merge_process_group_flag() does not repeat
-    outer_df = M.assign_merge_process_group_flag(
-        outer_df,
-        "1_to_ms",
-        "out_of_date_range_swab",
-        "count_barcode_swab",
-        "==1",
-        "count_barcode_voyager",
-        ">1",
-    )
-    outer_df = M.assign_merge_process_group_flag(
-        outer_df,
-        "m_to_1s",
-        "out_of_date_range_swab",
-        "count_barcode_swab",
-        ">1",
-        "count_barcode_voyager",
-        "==1",
-    )
-    outer_df = M.assign_merge_process_group_flag(
-        outer_df,
-        "m_to_ms",
-        "out_of_date_range_swab",
-        "count_barcode_swab",
-        ">1",
-        "count_barcode_voyager",
-        ">1",
-    )
+    count_barcodes_lab_condition_list = ["==1", ">1", ">1"]
+    count_barcodes_voyager_condition_list = [">1", "==1", ">1"]
+    if merge_type == "swabs":
+        merge_types = ["1_to_ms", "m_to_1s", "m_to_ms"]
+        out_of_date_range = "out_of_date_range_swab"
+        count_barcode = "count_barcode_swab"
+    elif merge_type == "antibody":
+        merge_types = ["1_to_ma", "m_to_1a", "m_to_ma"]
+        out_of_date_range = "out_of_date_range_antibody"
+        count_barcode = "count_barcode_antibody"
+
+    for merge, count_barcodes_lab, count_barcodes_voyager, out_of_date, count in zip(
+        merge_types,
+        count_barcodes_lab_condition_list,
+        count_barcodes_voyager_condition_list,
+        out_of_date_range,
+        count_barcode,
+    ):
+        outer_df = M.assign_merge_process_group_flag(
+            df=outer_df,
+            column_name_to_assign=merge,
+            out_of_date_range_flag=out_of_date,
+            count_barcode_labs_column_name=count,
+            count_barcode_labs_condition=count_barcodes_lab,
+            count_barcode_voyager_column_name="count_barcode_voyager",
+            count_barcode_voyager_condition=count_barcodes_voyager,
+        )
+
     outer_df = validate_merge_logic(
         df=outer_df,
         flag_column_names=[
@@ -150,10 +155,14 @@ def execute_and_resolve_flags_merge_part2(outer_df):
         match_type_colums=["1_to_ms", "m_to_1s", "m_to_ms"],
         group_by_column="barcode",
     )
+
+    return outer_df
+
+
+def merge_process_filtering(outer_df):
     # filter output dataframe
 
     # filter not-best match dataframe
 
     # filter unmatched dataframe
-
-    return outer_df
+    pass
