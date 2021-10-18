fa373f3519ed3017bb726207e506dffe3a186465
Jamie Howells <jamie.howells@ons.gov.uk>
2021-10-18 14:39:41 +0100

    Added config call to merge_ETL

--- cishouseholds/pipeline/merge_ETL.py
+++ cishouseholds/pipeline/merge_ETL.py
@@ -1,4 +1,5 @@
 from cishouseholds.pipeline.load import extract_from_table
+from cishouseholds.pipeline.load import get_config
 from cishouseholds.pipeline.load import update_table
 from cishouseholds.pipeline.merge_process import execute_and_resolve_flags_merge_specific_antibody
 from cishouseholds.pipeline.merge_process import execute_and_resolve_flags_merge_specific_swabs
@@ -10,16 +11,17 @@ def merge_antibody_swab_etl():
     """
     High level function call for running merging process for antibody and swab
     """
-    merge_antibody_etl()
-    merge_swab_etl()
+    storage_config = get_config()["storage"]
+    merge_antibody_etl(storage_config)
+    merge_swab_etl(storage_config)
 
 
-def merge_antibody_etl():
+def merge_antibody_etl(storage_config):
     """
     Process for matching and merging survey & swab data
     """
-    survey_table = "processed_survey_responses_v2"
-    antibody_table = "processed_blood_test_results"
+    survey_table = f"{storage_config['table_prefix']}processed_survey_responses_v2"
+    antibody_table = f"{storage_config['table_prefix']}processed_blood_test_results"
     survey_df = extract_from_table(survey_table)
     antibody_df = extract_from_table(antibody_table)
     df_best_match, df_not_best_match = execute_and_resolve_flags_merge_specific_antibody(
@@ -31,12 +33,12 @@ def merge_antibody_etl():
         survey_df = update_table(name, table_name)
 
 
-def merge_swab_etl():
+def merge_swab_etl(storage_config):
     """
     Process for matching and merging survey & swab data (after merging with antibody)
     """
-    survey_table = "processed_survey_antibody_merge"
-    swab_table = "processed_swab_test_results"
+    survey_table = f"{storage_config['table_prefix']}processed_survey_antibody_merge"
+    swab_table = f"{storage_config['table_prefix']}processed_swab_test_results"
     survey_df = extract_from_table(survey_table)
     swab_df = extract_from_table(swab_table)
     df_best_match, df_not_best_match, df_failed_match = execute_and_resolve_flags_merge_specific_swabs(
