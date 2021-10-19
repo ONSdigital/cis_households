85277f6147058e20a555ff72f668aea395a9b7e9
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-10-19 12:55:53 +0100

    pre

--- cishouseholds/derive.py
+++ cishouseholds/derive.py
@@ -6,44 +6,23 @@ from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 
 
-def assign_column_from_mapped_list_key(
-    df: DataFrame, column_name_to_asign: str, reference_column: str, map: dict, sep="|"
-):
-    """ """
-    cast_type = type(list(map.keys())[0]).__name__
-    df = df.withColumn(column_name_to_asign, F.lit(None).cast(cast_type))
+def assign_column_from_mapped_list_key(df: DataFrame, column_name_to_asign: str, reference_column: str, map: dict):
+    """
+    Assing a speciifc column value using a dictionary of values to assign as keys and
+    the list criteria corresponing to when that value should be assign as a value
+    Parameters
+    ----------
+    df
+    column_name_to_assign
+    reference_column
+    map
+    """
+    df = df.withColumn(column_name_to_asign, F.lit(None))
     for val, key_list in map.items():
         df = df.withColumn(
             column_name_to_asign,
             F.when(F.col(reference_column).isin(*key_list), val).otherwise(F.col(column_name_to_asign)),
         )
-    return df
-
-
-def assign_column_from_mapped_reference_column(
-    df: DataFrame, reference_column: str, column_name_to_assign: str, map: dict, include_not_in_map: bool = False
-):
-    """
-    Assign column_name_to_assign to mapped values of reference column
-    Parameters
-    ----------
-    df
-    reference_column
-    column_name_to_assign
-    map
-    include_not_in_map
-        allows values not mapped in map to remain as their current values
-    """
-    if column_name_to_assign not in df.columns:
-        df = df.withColumn(column_name_to_assign, F.lit(None))
-    mapping_expr = F.create_map([F.lit(x) for x in chain(*map.items())])  # type: ignore
-    df = df.withColumn(
-        column_name_to_assign,
-        F.when(
-            (F.lit(not include_not_in_map) | (F.col(reference_column).isin(*list(map.keys())))),
-            mapping_expr[df[reference_column]],
-        ).otherwise(F.col(column_name_to_assign)),
-    )
     return df
 
 
@@ -95,9 +74,12 @@ def assign_work_patient_facing_now(
         column_name_to_assign,
         work_healthcare_column,
         {
-            "No": ["No", "Yes, primary care, patient-facing", "Yes, secondary care, patient-facing"],
-            "Yes": [
+            "No": [
+                "Yes, primary care, patient-facing",
+                "Yes, secondary care, patient-facing",
                 "Yes, other healthcare, patient-facing",
+            ],
+            "Yes": [
                 "Yes, primary care, non-patient-facing",
                 "Yes, secondary care, non-patient-facing",
                 "Yes, other healthcare, non-patient-facing",
@@ -113,7 +95,6 @@ def assign_work_patient_facing_now(
 def assign_work_person_facing_now(
     df: DataFrame,
     column_name_to_assign: str,
-    age_column: str,
     work_patient_facing_now_column: str,
     work_social_care_column: str,
 ):
@@ -128,8 +109,24 @@ def assign_work_person_facing_now(
     column_name_to_assign
     """
     df = df.withColumn(column_name_to_assign, F.col(work_patient_facing_now_column))
-    df = assign_named_buckets(
-        df, age_column, column_name_to_assign, {0: "<=15y", 16: F.col(work_social_care_column), 75: ">=75y"}
+    df = assign_column_from_mapped_list_key(
+        df,
+        column_name_to_assign,
+        work_social_care_column,
+        {
+            "Yes": ["Yes, care/residential home, resident-facing", "Yes, other social care, resident-facing"],
+            "No": [
+                "No",
+                "Yes, care/residential home, non-resident-facing",
+                "Yes, other social care, non-resident-facing",
+            ],
+        },
+    )
+    df = df.withColumn(
+        column_name_to_assign,
+        F.when(F.col(work_patient_facing_now_column) == "Yes", "Yes")
+        .when(~F.col(work_patient_facing_now_column).isin("Yes", "No"), F.col(work_patient_facing_now_column))
+        .otherwise(F.col(column_name_to_assign)),
     )
     return df
 
