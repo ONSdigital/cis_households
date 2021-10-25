26e127e2678f7464573d18333e74f192771c9a8a
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-10-25 16:19:49 +0100

    merged main

--- cishouseholds/validate.py
+++ cishouseholds/validate.py
@@ -98,10 +98,6 @@ def validate_csv_header(text_file: RDD, expected_header: str):
         Exact header expected in csv file
     """
     header = text_file.first()
-    for i, field in enumerate(expected_header.split("|")):
-        _field = header.split("|")[i].strip()
-        if _field != field:
-            print("mismatch: ", field, "!=", _field)
     return expected_header == header
 
 
