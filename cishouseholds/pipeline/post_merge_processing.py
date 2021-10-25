034f0fba1c3c7e81036dec05f5513a36e05b40bb
fosted1 <David.Foster@ons.gov.uk>
2021-10-25 10:35:08 +0000

    Add integration test for imputation

--- cishouseholds/pipeline/post_merge_processing.py
+++ cishouseholds/pipeline/post_merge_processing.py
@@ -7,13 +7,16 @@ from cishouseholds.pipeline.pipeline_stages import register_pipeline_stage
 
 @register_pipeline_stage("process_post_merge")
 def process_post_merge():
+    # Todo: Merge on previously imputed values
     pass
 
 
 def impute_key_demographics(df):
-    # Todo: Merge on previously imputed values
+    """Impute missing values for key variables that are required for weight calibration."""
+
     for demographic_column in ["white", "sex", "date_of_birth"]:
         df = impute_and_flag(
+            df,
             imputation_function=impute_by_ordered_fill_forward,
             reference_column=demographic_column,
             column_identity="participant_id",
@@ -21,6 +24,7 @@ def impute_key_demographics(df):
         )
 
     df = impute_and_flag(
+        df,
         imputation_function=impute_by_mode,
         reference_column="white",
         group_by_column="ons_household_id",
@@ -28,12 +32,13 @@ def impute_key_demographics(df):
     # Todo: Add call to impute white by donor-based imputation
 
     df = impute_and_flag(
+        df,
         imputation_function=impute_by_distribution,
         reference_column="sex",
         group_by_columns=["white", "gor9d"],
         first_imputation_value="Female",
         second_imputation_value="Male",
     )
-    # Todo: Add call to impute date_of_birth using donor imputation
+    # Todo: Add call to impute date_of_birth using donor-based imputation
 
     return df
