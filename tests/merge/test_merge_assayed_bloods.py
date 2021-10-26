ef76cfe4298145e10da67199e34820e0a065aac2
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-10-26 10:01:46 +0100

    test

--- tests/merge/test_merge_assayed_bloods.py
+++ tests/merge/test_merge_assayed_bloods.py
@@ -6,34 +6,21 @@ from cishouseholds.merge import merge_assayed_bloods
 def test_merge_assayed_bloods(spark_session):
     input_df = spark_session.createDataFrame(
         data=[
-            ("S", "ONS00000002", 1, 1, None, None, None, None),
-            ("N", "ONS00000003", None, None, None, 1, None, 1),
-            ("N", "ONS00000003", None, None, None, 1, None, 1),
-            ("S", "ONS00000004", None, None, None, 1, 1, 1),
-            ("S", "ONS00000004", None, None, None, 1, 1, 1),  # check failed flags cannot apply to dropped columns
-            ("S", "ONS00000005", 1, None, 1, None, None, None),
-            ("N", "ONS00000005", 1, None, 1, None, None, None),
-            ("S", "ONS00000005", 1, None, 1, None, None, None),  # check correct failed flags detected
-            ("N", "ONS00000006", None, None, None, None, None, None),
-            ("N", "ONS00000006", None, None, None, None, None, None),  # check a match has been applied
-            ("S", "ONS00000007", 1, None, 1, 1, None, 1),  # check cannot flag as more than 1 type
+            ("S", "ONS00000002", 1, 111, 1, 2, 2, 2),  # singular
+            ("N", "ONS00000003", 2, 112, 2, 1, 1, 1),  # match N and S
+            ("S", "ONS00000003", 2, 112, 1, 2, 2, 1),
+            ("S", "ONS00000004", 4, 113, 2, 1, 1, 1),
+            ("N", "ONS00000004", 4, 114, 2, 1, 1, 1),  # mismatched well_id
         ],
         schema="""blood_group string, blood_sample_barcode string, antibody_test_plate_number integer,
                 antibody_test_well_id integer, col1 integer, col2 integer, col3 integer, col4 integer""",
     )
     expected_df = spark_session.createDataFrame(
         data=[
-            ("ONS00000005", 1, None, "S", 1, None, None, None, None, None, None, None, None),
-            ("ONS00000005", 1, None, "S", 1, None, None, None, None, None, None, None, None),
-            ("ONS00000005", 1, None, None, None, None, None, None, "N", 1, None, None, None),
-            ("ONS00000003", None, None, None, None, None, None, None, "N", None, 1, None, 1),
-            ("ONS00000003", None, None, None, None, None, None, None, "N", None, 1, None, 1),
-            ("ONS00000004", None, None, "S", None, 1, 1, 1, None, None, None, None, None),
-            ("ONS00000004", None, None, "S", None, 1, 1, 1, None, None, None, None, None),
-            ("ONS00000006", None, None, None, None, None, None, None, "N", None, None, None, None),
-            ("ONS00000006", None, None, None, None, None, None, None, "N", None, None, None, None),
-            ("ONS00000007", 1, None, "S", 1, 1, None, 1, None, None, None, None, None),
-            ("ONS00000002", 1, 1, "S", None, None, None, None, None, None, None, None, None),
+            ("ONS00000004", 4, 114, None, None, None, None, None, "N", 2, 1, 1, 1),
+            ("ONS00000002", 1, 111, "S", 1, 2, 2, 2, None, None, None, None, None),
+            ("ONS00000004", 4, 113, "S", 2, 1, 1, 1, None, None, None, None, None),
+            ("ONS00000003", 2, 112, "S", 1, 2, 2, 1, "N", 2, 1, 1, 1),
         ],
         schema="""blood_sample_barcode string ,antibody_test_plate_number integer,antibody_test_well_id integer,
         blood_group_s string,col1_s integer,col2_s integer,col3_s integer,col4_s integer,
@@ -41,5 +28,3 @@ def test_merge_assayed_bloods(spark_session):
     )
     output_df = merge_assayed_bloods(input_df, "blood_group")
     assert_df_equality(expected_df, output_df)
-
-    # comment
