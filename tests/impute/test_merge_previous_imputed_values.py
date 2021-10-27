6e33256bf8bb03a230101786bb6de7fca71aa358
Rowan Hemsi <rowan.hemsi@ons.gov.uk>
2021-10-27 16:53:16 +0100

    WIP: Add failing test data

--- tests/impute/test_merge_previous_imputed_values.py
+++ tests/impute/test_merge_previous_imputed_values.py
@@ -6,11 +6,15 @@ from cishouseholds.impute import merge_previous_imputed_values
 def test_merge_previous_imputed_values(spark_session):
     schema = "id integer, column integer, column_is_imputed integer, column_imputation_method string"
 
-    input_data = [(0, 42, 1, "imputation_method_1"), (1, None, 0, None)]
+    input_data = [(0, 42, 1, "imputation_method_1"), (1, None, 0, None), (2, 365, 0, None)]
 
-    lookup_data = [(0, 101, 1, "inputation_method_2"), (1, 500, 1, "imputation_method_3")]
+    lookup_data = [
+        (0, 101, 1, "inputation_method_2"),
+        (1, 500, 1, "imputation_method_3"),
+        (2, 500, 1, "imputation_method_1"),
+    ]
 
-    expected_data = [(0, 42, 1, "imputation_method_1"), (1, 500, 1, "imputation_method_3")]
+    expected_data = [(0, 42, 1, "imputation_method_1"), (1, 500, 1, "imputation_method_3"), (2, 365, 0, None)]
 
     input_df = spark_session.createDataFrame(input_data, schema=schema)
     lookup_df = spark_session.createDataFrame(lookup_data, schema=schema)
