d79befeb87de7ee0a7501e7aa8938d9cf2878bfa
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-10-28 09:24:55 +0100

    refactored ETL

--- tests/pipeline/test_survey_responses_version_2.py
+++ tests/pipeline/test_survey_responses_version_2.py
@@ -2,9 +2,11 @@ import pandas as pd
 import pytest
 from mimesis.schema import Schema
 
-from cishouseholds.pipeline.survey_responses_version_2_ETL import (
-    extract_validate_transform_survey_responses_version_2_delta,
-)
+from cishouseholds.pipeline.ETL_scripts import extract_validate_transform_input_data
+from cishouseholds.pipeline.input_variable_names import survey_responses_v2_variable_name_map
+from cishouseholds.pipeline.survey_responses_version_2_ETL import transform_survey_responses_version_2_delta
+from cishouseholds.pipeline.timestamp_map import survey_responses_v2_datetime_map
+from cishouseholds.pipeline.validation_schema import survey_responses_v2_validation_schema
 from dummy_data_generation.schemas import get_voyager_2_data_description
 
 
@@ -16,7 +18,13 @@ def responses_v2_survey_ETL_output(mimesis_field, pandas_df_to_temporary_csv):
     schema = Schema(schema=get_voyager_2_data_description(mimesis_field, ["ONS00000000"], ["ONS00000000"]))
     pandas_df = pd.DataFrame(schema.create(iterations=10))
     csv_file_path = pandas_df_to_temporary_csv(pandas_df, sep="|")
-    processed_df = extract_validate_transform_survey_responses_version_2_delta(csv_file_path.as_posix())
+    processed_df = extract_validate_transform_input_data(
+        csv_file_path.as_posix(),
+        survey_responses_v2_variable_name_map,
+        survey_responses_v2_datetime_map,
+        survey_responses_v2_validation_schema,
+        transform_survey_responses_version_2_delta,
+    )
     return processed_df
 
 
