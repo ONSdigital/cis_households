d5e76117e1900b97273d2d19556b6969469f1911
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-11-01 12:17:43 +0000

    refactored test

--- tests/edit/test_update_from_csv_lookup.py
+++ tests/edit/test_update_from_csv_lookup.py
@@ -6,23 +6,35 @@ from cishouseholds.edit import update_from_csv_lookup
 def test_update_from_csv_lookup(spark_session, pandas_df_to_temporary_csv):
     map_df = spark_session.createDataFrame(
         data=[
-            (1, "B", "val1", "vala"),
-            (2, "A", "val3", "valz"),
-            (2, "B", "val2", "valb"),
-            (3, "B", "val3", "valc"),
-            (4, "B", "val4", "vald"),
-            (5, "A", "val5", "vale"),
-            (6, "A", "val6", "velf"),
+            (1, "B", "original", "converted"),
+            (2, "A", "original", "converted"),
+            (2, "B", "original", "converted"),
+            (3, "B", "original", "converted"),
+            (4, "B", "original", "converted"),
+            (5, "A", "something else", "converted"),
+            (6, "A", "original", "converted"),
         ],
         schema="""id integer, column string, old string, new string""",
     )
     csv_filepath = pandas_df_to_temporary_csv(map_df.toPandas()).as_posix()
     input_df = spark_session.createDataFrame(
-        data=[(1, 1, "val1"), (2, "val3", "val2"), (3, 1, "val3"), (4, "val4", "val4"), (5, 3, "val5")],
+        data=[
+            (1, "original", "original"),
+            (2, "original", "original"),
+            (3, "original", "original"),
+            (4, "original", "original"),
+            (5, "original", "original"),
+        ],
         schema="""id integer, A string, B string""",
     )
     expected_df = spark_session.createDataFrame(
-        data=[(1, 1, "vala"), (2, "valz", "valb"), (3, 1, "valc"), (4, "val4", "vald"), (5, 3, "val5")],
+        data=[
+            (1, "original", "converted"),
+            (2, "converted", "converted"),
+            (3, "original", "converted"),
+            (4, "original", "converted"),
+            (5, "original", "original"),
+        ],
         schema="""id integer, A string, B string""",
     )
     df = input_df
