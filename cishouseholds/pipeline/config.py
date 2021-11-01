14b1757ec52e3a143f6748132085ba16c1695c8f
fosted1 <David.Foster@ons.gov.uk>
2021-11-01 16:37:46 +0000

    Refactor to use default config when not provided

--- cishouseholds/pipeline/config.py
+++ cishouseholds/pipeline/config.py
@@ -7,9 +7,14 @@ import yaml
 @functools.lru_cache(maxsize=1)
 def get_config() -> dict:
     """Read YAML config file from path specified in PIPELINE_CONFIG_LOCATION environment variable"""
-    _config_location = os.environ.get("PIPELINE_CONFIG_LOCATION")
-    if _config_location is None:
-        raise ValueError("PIPELINE_CONFIG_LOCATION environment variable must be set to the config file path")
-    with open(_config_location) as fh:
-        config = yaml.load(fh, Loader=yaml.FullLoader)
+    config_location = os.environ.get("PIPELINE_CONFIG_LOCATION")
+    config = {}
+    if config_location is None:
+        print(
+            "PIPELINE_CONFIG_LOCATION environment variable should be set to "
+            "the config file path. An empty dictionary will be used by default for this run."
+        )
+    else:
+        with open(config_location) as fh:
+            config = yaml.load(fh, Loader=yaml.FullLoader)
     return config
