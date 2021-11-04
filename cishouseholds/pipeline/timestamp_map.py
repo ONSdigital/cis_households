f5e650a93a21b8678da25626dab145174ff07c0d
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-11-04 10:11:10 +0000

    refactored datetime map

--- cishouseholds/pipeline/timestamp_map.py
+++ cishouseholds/pipeline/timestamp_map.py
@@ -23,5 +23,9 @@ blood_datetime_map = {
 
 historic_blood_datetime_map = {
     "yyyy-MM-dd HH:mm:ss": ["blood_sample_arrayed_date"],
-    "yyyy-MM-dd": ["blood_sample_received_date", "antibody_test_result_recorded_date"],
+    "yyyy-MM-dd": [
+        "blood_sample_received_date",
+        "antibody_test_result_recorded_date",
+        "blood_sample_collected_datetime",
+    ],
 }
