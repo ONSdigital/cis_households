66edd7392c024be6c599533f2c316518b67695c2
fosted1 <David.Foster@ons.gov.uk>
2021-11-08 12:47:34 +0000

    Fix v1 processing stage

--- cishouseholds/pipeline/survey_responses_version_1_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_1_ETL.py
@@ -1,7 +1,7 @@
 from pyspark.sql import DataFrame
 
 from cishouseholds.derive import assign_filename_column
-from cishouseholds.extract import get_files_not_processed
+from cishouseholds.extract import get_files_to_be_processed
 from cishouseholds.pipeline.ETL_scripts import extract_validate_transform_input_data
 from cishouseholds.pipeline.input_variable_names import survey_responses_v1_variable_name_map
 from cishouseholds.pipeline.load import update_table_and_log_source_files
@@ -15,7 +15,7 @@ def survey_responses_version_1_ETL(**kwargs):
     """
     End to end processing of a IQVIA survey responses CSV file.
     """
-    file_path_list = get_files_not_processed(**kwargs)
+    file_path_list = get_files_to_be_processed(**kwargs)
     if file_path_list:
         df = extract_validate_transform_input_data(
             file_path_list,
