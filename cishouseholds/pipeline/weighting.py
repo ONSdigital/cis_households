2773cd096a7669781084cc1083ecee617c4c256e
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-11-18 12:30:41 +0000

    pre

---
+++ cishouseholds/pipeline/weighting.py
@@ -0,0 +1,14 @@
+from cishouseholds.pyspark_utils import get_or_create_spark_session
+from cishouseholds.extract import read_csv_to_pyspark_df
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as F
+
+def load_auxillary_data():
+    spark_session = get_or_create_spark_session()
+    resource_paths = {}
+    auxillary_dfs = {}
+    for name, resource_path in resource_paths.items():
+        auxillary_dfs[name] = read_csv_to_pyspark_df(spark_session, resource_path,)
+
+def update_data(old_sample_df: DataFrame, new_sample_df: DataFrame):
+    
