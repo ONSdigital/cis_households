57bf7ca1a437ce79cc7080e6f66740eb11fe3e4c
Ohi Bokth <bokth89@gmail.com>
2021-12-01 14:53:28 +0000

    added 1178 and 1179 until A3

--- cishouseholds/dweights_1167.py
+++ cishouseholds/dweights_1167.py
@@ -1,7 +1,12 @@
+from typing import List
 from typing import Union
 
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+from cishouseholds.derive import assign_named_buckets
+from cishouseholds.pipeline.load import extract_from_table
 
 
 # 1167
@@ -75,9 +80,8 @@ def raw_dweight_for_AB_scenario_for_antibody(
         raw_dweight_antibodies_column,
         F.when(F.col(hh_dweight_antibodies_column).isNotNull(), F.col(hh_dweight_antibodies_column)),
     )
-
     df = df.withColumn(
-        raw_dweight_antibodies_column + "_b",
+        raw_dweight_antibodies_column + "_b",  # TODO: should this be AB
         F.when(
             (F.col(sample_new_previous_column) == "previous") & (F.col(hh_dweight_antibodies_column).isNotNull()),
             F.col(hh_dweight_antibodies_column),
@@ -202,4 +206,104 @@ def function_name_1171(
 
     # TODO part 4: check if they are the same across cis_area_code_20 by sample groups (by sample_source)
 
+    return df
+
+
+# 1178
+def function_name_1178(
+    hh_samples_df: DataFrame,
+    df_extract_by_country: DataFrame,
+    table_name: str,
+    required_extracts_column_list: List[str],
+    mandatory_extracts_column_list: List[str],
+    population_join_column: str,
+) -> DataFrame:
+    """
+    Parameters
+    ----------
+    hh_samples_df
+    df_extract_by_country
+    table_name
+    required_extracts_column_list
+    mandatory_extracts_column_list
+    population_join_column: Only swab/antibody
+    """
+    # STEP 1 - create: extract_dataset as per requirements
+    # (i.e extract dataset for calibration from survey_data dataset (individual level))
+    df = extract_from_table(table_name).select(*required_extracts_column_list)
+
+    # STEP 2 -  check there are no missing values
+    for var in mandatory_extracts_column_list:
+        df = df.withColumn(
+            "check_if_missing", F.when(F.col(var).isNull(), 1)
+        )  # TODO: check if multiple columns are needed
+
+    # STEP 3 -  create household level of the extract to be used in calculation of non response factor
+    df = (
+        df.withColumn("response_indicator", F.when(F.col("participant_id").isNotNull(), 1).otherwise(None))
+        .drop("participant_id")
+        .dropDuplicates("ons_household_id")
+    )
+
+    # STEP 4 - merge hh_samples_df (36 processing step) and household level extract (69 processing step)
+    # TODO: check if population_by_country comes by country separated or together
+    hh_samples_df = hh_samples_df.join(
+        df=df.select(*required_extracts_column_list, "check_if_missing"), on="ons_household_id", how="left"
+    )
+
+    hh_samples_df = hh_samples_df.join(
+        df=df_extract_by_country, on="population_country_{population_join_column}", how="left"
+    )
+
+    return df
+
+
+# 1178 needed filtering function
+def dataset_generation(df, logic):
+    return df.where(logic)
+
+
+# 1179
+def function1179_1(df):
+    """
+    Parameters
+    ----------
+    df
+    """
+    # A.1 group households  considering  values to index of multiple deprivation
+    map_index_multiple_deprivation_group_country = {
+        "england": {0: 1, 6569: 2, 13138: 3, 19707: 4, 26276: 5},
+        "wales": {0: 1, 764: 2, 1146: 3, 1528: 4, 1529: 5},
+        "scotland": {0: 1, 2790: 2, 4185: 3, 5580: 4, 5581: 5},
+        "northen_ireland": {0: 1, 356: 2, 534: 3, 712: 4, 713: 5},
+    }
+
+    for country in map_index_multiple_deprivation_group_country.keys():
+        df = assign_named_buckets(
+            df=df,
+            reference_column="index_multiple_deprivation",
+            column_name_to_assign="index_multiple_deprivation_group",
+            map=map_index_multiple_deprivation_group_country[country],
+        )
+
+    return df
+
+
+def function1179_2(df: DataFrame, country_column: str) -> DataFrame:
+    """
+    Parameters
+    ----------
+    df
+    country_column: For northen_ireland, use country, otherwise use cis_area_code_20
+    """
+    window_list = "sample_addressbase_indicator", country_column, "index_multiple_deprivation_group"
+
+    w = Window.partitionBy(window_list)
+
+    df = df.withColumn("total_sampled_households_cis_imd_addressbase", F.count(F.col("ons_household_id")).over(w))
+
+    df = df.withColumn(
+        "total_responded_households_cis_imd_addressbase",
+        F.when(F.col("interim_participant_id") == 1, F.count(F.col("ons_household_id"))).over(w),
+    )
     return df
