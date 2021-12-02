c78047c2445419f60ca64a072aa00c410e61d9c8
Ohi Bokth <bokth89@gmail.com>
2021-12-02 00:26:12 +0000

    added 1180

--- cishouseholds/dweights_1167.py
+++ cishouseholds/dweights_1167.py
@@ -3,8 +3,10 @@ from typing import Union
 
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
+from pyspark.sql import SparkSession as spark_session
 from pyspark.sql.window import Window
 
+from cishouseholds.derive import assign_from_lookup
 from cishouseholds.derive import assign_named_buckets
 from cishouseholds.pipeline.load import extract_from_table
 
@@ -99,7 +101,7 @@ def raw_dweight_for_AB_scenario_for_antibody(
 
 
 # 1169
-def function(
+def function_name_1169(
     df: DataFrame,
     sample_new_previous: str,
     tranche_eligible_hh: str,
@@ -289,16 +291,17 @@ def function1179_1(df):
     return df
 
 
-def function1179_2(df: DataFrame, country_column: str) -> DataFrame:
+# 1179
+def assign_total_sampled_households_cis_imd_addressbase(df: DataFrame, country_column: str) -> DataFrame:
     """
     Parameters
     ----------
     df
     country_column: For northen_ireland, use country, otherwise use cis_area_code_20
     """
-    window_list = "sample_addressbase_indicator", country_column, "index_multiple_deprivation_group"
+    window_list = ["sample_addressbase_indicator", country_column, "index_multiple_deprivation_group"]
 
-    w = Window.partitionBy(window_list)
+    w = Window.partitionBy(*window_list)
 
     df = df.withColumn("total_sampled_households_cis_imd_addressbase", F.count(F.col("ons_household_id")).over(w))
 
@@ -306,4 +309,271 @@ def function1179_2(df: DataFrame, country_column: str) -> DataFrame:
         "total_responded_households_cis_imd_addressbase",
         F.when(F.col("interim_participant_id") == 1, F.count(F.col("ons_household_id"))).over(w),
     )
+
+    # B.1 calculate raw non_response_factor by dividing total respondent household
+    # to total sampled households (calculated above A.2, A.3)
+    df = df.withColumn(
+        "raw_non_response_factor",
+        (
+            F.col("total_sampled_households_cis_imd_addressbase")
+            / F.col("total_responded_households_cis_imd_addressbase")
+        ),
+    )
+    return df
+
+
+def function1179_3(df: DataFrame, country_list: List[str], test_type: str) -> DataFrame:
+    """
+    Parameters
+    ----------
+    """
+    # B.2 calculate scaled non_response_factor by dividing raw_non_response_factor
+    # to the mean of raw_non_response_factor
+    w_country = Window.partitionBy(country_list)
+
+    raw_non_response_factor_mean = df.select(F.mean(F.col("raw_non_response_factor"))).collect()[0][0]
+
+    df = df.withColumn(
+        "mean_raw_non_response_factor",
+        (F.col("raw_non_response_factor") / raw_non_response_factor_mean).over(w_country),
+    )
+
+    df = df.withColumn("scaled_non_response_factor", F.col("raw_non_response_factor") / raw_non_response_factor_mean)
+
+    # B.3 calculate bounded non_response_factor by adjusting the values of the scaled
+    # non_response factor to be contained in a certain, pre-defined range
+    df = df.withColumn(
+        "bounded_non_response_factor",
+        F.when(F.col("scaled_non_response_factor") < 0.5, 0.6).when(F.col("scaled_non_response_factor") > 2.0, 1.8),
+    )  # TODO: is this in a country window ?
+
+    # C1. adjust design weights by the non_response_factor by multiplying the desing
+    # weights with the non_response factor
+    df = df.withColumn(
+        "household_level_designweight_adjusted_swab",
+        F.when(
+            # TODO: when  data extracted is a swab or longcovid type of dataset
+            True & F.col("response_indicator") == 1,
+            F.col("household_level_designweight_swab") * F.col("bounded_non_response_factor"),
+        ).over(w_country),
+    )
+
+    df = df.withColumn(
+        "household_level_designweight_adjusted_antibodies",
+        F.when(
+            # TODO: when  data extracted is a antibodies type of dataset
+            True & F.col("response_indicator") == 1,
+            F.col("household_level_designweight_antibodies") * F.col("bounded_non_response_factor"),
+        ).over(w_country),
+    )
+
+    # D.1 calculate the scaling factor and then apply the scaling factor the
+    # design weights adjusted by the non response factor.
+    # If there is a swab or long covid type dataset, then use the population
+    # totlas for swab and the naming will have "swab" incorporated.
+    # Same for antibodies type dataset
+
+    # sum_adjusted_design_weight_swab or antibodies
+    df = df.withColumn(
+        "sum_adjusted_design_weight_" + test_type,
+        F.when(F.col("response_indicator") == 1, F.col("household_level_designweight_adjusted_" + test_type)).over(
+            w_country
+        ),
+    )
+
+    # scaling_factor_adjusted_design_weight_swab or antibodies
+    sum_adjusted_design_weight = df.select(F.sum(F.col("adjusted_design_weight_" + test_type))).collect()[0][0]
+
+    df = df.withColumn(
+        "scaling_factor_adjusted_design_weight_" + test_type,
+        F.when(
+            F.col("response_indicator") == 1, (F.col("population_country_" + test_type) / sum_adjusted_design_weight)
+        ).over(w_country),
+    )
+
+    # scaled_design_weight_adjusted_swab or antibodies
+    df = df.withColumn(
+        "scaled_design_weight_adjusted_" + test_type,
+        F.when(
+            F.col("response_indicator") == 1,
+            (
+                F.col("scaling_factor_adjusted_design_weight_" + test_type)
+                * F.col("household_level_designweight_adjusted_" + test_type)
+            ),
+        ).over(w_country),
+    )
+    return df
+
+
+def precalibration_checkpoints(df, test_type, population_totals, dweight_list):
+    """
+    Parameters
+    ----------
+    df
+    test_type
+    population_totals
+    dweight_list
+    """
+    # check_1: The design weights are adding up to total population
+    check_1 = df.select(F.sum(F.col("scaled_design_weight_adjusted_") + test_type)).collect()[0][0] == population_totals
+
+    # check_2 and check_3: The  design weights are all are positive AND check there are no missing design weights
+    for dweight in dweight_list:
+        df = df.withColumn("not_positive_or_null", F.when((F.col(dweight) < 0) | (F.col(dweight).isNull()), 1))
+    check_2_3 = [1] in df.select("not_positive_or_null").distinct().collect()[0]
+
+    # check_4: if they are the same across cis_area_code_20 by sample groups (by sample_source)
+    check_4 = True
+
+    return check_1, check_2_3, check_4
+
+
+# 1180
+def function_1180(df):
+    """
+    Parameters
+    ----------
+    df
+    """
+    # A.1 re-code the region_code values, by replacing the alphanumeric code with numbers from 1 to 12
+    region_code_lookup_df = spark_session.createDataFrame(
+        data=[
+            ("E12000001", 1),
+            ("E12000002", 2),
+            ("E12000003", 3),
+            ("E12000004", 4),
+            ("E12000005", 5),
+            ("E12000006", 6),
+            ("E12000007", 7),
+            ("E12000008", 8),
+            ("E12000009", 9),
+            ("W99999999", 10),
+            ("S99999999", 11),
+            ("N99999999", 12),
+        ],
+        schema="reference_1 string, outcome integer",
+    )
+    df = assign_from_lookup(
+        df=df,
+        column_name_to_assign="interim_region_code",
+        reference_columns=["reference_1"],
+        lookup_df=region_code_lookup_df,
+    )
+
+    # A.2 re-code sex variable replacing string with integers
+    sex_code_lookup_df = spark_session.createDataFrame(
+        data=[
+            ("male", 1),
+            ("female", 2),
+        ],
+        schema="reference_1 string, outcome integer",
+    )
+    df = assign_from_lookup(
+        df=df,
+        column_name_to_assign="interim_sex",
+        reference_columns=["reference_1"],
+        lookup_df=sex_code_lookup_df,
+    )
+
+    # A.3 create age groups considering certain age boundaries,
+    # as needed for calibration weighting of swab data
+    map_age_at_visit = {2: 1, 12: 2, 17: 3, 25: 4, 35: 5, 50: 6, 70: 7}
+
+    df = assign_named_buckets(
+        df=df,
+        reference_column="age_at_visit",
+        column_name_to_assign="age_group_swab",
+        map=map_age_at_visit,
+    )
+
+    # A.4 create age groups considering certain age boundaries,
+    # needed for calibration weighting of antibodies data
+    df = assign_named_buckets(
+        df=df,
+        reference_column="age_at_visit",
+        column_name_to_assign="age_group_antibodies",
+        map=map_age_at_visit,
+    )
+
+    # A.5 Creating first partition(p1)/calibration variable
+    return df
+
+
+# 1180
+def function_1180_B(df, dataset, dataset_type):
+    """
+    Parameters
+    ----------
+    df
+    """
+    df = df.withColumn(
+        "p1_swab_longcovid_england",
+        (F.when(F.col("country_name") == "england")) & (dataset_type),
+        ((F.col("interim_region_code") - 1) * 14 + (F.col("interim_sex") - 1) * 7 + F.col("age_group_swab")),
+    )
+
+    df = df.withColumn(
+        "p1_swab_longcovid_wales_scot_ni",
+        (
+            (F.when(F.col("country_name") == "wales"))
+            | (F.when(F.col("country_name") == "scotland"))
+            | (F.when(F.col("country_name") == "northern_ireland"))  # TODO: double-check name
+        )
+        & (dataset_type),
+        ((F.col("interim_sex") - 1) * 7 + F.col("age_group_swab")),
+    )
+
+    df = df.withColumn(
+        "p1_for_antibodies_evernever_engl",
+        (F.when(F.col("country_name") == "england")) & (dataset_type),
+        ((F.col("interim_region_code") - 1) * 10 + (F.col("interim_sex") - 1) * 5 + F.col("age_group_antibodies")),
+    )
+
+    df = df.withColumn(
+        "p1_for_antibodies_28daysto_engl",
+        (F.when(F.col("country_name") == "england")) & (dataset_type),
+        ((F.col("interim_sex") - 1) * 5 + F.col("age_group_antibodies")),
+    )
+
+    df = df.withColumn(
+        "p1_for_antibodies_wales_scot_ni",
+        (
+            (F.when(F.col("country_name") == "wales"))
+            | (F.when(F.col("country_name") == "scotland"))
+            | (F.when(F.col("country_name") == "northern_ireland"))  # TODO: double-check name
+        )
+        & (dataset_type),
+        ((F.col("interim_sex") - 1) * 5 + F.col("age_group_antibodies")),
+    )
+
+    # A.6 Create second partition (p2)/calibration variable
+    df = df.withColumn(
+        "p2_for_antibodies",
+        ((F.when(F.col("country_name") == "wales")) | (F.when(F.col("country_name") == "england"))) & (dataset_type),
+        (F.col("ethnicity_white") + 1),
+    )
+
+    df = df.withColumn(
+        "p3_for_antibodies_28daysto_engl",
+        (F.when(F.col("country_name") == "england")) & (dataset_type) & (F.col("age_at_visit") >= 16),
+        F.col("interim_region_code"),
+    )
+    return df
+
+
+# 1180
+def function_1180_C(df, dataset, dataset_type):
+    """
+    Parameters
+    ----------
+    df
+    """
+    if dataset_type in ["swab_evernever", "swab_14_days", "long_covid_28days", "long_covid_42days"]:
+        df = df.select(
+            "country_name", "participant_id", "scaled_design_weight_adjusted_swab", "p1_swab_longcovid_england"
+        )
+
+    # TODO: create the following datasets england_swab_evernever,
+    # england_swab_14days, england_long_covid_24days, england_long_covid_42days
+
     return df
