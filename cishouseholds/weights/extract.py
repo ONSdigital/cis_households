b650dbbded11f3d2d43a0dd55d128317d46e5363
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-12-06 10:16:41 +0000

    none

--- cishouseholds/weights/extract.py
+++ cishouseholds/weights/extract.py
@@ -133,6 +133,59 @@ aps_value_map = {
 }
 # fmt: on
 
+def read_csv_to_pyspark_df(
+    spark_session: SparkSession,
+    csv_file_path: Union[str, list],
+    expected_raw_header_row: str,
+    schema: StructType,
+    sep: str = ",",
+    **kwargs,
+) -> DataFrame:
+    """
+    Validate and read a csv file into a PySpark DataFrame.
+
+    Parameters
+    ----------
+    csv_file_path
+        file to read to dataframe
+    expected_raw_header_row
+        expected first line of file
+    schema
+        schema to use for returned dataframe, including desired column names
+
+    Takes keyword arguments from ``pyspark.sql.DataFrameReader.csv``,
+    for example ``timestampFormat="yyyy-MM-dd HH:mm:ss 'UTC'"``.
+    """
+    spark_session = get_or_create_spark_session()
+    if not isinstance(csv_file_path, list):
+        csv_file_path = [csv_file_path]
+
+    for csv_file in csv_file_path:
+        text_file = spark_session.sparkContext.textFile(csv_file)
+        csv_header = validate_csv_header(text_file, expected_raw_header_row)
+        csv_fields = validate_csv_fields(text_file, delimiter=sep)
+
+        if not csv_header:
+            raise InvalidFileError(
+                f"Header of {csv_file} ({text_file.first()}) "
+                f"does not match expected header: {expected_raw_header_row}"
+            )
+
+        if not csv_fields:
+            raise InvalidFileError(
+                f"Number of fields in {csv_file} does not match expected number of columns from header"
+            )
+
+    return spark_session.read.csv(
+        csv_file_path,
+        header=True,
+        schema=schema,
+        ignoreLeadingWhiteSpace=True,
+        ignoreTrailingWhiteSpace=True,
+        sep=sep,
+        **kwargs,
+    )
+
 
 def load_auxillary_data(specify=[]):
     """
