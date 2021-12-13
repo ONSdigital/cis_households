f1885b74c9f0e9b909d36471971273b4f1d534ff
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2021-12-13 16:06:11 +0000

    none

--- tests/derive/test_assign_random_day_in_month.py
+++ tests/derive/test_assign_random_day_in_month.py
@@ -1,21 +1,20 @@
 from chispa import assert_df_equality
-
+import random
 from cishouseholds.derive import assign_random_day_in_month
 
 
 def test_assign_random_day_in_month(spark_session):
+    data = []
+    n = 50
+    for i in range(0,n):
+        data.append((random.randint(2018,2024), random.randint(1,12)))
     expected_df = spark_session.createDataFrame(
-        data=[
-            (2019, 6),
-            (2020, 8),
-            (2020, 4),
-        ],
+        data=data,
         schema="year integer, month integer",
     )
     output_df = assign_random_day_in_month(
         df=expected_df.drop("result"), column_name_to_assign="result", year_column="year", month_column="month"
     )
-    types = {}
-    for col, type in output_df.dtypes:
-        types[col] = type
-    assert types["result"] == "timestamp"
+    output_df.toPandas().to_csv("test.csv",index=False)
+    print(output_df.schema["result"].dataType)
+    assert output_df.schema["result"].dataType == "TimestampType"
