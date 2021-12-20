bc5b5305e9ebd5ac5d2a577346ff27c1a6551367
David Foster <foster.dev999@gmail.com>
2021-12-20 16:44:32 +0000

    Fix table name reference in generate outputs stage

--- cishouseholds/pipeline/generate_outputs.py
+++ cishouseholds/pipeline/generate_outputs.py
@@ -26,7 +26,7 @@ def generate_outputs():
     output_directory = Path(get_config()["output_directory"]) / output_datetime
     # TODO: Check that output dir exists
 
-    linked_df = extract_from_table("merged_responses_antibody_swab_delta")
+    linked_df = extract_from_table("merged_responses_antibody_swab_data")
 
     #    all_visits_df = extract_from_table("response_level_records")
     #    participant_df = extract_from_table("participant_level_with_vaccination_data")
