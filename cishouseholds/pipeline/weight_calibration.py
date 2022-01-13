cde80ca13d35bae15f62deff712f7366ecc62996
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-01-13 13:39:16 +0000

    test

--- cishouseholds/pipeline/weight_calibration.py
+++ cishouseholds/pipeline/weight_calibration.py
@@ -1,9 +1,9 @@
-# import rpy2.robjects as robjects
+import rpy2.robjects as robjects
 import yaml
 from pyspark.sql import functions as F
-# from rpy2.robjects import pandas2ri
-# from rpy2.robjects.conversion import localconverter
-# from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+from rpy2.robjects.packages import importr
 
 from cishouseholds.pipeline.ETL_scripts import extract_input_data
 from cishouseholds.pipeline.load import extract_from_table
@@ -15,15 +15,15 @@ from cishouseholds.weights.population_projections import proccess_population_pro
 from cishouseholds.weights.pre_calibration import pre_calibration_high_level
 from cishouseholds.weights.weights import generate_weights
 
-# regenesees = importr(
-#     "ReGenesees",
-#     # Fix conflicting method names in R
-#     robject_translations={
-#         "dim.GVF.db": "dim_GVF_db_",
-#         "print.GVF.db": "print_GVF_db_",
-#         "str.GVF.db": "str_GVF_db_",
-#     },
-# )
+regenesees = importr(
+    "ReGenesees",
+    # Fix conflicting method names in R
+    robject_translations={
+        "dim.GVF.db": "dim_GVF_db_",
+        "print.GVF.db": "print_GVF_db_",
+        "str.GVF.db": "str_GVF_db_",
+    },
+)
 
 
 @register_pipeline_stage("sample_file_ETL")
@@ -104,142 +104,142 @@ def pre_calibration(
     update_table(df_for_calibration, responses_pre_calibration_table, mode_overide="overwrite")
 
 
-# @register_pipeline_stage("weight_calibration")
-# def weight_calibration(
-#     population_totals_table: str,
-#     responses_pre_calibration_table: str,
-#     base_output_table_name: str,
-#     calibration_config_path: str,
-# ):
-#     """
-#     Run weight calibration for multiple datasets, as specified by the stage configuration.
+@register_pipeline_stage("weight_calibration")
+def weight_calibration(
+    population_totals_table: str,
+    responses_pre_calibration_table: str,
+    base_output_table_name: str,
+    calibration_config_path: str,
+):
+    """
+    Run weight calibration for multiple datasets, as specified by the stage configuration.
 
-#     calibration_config_path
-#         path to YAML file containing a list of dictionaries with keys:
-#             dataset_name: swab_evernever
-#             country: string country name in lower case
-#             bounds: list of lists containing lower and upper bounds
-#             design_weight_column: string column name
-#             calibration_model_components: list of string column names
+    calibration_config_path
+        path to YAML file containing a list of dictionaries with keys:
+            dataset_name: swab_evernever
+            country: string country name in lower case
+            bounds: list of lists containing lower and upper bounds
+            design_weight_column: string column name
+            calibration_model_components: list of string column names
 
-#     """
-#     spark_session = get_or_create_spark_session()
+    """
+    spark_session = get_or_create_spark_session()
 
-#     with open(calibration_config_path, "r") as config_file:
-#         calibration_config = yaml.load(config_file, Loader=yaml.FullLoader)
-#     population_totals_df = extract_from_table(population_totals_table)
-#     full_response_level_df = extract_from_table(responses_pre_calibration_table)
+    with open(calibration_config_path, "r") as config_file:
+        calibration_config = yaml.load(config_file, Loader=yaml.FullLoader)
+    population_totals_df = extract_from_table(population_totals_table)
+    full_response_level_df = extract_from_table(responses_pre_calibration_table)
 
-#     for dataset_options in calibration_config:
-#         population_totals_subset = (
-#             population_totals_df.where(F.col("dataset_name") == dataset_options["dataset_name"])
-#             .drop("dataset_name")
-#             .toPandas()
-#             .transpose()
-#         )
-#         population_totals_subset = population_totals_subset.rename(columns=population_totals_subset.iloc[0]).drop(
-#             population_totals_subset.index[0]
-#         )
-#         assert population_totals_subset.shape[0] != 0, "Population totals subset is empty."
-#         population_totals_vector = robjects.vectors.FloatVector(population_totals_subset.iloc[0].tolist())
+    for dataset_options in calibration_config:
+        population_totals_subset = (
+            population_totals_df.where(F.col("dataset_name") == dataset_options["dataset_name"])
+            .drop("dataset_name")
+            .toPandas()
+            .transpose()
+        )
+        population_totals_subset = population_totals_subset.rename(columns=population_totals_subset.iloc[0]).drop(
+            population_totals_subset.index[0]
+        )
+        assert population_totals_subset.shape[0] != 0, "Population totals subset is empty."
+        population_totals_vector = robjects.vectors.FloatVector(population_totals_subset.iloc[0].tolist())
 
-#         columns_to_select = (
-#             ["participant_id", "country_name_12"]
-#             + [dataset_options["design_weight_column"]]
-#             + dataset_options["calibration_model_components"]
-#         )
-#         responses_subset_df = (
-#             full_response_level_df.select(columns_to_select)
-#             .where(
-#                 (F.col(dataset_options["subset_flag_column"]) == 1)
-#                 & (F.col("country_name_12") == dataset_options["country"])
-#             )
-#             .toPandas()
-#         )
-#         assert responses_subset_df.shape[0] != 0, "Responses subset is empty."
+        columns_to_select = (
+            ["participant_id", "country_name_12"]
+            + [dataset_options["design_weight_column"]]
+            + dataset_options["calibration_model_components"]
+        )
+        responses_subset_df = (
+            full_response_level_df.select(columns_to_select)
+            .where(
+                (F.col(dataset_options["subset_flag_column"]) == 1)
+                & (F.col("country_name_12") == dataset_options["country"])
+            )
+            .toPandas()
+        )
+        assert responses_subset_df.shape[0] != 0, "Responses subset is empty."
 
-#         with localconverter(robjects.default_converter + pandas2ri.converter):
-#             # population_totals_subset_r_df = robjects.conversion.py2rpy(population_totals_subset)
-#             responses_subset_df_r_df = robjects.conversion.py2rpy(responses_subset_df)
+        with localconverter(robjects.default_converter + pandas2ri.converter):
+            # population_totals_subset_r_df = robjects.conversion.py2rpy(population_totals_subset)
+            responses_subset_df_r_df = robjects.conversion.py2rpy(responses_subset_df)
 
-#         sample_design = regenesees.e_svydesign(
-#             dat=responses_subset_df_r_df,
-#             ids=robjects.Formula("~participant_id"),
-#             weights=robjects.Formula(f"~{dataset_options['design_weight_column']}"),
-#         )
-#         for bounds in dataset_options["bounds"]:
-#             responses_subset_df_r_df = assign_calibration_weight(
-#                 responses_subset_df_r_df,
-#                 "calibration_weight",
-#                 population_totals_vector,
-#                 sample_design,
-#                 dataset_options["calibration_model_components"],
-#                 bounds,
-#             )
-#             responses_subset_df_r_df = assign_calibration_factors(
-#                 responses_subset_df_r_df,
-#                 "calibration_factors",
-#                 "calibration_weight",
-#                 dataset_options["design_weight_column"],
-#             )
+        sample_design = regenesees.e_svydesign(
+            dat=responses_subset_df_r_df,
+            ids=robjects.Formula("~participant_id"),
+            weights=robjects.Formula(f"~{dataset_options['design_weight_column']}"),
+        )
+        for bounds in dataset_options["bounds"]:
+            responses_subset_df_r_df = assign_calibration_weight(
+                responses_subset_df_r_df,
+                "calibration_weight",
+                population_totals_vector,
+                sample_design,
+                dataset_options["calibration_model_components"],
+                bounds,
+            )
+            responses_subset_df_r_df = assign_calibration_factors(
+                responses_subset_df_r_df,
+                "calibration_factors",
+                "calibration_weight",
+                dataset_options["design_weight_column"],
+            )
 
-#             with localconverter(robjects.default_converter + pandas2ri.converter):
-#                 calibrated_pandas_df = robjects.conversion.rpy2rp(responses_subset_df_r_df)
+            with localconverter(robjects.default_converter + pandas2ri.converter):
+                calibrated_pandas_df = robjects.conversion.rpy2rp(responses_subset_df_r_df)
 
-#             calibrated_df = spark_session.createDataFrame(calibrated_pandas_df)
-#             update_table(
-#                 calibrated_df,
-#                 base_output_table_name
-#                 + "_"
-#                 + dataset_options["dataset_name"]
-#                 + "_"
-#                 + dataset_options["country_name_12"]
-#                 + "_"
-#                 + bounds[0]
-#                 + "-"
-#                 + bounds[1],
-#                 mode_overide="overwrite",
-#             )
-
-
-# def convert_columns_to_r_factors(df: robjects.DataFrame, columns_to_covert: list) -> robjects.DataFrame:
-#     """
-#     Convert columns of an R dataframe to factors.
-#     Returns the dataframe, though changes are likely made in-place on the original dataframe.
-#     """
-#     for column in columns_to_covert:
-#         column_index = df.colnames.index(column)
-#         df[column_index] = robjects.vectors.FactorVector(df[column_index])
-#     return df
+            calibrated_df = spark_session.createDataFrame(calibrated_pandas_df)
+            update_table(
+                calibrated_df,
+                base_output_table_name
+                + "_"
+                + dataset_options["dataset_name"]
+                + "_"
+                + dataset_options["country_name_12"]
+                + "_"
+                + bounds[0]
+                + "-"
+                + bounds[1],
+                mode_overide="overwrite",
+            )
 
 
-# def assign_calibration_weight(
-#     df: robjects.DataFrame,
-#     column_name_to_assign: str,
-#     population_totals: robjects.DataFrame,
-#     sample_design,
-#     calibration_model_components: list,
-#     bounds: tuple,
-# ) -> robjects.DataFrame:
-#     """
-#     Assign calibration weights to the specified column.
-#     """
-#     df[column_name_to_assign] = regenesees.weights(
-#         regenesees.e_calibrate(
-#             sample_design,
-#             population_totals,
-#             calmodel=robjects.Formula("+".join(calibration_model_components)),
-#             calfun="linear",
-#             bounds=robjects.ListVector(bounds[0], bounds[1]),
-#             aggregate_stage=1,
-#         )
-#     )
-#     return df
+def convert_columns_to_r_factors(df: robjects.DataFrame, columns_to_covert: list) -> robjects.DataFrame:
+    """
+    Convert columns of an R dataframe to factors.
+    Returns the dataframe, though changes are likely made in-place on the original dataframe.
+    """
+    for column in columns_to_covert:
+        column_index = df.colnames.index(column)
+        df[column_index] = robjects.vectors.FactorVector(df[column_index])
+    return df
 
 
-# def assign_calibration_factors(
-#     df: robjects.DataFrame, column_name_to_assign: str, calibration_weight_column: str, design_weight_column: str
-# ) -> robjects.DataFrame:
-#     """Assign calibration factors to specified column."""
-#     df[column_name_to_assign] = df[calibration_weight_column] / df[design_weight_column]
-#     return df
+def assign_calibration_weight(
+    df: robjects.DataFrame,
+    column_name_to_assign: str,
+    population_totals: robjects.DataFrame,
+    sample_design,
+    calibration_model_components: list,
+    bounds: tuple,
+) -> robjects.DataFrame:
+    """
+    Assign calibration weights to the specified column.
+    """
+    df[column_name_to_assign] = regenesees.weights(
+        regenesees.e_calibrate(
+            sample_design,
+            population_totals,
+            calmodel=robjects.Formula("+".join(calibration_model_components)),
+            calfun="linear",
+            bounds=robjects.ListVector(bounds[0], bounds[1]),
+            aggregate_stage=1,
+        )
+    )
+    return df
+
+
+def assign_calibration_factors(
+    df: robjects.DataFrame, column_name_to_assign: str, calibration_weight_column: str, design_weight_column: str
+) -> robjects.DataFrame:
+    """Assign calibration factors to specified column."""
+    df[column_name_to_assign] = df[calibration_weight_column] / df[design_weight_column]
+    return df
