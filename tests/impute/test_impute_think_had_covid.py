618e486c9dcc4c801c716ebde0f48695ca928b14
David Foster <foster.dev999@gmail.com>
2022-01-18 12:35:20 +0000

    Add failing test example for carry forward

--- tests/impute/test_impute_think_had_covid.py
+++ tests/impute/test_impute_think_had_covid.py
@@ -7,6 +7,9 @@ from cishouseholds.impute import impute_think_had_covid
 def test_impute_think_had_covid(spark_session):
     input_df = spark_session.createDataFrame(
         data=[
+            # 0 - Check carry forward of first records date, type and contact when no contact reported on later visit
+            (0, "2020-01-01", "2020-01-01", "A", "Yes"),
+            (0, None, "2020-01-02", None, "No"),
             (1, "2020-03-12", "2020-12-20", "A", "Yes"),
             (1, None, "2020-11-20", "B", "Yes"),
             (1, "2020-02-02", "2020-01-20", "B", "Yes"),
@@ -22,16 +25,18 @@ def test_impute_think_had_covid(spark_session):
     )
     expected_df = spark_session.createDataFrame(
         data=[
+            (0, "2020-01-01", "2020-01-01", "A", "Yes"),
+            (0, "2020-01-01", "2020-01-02", "A", "Yes"),
+            (1, None, "2020-01-20", "B", "No"),
+            (1, "2020-03-12", "2020-11-20", "B", "Yes"),
+            (1, "2020-03-12", "2020-12-20", "A", "Yes"),
+            (2, "2020-04-05", "2020-11-20", "A", "Yes"),
+            (2, "2020-04-05", "2020-12-20", None, "Yes"),
+            (3, "2020-05-09", "2020-12-20", "B", "Yes"),
             (4, None, "2020-10-20", None, "No"),
             (4, "2020-11-09", "2020-11-20", None, "Yes"),
             (4, "2020-11-22", "2021-01-11", "B", "Yes"),
             (4, "2020-11-22", "2021-02-11", "A", "Yes"),
-            (1, None, "2020-01-20", "B", "No"),
-            (1, "2020-03-12", "2020-11-20", "B", "Yes"),
-            (1, "2020-03-12", "2020-12-20", "A", "Yes"),
-            (3, "2020-05-09", "2020-12-20", "B", "Yes"),
-            (2, "2020-04-05", "2020-11-20", "A", "Yes"),
-            (2, "2020-04-05", "2020-12-20", None, "Yes"),
         ],
         schema="id integer, date string, visit_date string, type string, contact string",
     )
