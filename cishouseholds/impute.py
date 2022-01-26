e83a03457ff323a6b169e988bf29c17c5e7f0b31
Stefen Lamothe <78305473+slamothe99@users.noreply.github.com>
2022-01-26 10:25:11 +0000

    added work column fill forward (#233)
    Co-authored-by: Ohi Bokth <65452503+ohibo@users.noreply.github.com>

--- cishouseholds/impute.py
+++ cishouseholds/impute.py
@@ -11,6 +11,25 @@ from pyspark.sql.types import DoubleType
 from pyspark.sql.window import Window
 
 
+def fill_forward_work_columns(
+    df: DataFrame,
+    fill_forward_columns: List[str],
+    participant_id_column: str,
+    visit_date_column: str,
+    main_job_changed_column: str,
+) -> DataFrame:
+    """ """
+    window = Window.partitionBy(participant_id_column).orderBy(visit_date_column)
+    for col in fill_forward_columns:
+        df = df.withColumn(
+            col,
+            F.when(
+                F.col(main_job_changed_column) != "Yes", F.first(F.col(col), ignorenulls=True).over(window)
+            ).otherwise(F.col(col)),
+        )
+    return df
+
+
 def impute_by_distribution(
     df: DataFrame,
     column_name_to_assign: str,
