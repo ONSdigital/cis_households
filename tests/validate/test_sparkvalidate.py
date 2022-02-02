41d7378a64878450118fcecb09de17583fa9fd07
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-02-02 16:47:53 +0000

    working validation

--- tests/validate/test_sparkvalidate.py
+++ tests/validate/test_sparkvalidate.py
@@ -13,14 +13,14 @@ def test_sparkvalidate(spark_session):
     df_expected = spark_session.createDataFrame(
         data=[
             # fmt: off
-                ('a',   1,  4,  'yes', [
+                (1,'a',   1,  4,  'yes', [
                     "column_4 the row is 'no'",
                     'column_3 should be in between lower_bound and upper_bound',
                     'col_2 and col_3 should_be_within_interval 4 and 10'
                     ]
                 ),
-                ('aa',  12, 9,  'no', ['larger_than_10']),
-                ('ab',	3,  10, 'yes', [
+                (2,'aa',  12, 9,  'no', ['larger_than_10']),
+                (3,'ab',	3,  10, 'yes', [
                     "column_4 the row is 'no'",
                     'column_3 should be in between lower_bound and upper_bound',
                     'col_2 and col_3 should_be_within_interval 4 and 10',
@@ -28,7 +28,7 @@ def test_sparkvalidate(spark_session):
                     'larger_than_10'
                     ]
                 ),
-                ('ab',	8,  10, 'yes', [
+                (4,'ab',	8,  10, 'yes', [
                     "column_4 the row is 'no'",
                     'column_3 should be in between lower_bound and upper_bound',
                     'col_2 and col_3 should_be_within_interval 4 and 10',
@@ -36,13 +36,13 @@ def test_sparkvalidate(spark_session):
                     'larger_than_10'
                     ]
                 ),
-                ('b',   2,  7,  'no', [
+                (5,'b',   2,  7,  'no', [
                     "column_1 should contain 'a'",
                     'column_3 should be in between lower_bound and upper_bound',
                     'col_2 and col_3 should_be_within_interval 4 and 10'
                     ]
                 ),
-                (None,   2,  7,  'no',   [
+                (6,None,   2,  7,  'no',   [
                     'column_3 should be in between lower_bound and upper_bound',
                     'col_2 and col_3 should_be_within_interval 4 and 10'
                     ]
@@ -51,6 +51,7 @@ def test_sparkvalidate(spark_session):
         ],
         schema=StructType(
             [
+                StructField("id", IntegerType(), True),
                 StructField("column_1", StringType(), True),
                 StructField("column_2", IntegerType(), True),
                 StructField("column_3", IntegerType(), True),
@@ -88,9 +89,6 @@ def test_sparkvalidate(spark_session):
         "test_function": {"column_1": "column_2", "column_2": "column_3"},
     }
     validate_df.validate(operations=operations)
-    d, f = validate_df.filter(return_failed=True, any=True, selected_errors=["column_1 should contain 'a'"])
-
-    validate_df.dataframe.toPandas().to_csv("out.csv", index=False)
     assert_df_equality(
         validate_df.dataframe, df_expected, ignore_row_order=True, ignore_column_order=True, ignore_nullable=True
     )
