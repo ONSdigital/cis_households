f4b396c1f7d793b31da53c47163d206259e3ea5a
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-02-03 15:25:37 +0000

    added duplication df

--- cishouseholds/pipeline/report.py
+++ cishouseholds/pipeline/report.py
@@ -2,6 +2,7 @@ import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
 
 from cishouseholds.pipeline.load import extract_from_table
+from cishouseholds.pyspark_utils import get_or_create_spark_session
 
 
 def report(
@@ -11,6 +12,7 @@ def report(
     error_column: str,
     processed_file_table: str,
     invalid_files_table: str,
+    duplicate_row_flag_column: str,
 ):
     processed_file_log = extract_from_table(processed_file_table)
     invalid_files_log = extract_from_table(invalid_files_table)
@@ -25,6 +27,22 @@ def report(
     invalid_df_errors = (
         invalid_df_errors.withColumn("ERROR LIST", F.explode(error_column)).groupBy("ERROR LIST").count()
     )
+
+    duplicated_df = valid_df.filter(duplicate_row_flag_column).union(valid_df.filter(duplicate_row_flag_column))
+
+    spark_session = get_or_create_spark_session()
+
+    counts_df = spark_session.createDataFrame(
+        data=[
+            ("processed_file_log", processed_file_count),
+            ("invalid_file_log", invalid_files_count),
+            ("valid_survey_responses", num_valid_survey_responses),
+            ("invalid_survey_responses", num_invalid_survey_responses),
+        ],
+        schema="dataset string, count integer",
+    )
+
+    return duplicated_df, counts_df, valid_df_errors, invalid_df_errors
 
 
 # report =SparkVal.report("unique_participant_response_id",processed_file_log,invalid_files_log)
