54fa6560df54fbba51bdac86f361ad482e40ef9c
David Foster <foster.dev999@gmail.com>
2022-02-04 17:48:18 +0000

    Fix dob parsing format

--- cishouseholds/pipeline/timestamp_map.py
+++ cishouseholds/pipeline/timestamp_map.py
@@ -12,7 +12,7 @@ survey_responses_datetime_map = {
         "outside_uk_last_date",
         "improved_visit_date",
     ],
-    "dd/MM/yy HH:mm": ["date_of_birth"],
+    "dd/MM/yyyy HH:mm": ["date_of_birth"],
     "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'": ["visit_datetime", "samples_taken_datetime"],
 }
 swab_datetime_map = {"yyyy-MM-dd HH:mm:ss": ["pcr_result_recorded_datetime"]}
