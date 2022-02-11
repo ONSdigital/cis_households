d140b249a442c6d51fc58f7dc13ed06b1219607a
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-02-11 12:08:34 +0000

    fixed error in generating report xcel sheet

--- cishouseholds/pipeline/generate_outputs.py
+++ cishouseholds/pipeline/generate_outputs.py
@@ -71,13 +71,14 @@ def report(
     )
 
     output = BytesIO()
-    counts_df.to_excel(output, sheet_name="dataset totals", index=False)
-    valid_df_errors.toPandas().to_excel(output, sheet_name="errors in valid dataset", index=False)
-    invalid_df_errors.toPandas().to_excel(output, sheet_name="errors in invalid dataset", index=False)
-    duplicated_df.toPandas().to_excel(output, sheet_name="duplicated rows", index=False)
+    with pd.ExcelWriter(output) as writer:
+        counts_df.to_excel(writer, sheet_name="dataset totals", index=False)
+        valid_df_errors.toPandas().to_excel(writer, sheet_name="errors in valid dataset", index=False)
+        invalid_df_errors.toPandas().to_excel(writer, sheet_name="errors in invalid dataset", index=False)
+        duplicated_df.toPandas().to_excel(writer, sheet_name="duplicated rows", index=False)
 
     write_string_to_file(
-        output.read(), f"{output_directory}/report_output_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.xlsx"
+        output.getbuffer(), f"{output_directory}/report_output_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.xlsx"
     )
 
 
