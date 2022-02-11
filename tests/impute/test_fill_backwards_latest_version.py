1d0ddaa5ec833be961112bec3ccaa1876e70158d
Ohi Bokth <65452503+ohibo@users.noreply.github.com>
2022-02-11 11:47:47 +0000

    1732 Edit backwards to get most recent demographic information (#268)
    * function and test start
    
    * created function and test but ETL function calls to-do
    
    * mistakingly added a pdb, it is now deleted
    
    * added function call into union_dependent_transformations()
    
    * integration test
    
    * Fix derivation of dataset version
    
    * added changes from review
    
    * function name change
    
    * regression tests
    
    * Fix indentation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: David Foster <foster.dev999@gmail.com>
    Co-authored-by: David Foster <31405412+foster999@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/impute/test_fill_backwards_latest_version.py
@@ -0,0 +1,52 @@
+from chispa import assert_df_equality
+
+from cishouseholds.impute import fill_backwards_overriding_not_nulls
+
+
+def test_fill_backwards_lattest_version(spark_session):
+    input_data = [
+        # fmt:off
+            ("Voyager 1 original",      1,     1,     '2020-01-01',       'm',        '1990-01-01',       'white'),
+            ("Voyager 1 original",      1,     2,     '2020-01-02',       'm',        '1990-01-01',       'white'),
+
+            ("Voyager 1 original",      2,     3,     '2020-01-02',       'm',        '1990-01-01',       'white'),
+
+            ("Voyager 2",               1,     4,     '2020-01-01',       'm',        '1990-01-01',       'white'),
+            ("Voyager 2",               1,     5,     '2020-01-02',       'm',        '1990-01-01',       'black'),
+
+            ("Voyager 2",               2,     6,     '2020-01-02',       'f',        '1990-01-01',       'white'),
+        # fmt:on
+    ]
+
+    expected_data = [
+        # fmt:off
+            ("Voyager 1 original",      1,     1,     '2020-01-01',       'm',        '1990-01-01',       'black'),
+            ("Voyager 1 original",      1,     2,     '2020-01-02',       'm',        '1990-01-01',       'black'),
+            ("Voyager 2",               1,     4,     '2020-01-01',       'm',        '1990-01-01',       'black'),
+            ("Voyager 2",               1,     5,     '2020-01-02',       'm',        '1990-01-01',       'black'),
+
+            ("Voyager 1 original",      2,     3,     '2020-01-02',       'f',        '1990-01-01',       'white'),
+            ("Voyager 2",               2,     6,     '2020-01-02',       'f',        '1990-01-01',       'white'),
+        # fmt:on
+    ]
+    schema = """
+            dataset string,
+            participant_id integer,
+            visit_id integer,
+            visit_date string,
+            sex string,
+            dob string,
+            ethnicity string
+            """
+
+    expected_df = spark_session.createDataFrame(data=expected_data, schema=schema)
+    input_df = spark_session.createDataFrame(data=input_data, schema=schema)
+
+    df = fill_backwards_overriding_not_nulls(
+        df=input_df,
+        column_identity="participant_id",
+        ordering_column="visit_date",
+        dataset_column="dataset",
+        column_list=["sex", "dob", "ethnicity"],
+    )
+    assert_df_equality(df, expected_df, ignore_row_order=True, ignore_column_order=True)
