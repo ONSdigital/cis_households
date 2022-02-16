dcd01a51c16b0a761c81f60f5d345e4b75babd9f
Ohi Bokth <bokth89@gmail.com>
2022-02-16 14:27:18 +0000

    added dictionary

--- cishouseholds/pipeline/survey_responses_version_2_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_2_ETL.py
@@ -339,6 +339,54 @@ def derive_age_columns(df: DataFrame) -> DataFrame:
 
 
 def derive_work_status_columns(df: DataFrame) -> DataFrame:
+
+    work_status_dict = {
+        "Child under 5y attending child care": "Child under 5y attending child care",  # noqa: E501
+        "Child under 5y attending nursery or pre-school or childminder": "Child under 5y attending child care",  # noqa: E501
+        "Child under 4-5y attending nursery or pre-school or childminder": "Child under 5y attending child care",  # noqa: E501
+        "Child under 5y not attending nursery or pre-school or childminder": "Child under 5y not attending child care",  # noqa: E501
+        "Child under 5y not attending child care": "Child under 5y not attending child care",  # noqa: E501
+        "Child under 4-5y not attending nursery or pre-school or childminder": "Child under 5y not attending child care",  # noqa: E501
+        "Employed and currently not working (e.g. on leave due to the COVID-19 pandemic (furloughed) or sick leave for 4 weeks or longer or maternity/paternity leave)": "Employed and currently not working",  # noqa: E501
+        "Employed and currently working (including if on leave or sick leave for less than 4 weeks)": "Employed and currently working",  # noqa: E501
+        "Not working and not looking for work (including voluntary work)": "Not working and not looking for work",  # noqa: E501
+        "Not working and not looking for work": "Not working and not looking for work",  # noqa: E501
+        "Self-employed and currently not working (e.g. on leave due to the COVID-19 pandemic (furloughed) or sick leave for 4 weeks or longer or maternity/paternity leave)": "Self-employed and currently not working",  # noqa: E501
+        "Self-employed and currently not working (e.g. on leave due to the COVID-19 pandemic or sick leave for 4 weeks or longer or maternity/paternity leave)": "Self-employed and currently not working",  # noqa: E501
+        "Self-employed and currently working (include if on leave or sick leave for less than 4 weeks)": "Self-employed and currently working",  # noqa: E501
+        "Retired (include doing voluntary work here)": "Retired",  # noqa: E501
+        "Retired": "Retired",  # noqa: E501
+        "Looking for paid work and able to start": "Looking for paid work and able to start",  # noqa: E501
+        "Attending college or other further education provider (including apprenticeships) (including if temporarily absent)": "5y and older in full-time education",  # noqa: E501
+        "Attending university (including if temporarily absent)": "5y and older in full-time education",  # noqa: E501
+        "4-5y and older at school/home-school (including if temporarily absent)": "5y and older in full-time education",  # noqa: E501
+        "5y and older in full-time education": "5y and older in full-time education",  # noqa: E501
+        "Retired (include doing voluntary work here)": "Retired",  # noqa: E501
+        "Retired": "Retired",  # noqa: E501
+        "Attending college or other further education provider (including apprenticeships) (including if temporarily absent)": "Attending college or FE (including if temporarily absent)",  # noqa: E501
+        "Attending university (including if temporarily absent)": "Attending university (including if temporarily absent)",  # noqa: E501
+        "Child under 5y attending child care": "Child under 4-5y attending child care",  # noqa: E501
+        "Child under 5y attending nursery or pre-school or childminder": "Child under 4-5y attending child care",  # noqa: E501
+        "Child under 4-5y attending nursery or pre-school or childminder": "Child under 4-5y attending child care",  # noqa: E501
+        "Child under 5y not attending nursery or pre-school or childminder": "Child under 4-5y not attending child care",  # noqa: E501
+        "Child under 5y not attending child care": "Child under 4-5y not attending child care",  # noqa: E501
+        "Child under 4-5y not attending nursery or pre-school or childminder": "Child under 4-5y not attending child care",  # noqa: E501
+        "4-5y and older at school/home-school (including if temporarily absent)": "4-5y and older at school/home-school",  # noqa: E501
+        "Employed and currently not working (e.g. on leave due to the COVID-19 pandemic (furloughed) or sick leave for 4 weeks or longer or maternity/paternity leave)": "Employed and currently not working",  # noqa: E501
+        "Employed and currently working (including if on leave or sick leave for less than 4 weeks)": "Employed and currently working",  # noqa: E501
+        "Not in paid work and not looking for paid work (include doing voluntary work here)": "Not working and not looking for work",  # noqa: E501
+        "Self-employed and currently not working (e.g. on leave due to the COVID-19 pandemic or sick leave for 4 weeks or longer or maternity/paternity leave)": "Self-employed and currently not working",  # noqa: E501
+        "Self-employed and currently not working (e.g. on leave due to the COVID-19 pandemic (furloughed) or sick leave for 4 weeks or longer or maternity/paternity leave)": "Self-employed and currently not working",  # noqa: E501
+        "Self-employed and currently working (include if on leave or sick leave for less than 4 weeks)": "Self-employed and currently working",  # noqa: E501
+        "Looking for paid work and able to start": "Looking for paid work and able to start",  # noqa: E501
+    }
+
+    df = update_column_values_from_map(
+        df=df,
+        column="work_status",
+        map=work_status_dict,
+    )
+
     df = df.withColumn(
         "work_status", F.coalesce(F.col("work_status_v0"), F.col("work_status_v1"), F.col("work_status_v2"))
     )
