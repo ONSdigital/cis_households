d54900b985872a62b66eb4bfd799761b55e71929
fosted1 <David.Foster@ons.gov.uk>
2022-02-22 11:27:40 +0000

    Add dropping of gender column

--- cishouseholds/pipeline/survey_responses_version_0_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_0_ETL.py
@@ -9,5 +9,5 @@ def transform_survey_responses_version_0_delta(df: DataFrame) -> DataFrame:
     Call functions to process input for iqvia version 0 survey deltas.
     """
     df = assign_column_uniform_value(df, "survey_response_dataset_major_version", 0)
-    df = df.withColumn("sex", F.coalesce(F.col("sex"), F.col("gender")))
+    df = df.withColumn("sex", F.coalesce(F.col("sex"), F.col("gender"))).drop("gender")
     return df
