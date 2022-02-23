65e303258a1c02c06874e8c2cddc23d63788dcdb
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-02-23 09:21:06 +0000

    add work location edit

--- cishouseholds/pipeline/survey_responses_version_2_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_2_ETL.py
@@ -41,6 +41,7 @@ from cishouseholds.edit import clean_within_range
 from cishouseholds.edit import convert_null_if_not_in_list
 from cishouseholds.edit import format_string_upper_and_clean
 from cishouseholds.edit import map_column_values_to_null
+from cishouseholds.edit import update_column_if_ref_in_list
 from cishouseholds.edit import update_column_values_from_column_reference
 from cishouseholds.edit import update_column_values_from_map
 from cishouseholds.edit import update_face_covering_outside_of_home
@@ -750,6 +751,18 @@ def union_dependent_derivations(df):
         participant_id_column="participant_id",
         visit_date_column="visit_datetime",
         main_job_changed_column="work_main_job_changed",
+    )
+    df = update_column_if_ref_in_list(
+        df=df,
+        column_name_to_update="work_location",
+        old_value=None,
+        new_value="Not applicable, not currently working",
+        reference_column="work_status_v0",
+        check_list=[
+            "Furloughed (temporarily not working)",
+            "Not working (unemployed, retired, long-term sick etc.)",
+            "Student",
+        ],
     )
     df = assign_work_patient_facing_now(
         df, "work_patient_facing_now", age_column="age_at_visit", work_healthcare_column="work_health_care_combined"
