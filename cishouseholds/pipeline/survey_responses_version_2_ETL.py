7866b87c70fd1f505df359d7d80b0d4b7b3435c7
David Foster <foster.dev999@gmail.com>
2022-02-28 12:38:46 +0000

    Remove duplicated fill forwards

--- cishouseholds/pipeline/survey_responses_version_2_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_2_ETL.py
@@ -945,14 +945,6 @@ def fill_forwards_transformations(df):
         record_changed_column="cis_covid_vaccine_received",
         record_changed_value="Yes",
     )
-    df = impute_outside_uk_columns(
-        df=df,
-        outside_uk_since_column="been_outside_uk_since_april_2020",
-        outside_uk_date_column="been_outside_uk_last_date",
-        outside_country_column="been_outside_uk_last_country",
-        visit_datetime_column="visit_datetime",
-        id_column="participant_id",
-    )
     df = fill_forward_from_last_change(
         df=df,
         fill_forward_columns=[
