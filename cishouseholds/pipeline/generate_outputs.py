2f9f30bab33368ccf2e978417b7ee91afadbf9a5
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-02-28 09:06:18 +0000

    modified edited df to also be filtered

--- cishouseholds/pipeline/generate_outputs.py
+++ cishouseholds/pipeline/generate_outputs.py
@@ -121,10 +121,14 @@ def record_level_interface(
     filtered_survey_responses_table: str,
 ):
     input_df = extract_from_table(survey_responses_table)
-    edited_df = update_from_csv_lookup(df=input_df, csv_filepath=csv_editing_file, id_column=unique_id_column)
-    update_table(edited_df, edited_survey_responses_table, "overwrite")
+    edited_df = input_df
+    if csv_editing_file != "":
+        edited_df = update_from_csv_lookup(df=input_df, csv_filepath=csv_editing_file, id_column=unique_id_column)
 
-    filtered_df = edited_df.filter(F.col(unique_id_column).isin(unique_id_list))
+    if unique_id_list != [] and unique_id_list != "":
+        filtered_df = edited_df.filter(F.col(unique_id_column).isin(unique_id_list))
+
+    update_table(edited_df, edited_survey_responses_table, "overwrite")
     update_table(filtered_df, filtered_survey_responses_table, "overwrite")
 
 
