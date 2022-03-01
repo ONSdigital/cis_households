c682de1f956dc3e3f2afcd890d0e80023e4edb7b
David Foster <foster.dev999@gmail.com>
2022-03-01 16:56:55 +0000

    Add pydata theme to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,18 @@ templates_path = ["_templates"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "pydata_sphinx_theme"
+
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/ONS-SST/cis_households",
+            "icon": "fab fa-github-square",
+            "type": "fontawesome",
+        }
+    ]
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
