06ae4321d7c48dc5aa8a91bb3e744fe16acd1ac4
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-03-03 09:01:16 +0000

    removed commented code

--- cishouseholds/pipeline/validation_ETL.py
+++ cishouseholds/pipeline/validation_ETL.py
@@ -74,20 +74,6 @@ def validation_calls(SparkVal):
         error_message="cis vaccine type other should be null unless vaccine type is 'Other / specify'",
     )
 
-    # _vaccine_n_columns_are_null = [
-    #     F.col(item).isNull()
-    #     for number in range(1, 5)
-    #     for item in (
-    #         f"cis_covid_vaccine_type_{number}",
-    #         f"cis_covid_vaccine_type_other_{number}",
-    #         f"cis_covid_vaccine_date_{number}",
-    #     )
-    # ]
-    # SparkVal.validate_udl(
-    #     logic=(F.col("visit_type") == "First Visit" | (reduce(lambda x, y: x & y, _vaccine_n_columns_are_null))),
-    #     error_message="vaccine _n fields are all null if not first visit",
-    # )
-
     SparkVal.validate_udl(
         logic=(
             (
