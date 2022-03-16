cf2855d209859fe71cd26d938e290a62c0983bc0
Ohi Bokth <bokth89@gmail.com>
2022-03-16 13:34:21 +0000

    created a function to measure time of execution

---
+++ tests/test_broadcast_time.py
@@ -0,0 +1,15 @@
+import timeit
+
+from cishouseholds.pyspark_utils import get_or_create_spark_session
+from tests.derive.test_assign_multigen import test_assign_multigeneration
+
+
+def test_time_required(f):
+    spark_session = get_or_create_spark_session()
+    start = timeit.timeit()
+    f(spark_session)
+    end = timeit.timeit()
+    print(f"-- Time required for is ", end - start)  # functional
+
+
+test_time_required(f=test_assign_multigeneration)
