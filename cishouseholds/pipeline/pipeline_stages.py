c3f6d5aecdbba47e48775fe832bab030e5b45946
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-03-17 11:42:50 +0000

    reset and merged

--- cishouseholds/pipeline/pipeline_stages.py
+++ cishouseholds/pipeline/pipeline_stages.py
@@ -676,7 +676,7 @@ def join_geographic_data(
     survey_responses_table: str,
     geographic_responses_table: str,
     id_column: str,
-    # region_column: str,
+    region_column: str,
 ):
     """
     Join weights file onto survey data by household id.
@@ -694,7 +694,11 @@ def join_geographic_data(
     """
     design_weights_df = extract_from_table(geographic_table)
     survey_responses_df = extract_from_table(survey_responses_table)
-    geographic_survey_df = survey_responses_df.drop("postcode").join(design_weights_df, on=id_column, how="left")
+    geographic_survey_df = (
+        survey_responses_df.drop("postcode")
+        .join(design_weights_df, on=id_column, how="left")
+        .filter(F.col(region_column).isNotNull())
+    )
     update_table(geographic_survey_df, geographic_responses_table)
 
 
