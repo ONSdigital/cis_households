a9afcd21988cd49dd8103d4363801351ca3a6a22
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-03-28 12:21:48 +0100

    added sepearted unit test of pivot and join

--- cishouseholds/edit.py
+++ cishouseholds/edit.py
@@ -314,6 +314,23 @@ def clean_postcode(df: DataFrame, postcode_column: str):
     return df.drop("TEMP")
 
 
+def create_edited_df(df: DataFrame, csv: DataFrame, id_column: str) -> DataFrame:
+    """
+    Join the dataframe and csv editing files after formatting csv lookup df
+    csv
+    df
+    id_column
+        column on which to join the csv lookup on main dataset df
+    """
+    csv = (
+        csv.groupBy("id")
+        .pivot("target_column_name")
+        .agg(F.first("old_value").alias("old_value"), F.first("new_value").alias("new_value"))
+        .drop("old_value", "new_value")
+    )
+    return df.join(csv, csv.id == df[id_column], how="left").drop(csv.id)
+
+
 def update_from_csv_lookup(df: DataFrame, csv_filepath: str, dataset_name: Union[str, None], id_column: str):
     """
     Update specific cell values from a map contained in a csv file.
@@ -333,13 +350,7 @@ def update_from_csv_lookup(df: DataFrame, csv_filepath: str, dataset_name: Union
     if dataset_name is not None:
         csv = csv.filter(F.col("dataset_name").eqNullSafe(dataset_name))
 
-    csv = (
-        csv.groupBy("id")
-        .pivot("target_column_name")
-        .agg(F.first("old_value").alias("old_value"), F.first("new_value").alias("new_value"))
-        .drop("old_value", "new_value")
-    )
-    df = df.join(csv, csv.id == df[id_column], how="left").drop(csv.id)
+    df = create_edited_df(df, csv, id_column)
     r = re.compile(r"(.*){1,}_old_value$")
     cols = [col[:-10] for col in list(filter(r.match, csv.columns))]
     for col in cols:
---
+++ tests/edit/test_create_edited_df.py
@@ -0,0 +1,48 @@
+from chispa import assert_df_equality
+
+from cishouseholds.edit import create_edited_df
+
+
+def test_create_edited_df(spark_session):
+    map_df = spark_session.createDataFrame(
+        data=[
+            (1, "i", "B", "original", "converted"),
+            (2, "i", "A", "original", "converted"),
+            (2, "i", "B", "original", "converted"),
+            (3, "i", "B", "original", "converted"),
+            (4, "i", "B", "original", "converted"),
+            (5, "i", "A", "something else", "converted"),
+            (6, "i", "A", "value", "converted A from value"),
+            (6, "i", "B", None, "converted B from null"),
+            (6, "i", "C", None, "converted C from null"),
+            (None, "i", "B", None, "null"),  # missing id should be joined but not updated
+            (7, "i", "B", None, "converted B from null"),  # non existent id should not be joined
+        ],
+        schema="""id integer, dataset_name string, target_column_name string, old_value string, new_value string""",
+    )
+    input_df = spark_session.createDataFrame(
+        data=[
+            (1, "original", "original"),
+            (2, "original", "original"),
+            (3, "original", "original"),
+            (4, "original", "original"),
+            (5, "original", "original"),
+            (6, "value", None),
+        ],
+        schema="""id integer, A string, B string""",
+    )
+    expected_df = spark_session.createDataFrame(
+        # fmt: off
+        data=[
+            (1, "original", "original", None, None, "original", "converted", None, None),
+            (2, "original", "original", "original", "converted", "original", "converted", None, None),
+            (3, "original", "original", None, None, "original", "converted", None, None),
+            (4, "original", "original", None, None, "original", "converted", None, None),
+            (5, "original", "original", "something else", "converted", None, None, None, None),
+            (6,"value",None,"value","converted A from value",None,"converted B from null",None,"converted C from null"),
+        ],
+        # fmt: on
+        schema="""id integer, A string, B string,  A_old_value string, A_new_value string, B_old_value string, B_new_value string, C_old_value string, C_new_value string""",
+    )
+    output_df = create_edited_df(input_df, map_df, "id")
+    assert_df_equality(expected_df, output_df, ignore_nullable=True, ignore_column_order=True)
