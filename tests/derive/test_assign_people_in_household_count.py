3dbf6aa9d839055580123298e6aa2e3796191110
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-03-29 10:03:57 +0100

    none

--- tests/derive/test_assign_people_in_household_count.py
+++ tests/derive/test_assign_people_in_household_count.py
@@ -32,3 +32,27 @@ def test_assign_people_in_household_count(spark_session):
         ignore_row_order=True,
         ignore_column_order=True,
     )
+
+
+def test_assign_people_in_household_count_real(spark_session):
+    expected_df = spark_session.createDataFrame(
+        data=[
+            (1,0,0,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None),
+            (1,None,None,None,0,0,0,None,0,None,0,None,None,None,None,None,None,None,None),
+            (1,None,None,0,0,0,0,0,0,0,29,0,0,0,0,0,0,0,0),
+            (1,None,None,0,3,2,0,0,0,0,29,0,0,0,0,0,0,0,0)
+        ],
+        schema="count integer, non_consent integer, i1 integer, i2 integer, i3 integer, i4 integer, i5 integer, i6 integer, i7 integer, i8 integer, p1 integer, p2 integer, p3 integer, p4 integer, p5 integer, p6 integer, p7 integer, p8 integer, total_count integer",
+    )
+    input_df = expected_df.drop("total_count")
+    input_df.show()
+    output_df = assign_people_in_household_count(
+        input_df, "total_count", r"i[1-8]", r"i[1-5,7,8]", r"p[1-8]", "count", "non_consent"
+    )
+    output_df.show()
+    assert_df_equality(
+        output_df,
+        expected_df,
+        ignore_row_order=True,
+        ignore_column_order=True,
+    )
