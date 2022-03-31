1fa4707521950bfde0431aedb541432b335001d2
Ohi Bokth <bokth89@gmail.com>
2022-03-31 14:44:44 +0100

    got rid of work_social_care

--- cishouseholds/pipeline/survey_responses_version_1_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_1_ETL.py
@@ -47,7 +47,6 @@ def transform_survey_responses_version_1_delta(df: DataFrame) -> DataFrame:
         "work_main_job_role",
         "work_sectors",
         "work_sectors_other",
-        "work_social_care",
         "work_health_care_v0",
         "work_health_care_v1_v2",
         "work_nursing_or_residential_care_home",
