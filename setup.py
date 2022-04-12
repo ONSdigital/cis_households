306d66e938a1e8209ee673d44132744d8d619914
David Foster <foster.dev999@gmail.com>
2022-04-12 14:48:15 +0100

    Bump version: 1.2.6-beta.0 → 1.2.6-beta.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ dev_requires = [
 
 setuptools.setup(
     name="cishouseholds",
-    version="1.2.6-beta.0",
+    version="1.2.6-beta.1",
     author="CIS development team",
     author_email="cis.dev@ons.gov.uk",
     description="Data engineering pipeline for the Office for National Statistics COVID-19 Infection Survey (CIS)",
