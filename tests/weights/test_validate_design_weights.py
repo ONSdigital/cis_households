7d630ac63e67c6dea2d97bdffb0b2409aa18c5d7
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-04-19 14:20:04 +0100

    fixed long type issue

--- tests/weights/test_validate_design_weights.py
+++ tests/weights/test_validate_design_weights.py
@@ -44,5 +44,6 @@ def test_validate_design_weights(spark_session):
         column_name_to_assign="validated",
         num_households_column="num_hh",
         group_by_columns=["window"],
+        window=window,
     )
     assert_df_equality(output_df, expected_df, ignore_column_order=True, ignore_row_order=True, ignore_nullable=True)
