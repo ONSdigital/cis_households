14294d65081c3fd19b22a7c57840ecb58c21095a
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-05-03 09:59:41 +0100

    cast output to int

--- cishouseholds/derive.py
+++ cishouseholds/derive.py
@@ -33,8 +33,8 @@ def assign_distinct_count_in_group(
     group_window = Window.partitionBy(*group_by_columns)
     df = df.withColumn(
         column_name_to_assign,
-        F.sum(F.when(F.row_number().over(count_distinct_columns_window) == 1, 1)).over(group_window),
-    ).cast("integer")
+        F.sum(F.when(F.row_number().over(count_distinct_columns_window) == 1, 1)).over(group_window).cast("integer"),
+    )
     return df
 
 
