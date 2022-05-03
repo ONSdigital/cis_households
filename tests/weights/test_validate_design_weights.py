5ead7b20400f9154ddd02b6b72c299728bd184d0
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-05-03 12:47:32 +0100

    test

--- tests/weights/test_validate_design_weights.py
+++ tests/weights/test_validate_design_weights.py
@@ -51,7 +51,7 @@ def test_validate_design_weights(spark_session):
         )
     with pytest.raises(DesignWeightError, match="check_2: The design weights are NOT all are positive."):
         output_df = validate_design_weights_or_precal(
-            df=input1_df,
+            df=input2_df,
             num_households_column="num_hh",
             swab_weight_column="swab_weight",
             antibody_weight_column="antibody_weight",
