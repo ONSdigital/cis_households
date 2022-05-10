cdd5024a1cb447260836a265dfc5cbd8dcbb9a12
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-05-10 11:44:22 +0100

    minor changes

--- cishouseholds/pipeline/survey_responses_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_digital_ETL.py
@@ -2,5 +2,5 @@ from cishouseholds.derive import derive_self_isolating_from_digital
 
 
 def digital_specific_cleaning(df):
-    df = derive_self_isolating_from_digital(df, "are_you_self_isolating_s2", "self_isolating", "self_isolating_reason")
+    df = derive_self_isolating_from_digital(df, "self_isolating_detailed", "self_isolating", "self_isolating_reason")
     return df
