6a54f21bdef7bc46af05ffe0f219ef9e5903b787
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-05-19 12:00:56 +0100

    added version digital as v3

--- cishouseholds/pipeline/survey_responses_version_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_digital_ETL.py
@@ -1,6 +1,8 @@
 from cishouseholds.derive import assign_column_uniform_value
+from cishouseholds.pipeline.survey_responses_version_2_ETL import transform_survey_responses_generic
 
 
 def digital_specific_cleaning(df):
     df = assign_column_uniform_value(df, "survey_response_dataset_major_version", 3)
+    df = transform_survey_responses_generic(df)
     return df
