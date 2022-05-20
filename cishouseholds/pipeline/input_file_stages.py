2ffc29f5a5a77d274550b02e292348ad259445a4
BixLaw <92517253+BixLaw@users.noreply.github.com>
2022-05-20 11:20:33 +0100

    CISDP-2204 Added a digital specific transformation func which creates requested column (#483)
    * CISDP-2204 Added a digital specific transformation func which creates the column
    
    * Update cishouseholds/pipeline/input_file_stages.py
    
    Co-authored-by: David Foster <31405412+foster999@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * CISDP-2204 new regression tests
    
    Co-authored-by: David Foster <31405412+foster999@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- cishouseholds/pipeline/input_file_stages.py
+++ cishouseholds/pipeline/input_file_stages.py
@@ -11,6 +11,7 @@ from cishouseholds.pipeline.survey_responses_version_1_ETL import transform_surv
 from cishouseholds.pipeline.survey_responses_version_2_ETL import clean_survey_responses_version_2
 from cishouseholds.pipeline.survey_responses_version_2_ETL import derive_additional_v1_2_columns
 from cishouseholds.pipeline.survey_responses_version_2_ETL import digital_specific_cleaning
+from cishouseholds.pipeline.survey_responses_version_2_ETL import digital_specific_transformations
 from cishouseholds.pipeline.survey_responses_version_2_ETL import transform_survey_responses_generic
 from cishouseholds.pipeline.survey_responses_version_2_ETL import transform_survey_responses_version_2_delta
 from cishouseholds.pipeline.swab_delta_ETL import transform_swab_delta
@@ -141,7 +142,7 @@ cis_digital_parameters = {
     "id_column": "participant_completion_window_id",
     "validation_schema": validation_schemas["cis_digital_validation_schema"],
     "datetime_column_map": cis_digital_datetime_map,
-    "transformation_functions": [digital_specific_cleaning],
+    "transformation_functions": [digital_specific_cleaning, digital_specific_transformations],
     "sep": "|",
     "cast_to_double_list": survey_response_cisd_cast_to_double,
     "source_file_column": "survey_response_source_file",
@@ -158,4 +159,4 @@ for parameters in [
     unassayed_blood_delta_parameters,
     historical_blood_parameters,
 ]:
-    generate_input_processing_function(**parameters)
+    generate_input_processing_function(**parameters)  # type:ignore
