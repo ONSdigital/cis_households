96148d7b3661318e926918ff74db401c7c69c0ac
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-05-20 10:52:22 +0100

    added visit id col

--- cishouseholds/pipeline/survey_responses_version_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_digital_ETL.py
@@ -6,6 +6,6 @@ from cishouseholds.pipeline.survey_responses_version_2_ETL import transform_surv
 
 def digital_specific_cleaning(df):
     df = assign_column_uniform_value(df, "survey_response_dataset_major_version", 3)
-    df = df.withColumn("visit_id", F.col())
+    df = df.withColumn("visit_id", F.col("participant_completion_window_id"))
     df = transform_survey_responses_generic(df)
     return df
