8e0a0209b7579fc0e30aa086ab540a089d10fc94
Bix Lawrence <bix.lawrence@ons.gov.uk>
2022-05-23 16:52:33 +0100

    Created a new column based on been_outside_uk

--- cishouseholds/pipeline/survey_responses_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_digital_ETL.py
@@ -6,6 +6,7 @@ from cishouseholds.derive import derive_had_symptom_last_7days_from_digital
 
 def digital_specific_transformations(df):
     df = df.withColumn("face_covering_outside_of_home", F.lit(None).cast("string"))
+    df = df.withColumn("been_outside_uk", "been_outside_uk_since_last_visit")
     df = concat_fields_if_true(df, "think_had_covid_which_symptoms", "think_had_covid_which_symptom_", "Yes", ";")
     df = concat_fields_if_true(df, "which_symptoms_last_7_days", "symptoms_last_7_days_", "Yes", ";")
     df = concat_fields_if_true(df, "long_covid_symptoms", "long_covid_symptom_", "Yes", ";")
