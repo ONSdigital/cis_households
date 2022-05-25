784c4c6442298d57ed46269aed308844eb58bcfb
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-05-25 10:46:31 +0100

    updated digital null mappings!

--- cishouseholds/pipeline/survey_responses_version_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_digital_ETL.py
@@ -1,6 +1,8 @@
 import pyspark.sql.functions as F
 
 from cishouseholds.derive import assign_column_uniform_value
+from cishouseholds.derive import assign_raw_copies
+from cishouseholds.edit import apply_value_map_multiple_columns
 from cishouseholds.pipeline.survey_responses_version_2_ETL import transform_survey_responses_generic
 
 
@@ -10,4 +12,33 @@ def digital_specific_transformations(df):
     df = df.withColumn("visit_id", F.col("participant_completion_window_id"))
     df = df.withColumn("visit_datetime", F.col("digital_enrolment_invite_datetime"))
     df = transform_survey_responses_generic(df)
+    df = assign_raw_copies(
+        df,
+        [
+            "work_in_additional_paid_employment",
+            "self_isolating",
+            "illness_lasting_over_12_months",
+            "ever_smoked_regularly",
+            "currently_smokes_or_vapes",
+            "hours_a_day_with_someone_else_at_home",
+            "face_covering_work_or_education",
+        ],
+    )
+    mapping_dict = {
+        "Prefer not to say": None,
+        "Don&#39;t Know": None,
+        "Don&amp;#39;t Know": None,
+    }
+    df = apply_value_map_multiple_columns(
+        df,
+        {
+            "work_in_additional_paid_employment": mapping_dict,
+            "self_isolating": mapping_dict,
+            "illness_lasting_over_12_months": mapping_dict,
+            "ever_smoked_regularly": mapping_dict,
+            "currently_smokes_or_vapes": mapping_dict,
+            "hours_a_day_with_someone_else_at_home": mapping_dict,
+            "face_covering_work_or_education": mapping_dict,
+        },
+    )
     return df
