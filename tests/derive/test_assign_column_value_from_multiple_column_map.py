f2657ddbf4a4f1c7aa4a74e66ee824ebe436b9df
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-05-27 14:47:51 +0100

    fixed tests

--- tests/derive/test_assign_column_value_from_multiple_column_map.py
+++ tests/derive/test_assign_column_value_from_multiple_column_map.py
@@ -16,7 +16,7 @@ def test_assign_column_value_from_multiple_column_map(spark_session):
     output_df = assign_column_value_from_multiple_column_map(
         expected_df.drop("result"),
         "result",
-        {[1, [1, None, None]], [2, [3, 3, None]], [3, [2, 5, [3, 4]]]},
+        [[1, [1, None, None]], [2, [3, 3, None]], [3, [2, 5, [3, 4]]]],
         ["s1", "s2", "s3"],
     )
     assert_df_equality(output_df, expected_df, ignore_nullable=True, ignore_column_order=True, ignore_row_order=True)
