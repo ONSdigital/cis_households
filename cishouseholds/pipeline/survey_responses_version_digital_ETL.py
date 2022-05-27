ce639eb56bda1d9028987a42421f57aaeee7553f
David Foster <31405412+foster999@users.noreply.github.com>
2022-05-27 11:09:11 +0100

    2217 Update digital schema (#493)
    * Update CISD schemas
    
    Based on 17/05/2022 extract and ONS Extract v0.4
    
    * Fixed missing commas in some choice lists
    
    * Update schemas based on v0.5 and 20/05 extract
    
    Changed some var names and choices based on latest extract
    
    * reran regressions
    
    * update based on desired_digital_schema.txt
    
    * rerun regressions
    
    * reran regressions
    
    * Update digital datetime fields and map
    
    * Correct datetime calls in digital dummy schema
    
    * update picklists based on 2505 2605 extracts
    
    * update datetime start/ends and yes_no_none
    
    Revert previous changes to yes_no_none options
    
    * fix validation_schema mismatch
    
    * Remove incorrect visit_datetime derivation
    
    * Rerun regression tests
    
    Co-authored-by: Matthew Maynard <matthew.maynard@ons.gov.uk>
    Co-authored-by: Stefen Lamothe <78305473+slamothe99@users.noreply.github.com>
    Co-authored-by: Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>

--- cishouseholds/pipeline/survey_responses_version_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_digital_ETL.py
@@ -8,19 +8,20 @@ from cishouseholds.pipeline.survey_responses_version_2_ETL import transform_surv
 
 def digital_specific_transformations(df):
     df = assign_column_uniform_value(df, "survey_response_dataset_major_version", 3)
-    df = df.withColumn("face_covering_outside_of_home", F.lit(None).cast("string"))
     df = df.withColumn("visit_id", F.col("participant_completion_window_id"))
-    df = df.withColumn("visit_datetime", F.col("digital_enrolment_invite_datetime"))
-    df = df.withColumn("times_outside_shopping_or_socialising_last_7_days", F.lit(None))
+    df = df.withColumn("visit_datetime", F.lit(None).cast("timestamp"))  # Placeholder for 2199
+
     df = transform_survey_responses_generic(df)
     df = clean_barcode_simple(df, "swab_sample_barcode_user_entered")
     df = clean_barcode_simple(df, "blood_sample_barcode_user_entered")
+
+    df = df.withColumn("times_outside_shopping_or_socialising_last_7_days", F.lit(None))
     df = edit_to_sum_or_max_value(
         df=df,
         column_name_to_assign="times_outside_shopping_or_socialising_last_7_days",
         columns_to_sum=[
             "times_shopping_last_7_days",
-            "times_socialise_last_7_days",
+            "times_socialising_last_7_days",
         ],
         max_value=7,
     )
