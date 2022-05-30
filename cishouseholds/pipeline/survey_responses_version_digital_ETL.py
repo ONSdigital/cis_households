9dc15ef7dd1692d2d3b30d4718c8f8cb12930348
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-05-30 11:44:25 +0100

    updated columns to update

--- cishouseholds/pipeline/survey_responses_version_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_digital_ETL.py
@@ -12,32 +12,45 @@ def digital_specific_transformations(df):
     df = df.withColumn("visit_id", F.col("participant_completion_window_id"))
     df = df.withColumn("visit_datetime", F.col("digital_enrolment_invite_datetime"))
     df = transform_survey_responses_generic(df)
-    df = assign_raw_copies(
-        df,
-        [
-            "work_in_additional_paid_employment",
-            "self_isolating",
-            "illness_lasting_over_12_months",
-            "ever_smoked_regularly",
-            "currently_smokes_or_vapes",
-            "hours_a_day_with_someone_else_at_home",
-            "face_covering_work_or_education",
-        ],
-    )
+    dont_know_columns = [
+        "work_in_additional_paid_employment",
+        "work_nursing_or_residential_care_home",
+        "work_direct_contact_patients_or_clients",
+        "self_isolating",
+        "illness_lasting_over_12_months",
+        "ever_smoked_regularly",
+        "currently_smokes_or_vapes",
+        "cis_covid_vaccine_type_1",
+        "cis_covid_vaccine_type_2",
+        "cis_covid_vaccine_type_3",
+        "cis_covid_vaccine_type_4",
+        "cis_covid_vaccine_type_5",
+        "cis_covid_vaccine_type_6",
+        "other_household_member_hospital_last_28_days",
+        "other_household_member_care_home_last_28_days",
+        "hours_a_day_with_someone_else_at_hom",
+        "physical_contact_under_18_years",
+        "physical_contact_18_to_69_years",
+        "physical_contact_over_70_years",
+        "social_distance_contact_under_18_years",
+        "social_distance_contact_18_to_69_year",
+        "social_distance_contact_over_70_years",
+        "times_hour_or_longer_another_home_last_7_days",
+        "times_hour_or_longer_another_person_your_home_last_7_days",
+        "times_shopping_last_7_days",
+        "times_socialising_last_7_days",
+        "face_covering_work_or_education",
+        "face_covering_other_enclosed_places",
+        "cis_covid_vacine_type",
+    ]
+    df = assign_raw_copies(df, dont_know_columns)
     mapping_dict = {
         "Prefer not to say": None,
         "Don't Know": None,
     }
     df = apply_value_map_multiple_columns(
         df,
-        {
-            "work_in_additional_paid_employment": mapping_dict,
-            "self_isolating": mapping_dict,
-            "illness_lasting_over_12_months": mapping_dict,
-            "ever_smoked_regularly": mapping_dict,
-            "currently_smokes_or_vapes": mapping_dict,
-            "hours_a_day_with_someone_else_at_home": mapping_dict,
-            "face_covering_work_or_education": mapping_dict,
-        },
+        {k: mapping_dict for k in dont_know_columns},
     )
+
     return df
