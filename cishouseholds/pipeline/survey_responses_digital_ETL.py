bed23afdec7e037d1213e3a6c0ac19bb1b773dfe
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-05-30 09:51:02 +0100

    combined visit status columns and updated mapping

--- cishouseholds/pipeline/survey_responses_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_digital_ETL.py
@@ -4,6 +4,7 @@ from cishouseholds.derive import assign_raw_copies
 from cishouseholds.derive import concat_fields_if_true
 from cishouseholds.derive import derive_had_symptom_last_7days_from_digital
 from cishouseholds.edit import apply_value_map_multiple_columns
+from cishouseholds.edit import update_column_values_from_map
 
 
 def digital_specific_cleaning(df):
@@ -38,7 +39,9 @@ def digital_specific_transformations(df):
     df = concat_fields_if_true(df, "think_had_covid_which_symptoms", "think_had_covid_which_symptom_", "Yes", ";")
     df = concat_fields_if_true(df, "which_symptoms_last_7_days", "think_have_covid_symptom_", "Yes", ";")
     df = concat_fields_if_true(df, "long_covid_symptoms", "think_have_long_covid_symptom_", "Yes", ";")
-
+    df = update_column_values_from_map(
+        df, "survey_completion_status", {"In Progress": "Partially Completed", "Submitted": "Completed"}
+    )
     df = derive_had_symptom_last_7days_from_digital(
         df,
         "had_symptom_last_7days",
