786ef711d4a4d600c49519b688eb72d885b0f040
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-06-09 10:01:07 +0100

    none

--- cishouseholds/pipeline/survey_responses_version_digital_ETL.py
+++ cishouseholds/pipeline/survey_responses_version_digital_ETL.py
@@ -12,37 +12,7 @@ def digital_specific_transformations(df):
     df = df.withColumn("visit_datetime", F.lit(None).cast("timestamp"))  # Placeholder for 2199
 
     df = transform_survey_responses_generic(df)
-    dont_know_columns = [
-        "work_in_additional_paid_employment",
-        "work_nursing_or_residential_care_home",
-        "work_direct_contact_patients_or_clients",
-        "self_isolating",
-        "illness_lasting_over_12_months",
-        "ever_smoked_regularly",
-        "currently_smokes_or_vapes",
-        "cis_covid_vaccine_type_1",
-        "cis_covid_vaccine_type_2",
-        "cis_covid_vaccine_type_3",
-        "cis_covid_vaccine_type_4",
-        "cis_covid_vaccine_type_5",
-        "cis_covid_vaccine_type_6",
-        "other_household_member_hospital_last_28_days",
-        "other_household_member_care_home_last_28_days",
-        "hours_a_day_with_someone_else_at_home",
-        "physical_contact_under_18_years",
-        "physical_contact_18_to_69_years",
-        "physical_contact_over_70_years",
-        "social_distance_contact_under_18_years",
-        "social_distance_contact_18_to_69_years",
-        "social_distance_contact_over_70_years",
-        "times_hour_or_longer_another_home_last_7_days",
-        "times_hour_or_longer_another_person_your_home_last_7_days",
-        "times_shopping_last_7_days",
-        # "times_socialising_last_7_days",
-        "face_covering_work_or_education",
-        "face_covering_other_enclosed_places",
-        # "cis_covid_vacine_type",
-    ]
+    
     df = assign_raw_copies(df, dont_know_columns)
     dont_know_mapping_dict = {"Prefer not to say": None, "Don't Know": None, "I don't know the type": None}
     df = apply_value_map_multiple_columns(
