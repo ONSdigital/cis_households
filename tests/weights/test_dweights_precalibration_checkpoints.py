c8d648373c2afcc000a166035136a4e1b7b91b5d
Ohi Bokth <bokth89@gmail.com>
2022-06-24 13:51:08 +0100

    modified output of validate_design_weights and used pytest.raise to assert specific errors

--- tests/weights/test_dweights_precalibration_checkpoints.py
+++ tests/weights/test_dweights_precalibration_checkpoints.py
@@ -6,14 +6,11 @@ from cishouseholds.weights.design_weights import DesignWeightError
 from cishouseholds.weights.design_weights import validate_design_weights
 
 
-@pytest.mark.xfail(reason="Input data do not pass checks")
 def test_precal_and_design_weights_checkpoints(spark_session):
     schema = """
                 id integer,
                 country string,
                 num_households integer,
-                groupby double,
-                sample_group string,
                 design_weight_1 double,
                 design_weight_2 double,
                 not_positive_or_null integer
@@ -23,24 +20,19 @@ def test_precal_and_design_weights_checkpoints(spark_session):
         data=[
             # fmt: off
             #   id      country         num_households
-            #                                   groupby     sample_group
-            #                                                           design_weight_1
-            #                                                                       design_weight_2
-            #                                                                                   not_positive_or_null
-                (1,     'england',      3,       2.0,       'new',      2.5,        1.0,        1),
-                (1,     'england',      3,       2.0,       'new',      -1.5,       1.2,        None),
-                (1,     'england',      3,       2.0,       'new',      -1.5,       None,       None),
-
-                (1,     'england',      3,       2.0,       'old',      2.5,        1.0,        1),
-                (1,     'england',      3,       2.0,       'old',      -1.5,       1.2,        None),
-                (1,     'england',      3,       2.0,       'old',      -1.5,       None,       None),
+            #                                   design_weight_1
+            #                                                design_weight_2
+            #                                                            not_positive_or_null
+                (1,     'england',      3,       2.5,        1.0,        1),
+                (1,     'england',      3,       -1.5,       1.2,        None),
+                (1,     'england',      3,       -1.5,       None,       None),
             # fmt: on
         ],
         schema=schema,
     )
     input_df_not_pass = expected_df_not_pass.drop("not_positive_or_null")
 
-    with pytest.raises(DesignWeightError):
+    with pytest.raises(DesignWeightError) as weightexception:
         validate_design_weights(
             df=input_df_not_pass,
             num_households_by_cis_column="num_households",
@@ -51,23 +43,22 @@ def test_precal_and_design_weights_checkpoints(spark_session):
             country_column="country",
             rounding_value=18,
         )
+        assert "Antibody design weights do not sum to country population totals" in weightexception.value
+        assert "Swab design weights do not sum to cis area population totals." in weightexception.value
+        assert "records have negative design weights." in weightexception.value
+        assert "records with null swab or antibody design weights." in weightexception.value
 
     # test to pass ------------------------
     expected_df_pass = spark_session.createDataFrame(
         data=[
             # fmt: off
             #    id     country         num_households
-            #                                   groupby     sample_group
-            #                                                           design_weight_1
-            #                                                                       design_weight_2
-            #                                                                                   not_positive_or_null
-                (1,     'england',      21,     3.0,        'new',      7.0,        7.0,        None),
-                (1,     'england',      21,     3.0,        'new',      7.0,        7.0,        None),
-                (1,     'england',      21,     3.0,        'new',      7.0,        7.0,        None),
-
-                (1,     'england',      12,     3.0,        'old',      4.0,        4.0,        None),
-                (1,     'england',      12,     3.0,        'old',      4.0,        4.0,        None),
-                (1,     'england',      12,     3.0,        'old',      4.0,        4.0,        None),
+            #                                  design_weight_1
+            #                                               design_weight_2
+            #                                                           not_positive_or_null
+                (1,     'england',      21,     7.0,        7.0,        None),
+                (1,     'england',      21,     7.0,        7.0,        None),
+                (1,     'england',      21,     7.0,        7.0,        None),
             # fmt: on
         ],
         schema=schema,
@@ -77,14 +68,7 @@ def test_precal_and_design_weights_checkpoints(spark_session):
 
     input_df_pass = expected_df_pass.drop("not_positive_or_null")
 
-    (
-        swab_weight_column_type,
-        antibody_weight_column_type,
-        antibody_design_weights_sum_to_population,
-        swab_design_weights_sum_to_population,
-        check_negative_design_weights,
-        check_null_design_weights,
-    ) = validate_design_weights(
+    validate_design_weights(
         df=input_df_pass,
         num_households_by_cis_column="num_households",
         num_households_by_country_column="num_households",
@@ -92,12 +76,4 @@ def test_precal_and_design_weights_checkpoints(spark_session):
         antibody_weight_column="design_weight_2",
         cis_area_column="country",
         country_column="country",
-        swab_group_by_columns=["country", "groupby", "sample_group"],
     )
-
-    assert swab_weight_column_type is True
-    assert antibody_weight_column_type is True
-    assert antibody_design_weights_sum_to_population is True
-    assert swab_design_weights_sum_to_population is True
-    assert check_negative_design_weights is not True
-    assert check_null_design_weights is not True
