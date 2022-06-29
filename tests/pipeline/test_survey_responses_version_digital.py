8359936b61e0709ad4b27d1eda8c15cb85380d93
David Foster <foster.dev999@gmail.com>
2022-06-29 13:05:10 +0100

    Skip out of scope tests and add new mark

--- tests/pipeline/test_survey_responses_version_digital.py
+++ tests/pipeline/test_survey_responses_version_digital.py
@@ -1,6 +1,7 @@
 import pytest
 
 
+@pytest.mark.regression
 @pytest.mark.integration
 def test_responses_digital_df(responses_digital_ETL_output, regression_test_df):
     regression_test_df(
@@ -10,6 +11,7 @@ def test_responses_digital_df(responses_digital_ETL_output, regression_test_df):
     )  # remove source file column, as it varies for our temp dummy data
 
 
+@pytest.mark.regression
 @pytest.mark.integration
 def test_responses_digital_schema(regression_test_df_schema, responses_digital_ETL_output):
     regression_test_df_schema(responses_digital_ETL_output, "processed_responses_digital")  # check
