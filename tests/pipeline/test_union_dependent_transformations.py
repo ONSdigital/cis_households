35df17186fbd8c22aca5bee5875565ddf5945f31
David Foster <foster.dev999@gmail.com>
2022-06-29 14:19:53 +0100

    Remove regression testing from post union test

--- tests/pipeline/test_union_dependent_transformations.py
+++ tests/pipeline/test_union_dependent_transformations.py
@@ -6,15 +6,15 @@ from cishouseholds.pipeline.pipeline_stages import union_dependent_cleaning
 from cishouseholds.pipeline.pipeline_stages import union_dependent_derivations
 
 
-@pytest.fixture(scope="session")
-def union_transformed_output(
+def test_union_dependent_transformations(
     responses_v0_survey_ETL_output,
     responses_v1_survey_ETL_output,
     responses_v2_survey_ETL_output,
     responses_digital_ETL_output,
 ):
     """
-    Generate dummy survey responses digital.
+    Check that pyspark can build a valid plan for union-dependent processing steps, given outputs from input processing.
+    Should highlight any column name reference errors.
     """
     df = union_multiple_tables(
         [
@@ -27,18 +27,3 @@ def union_transformed_output(
     df = fill_forwards_transformations(df)
     df = union_dependent_cleaning(df)
     df = union_dependent_derivations(df)
-
-    df = df.drop("survey_response_source_file")
-    return df
-
-
-@pytest.mark.integration
-def test_union_transformed_output(union_transformed_output, regression_test_df):
-    regression_test_df(
-        union_transformed_output, "visit_id", "processed_post_union_data_regression"
-    )  # remove source file column, as it varies for our temp dummy data
-
-
-@pytest.mark.integration
-def test_union_transformed_schema(regression_test_df_schema, union_transformed_output):
-    regression_test_df_schema(union_transformed_output, "processed_post_union_schema_regression")  # check
