046f8f5f5db924084035936bbf84cf8b3c47d36d
David Foster <foster.dev999@gmail.com>
2022-06-30 13:48:52 +0100

    Move cohort cleaning to generic processing

--- cishouseholds/pipeline/high_level_transformations.py
+++ cishouseholds/pipeline/high_level_transformations.py
@@ -1100,6 +1100,9 @@ def transform_survey_responses_generic(df: DataFrame) -> DataFrame:
         "work_not_from_home_days_per_week",
         {"NA": "99", "N/A (not working/in education etc)": "99", "up to 1": "0.5"},
     )
+    df = df.withColumn(
+        "study_cohort", F.when(F.col("study_cohort").isNull(), "Original").otherwise(F.col("study_cohort"))
+    )
     return df
 
 
@@ -1881,9 +1884,6 @@ def union_dependent_derivations(df):
         visit_date_column="visit_datetime",
         record_changed_column="cis_covid_vaccine_received",
         record_changed_value="Yes",
-    )
-    df = df.withColumn(
-        "study_cohort", F.when(F.col("study_cohort").isNull(), "Original").otherwise(F.col("study_cohort"))
     )
     return df
 
