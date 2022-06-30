4face9f7c5705c77e1ca48c546e00619000d53f3
Ohi Bokth <bokth89@gmail.com>
2022-06-30 11:53:40 +0100

    changed location of study_cohort to union_dependent_trans because in v0 isnt created

--- cishouseholds/pipeline/high_level_transformations.py
+++ cishouseholds/pipeline/high_level_transformations.py
@@ -1067,10 +1067,6 @@ def transform_survey_responses_generic(df: DataFrame) -> DataFrame:
         reference_column="email_address",
         pattern=r"/^w+[+.w-]*@([w-]+.)*w+[w-]*.([a-z]{2,4}|d+)$/i",
     )
-    df = df.withColumn(
-        "study_cohort", F.when(F.col("study_cohort").isNull(), "Original").otherwise(F.col("study_cohort"))
-    )
-
     df = clean_postcode(df, "postcode")
     df = assign_outward_postcode(df, "outward_postcode", reference_column="postcode")
 
@@ -1872,6 +1868,9 @@ def union_dependent_derivations(df):
         visit_date_column="visit_datetime",
         record_changed_column="cis_covid_vaccine_received",
         record_changed_value="Yes",
+    )
+    df = df.withColumn(
+        "study_cohort", F.when(F.col("study_cohort").isNull(), "Original").otherwise(F.col("study_cohort"))
     )
     return df
 
