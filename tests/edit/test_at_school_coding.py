a45fcc0190c205c186b65b3388481f932a5d987a
Sashi <SashiDareddy@users.noreply.github.com>
2022-07-06 10:11:26 +0100

    2314 Added at-school regex (#596)
    * refactored add_work_from_home_identifier to be a wrapper around a generic assign_regex_match_result function
    
    * added at-school regex pattern
    
    * added positive & negative pattern matching
    
    * updated docstring & added debug_mode
    
    * refactored add_work_from_home_identifier and add_at_school_identifier conforming to new function signature.
    
    * added negative test cases for at-school
    
    * removed redundant parenthesis
    
    * moved test cases from .yml into .py
    
    Co-authored-by: David Foster <31405412+foster999@users.noreply.github.com>

---
+++ tests/edit/test_at_school_coding.py
@@ -0,0 +1,23 @@
+import pytest
+import yaml
+from at_school_testcases import test_data
+from chispa import assert_df_equality
+
+from cishouseholds.edit import add_at_school_identifier
+
+
+@pytest.fixture
+def at_school_test_cases():
+    test_data_melted = [
+        (test_case, pos_or_neg == "positive")
+        for pos_or_neg, test_cases in test_data.items()
+        for test_case in test_cases
+    ]
+    return test_data_melted
+
+
+def test_add_at_school_identifier(at_school_test_cases, spark_session):
+
+    expected_df = spark_session.createDataFrame(at_school_test_cases, schema="test_case string, at_school boolean")
+    actual_df = add_at_school_identifier(df=expected_df.drop("at_school"), columns_to_check_in=["test_case"])
+    assert_df_equality(actual_df, expected_df, ignore_row_order=True, ignore_column_order=True, ignore_nullable=True)
