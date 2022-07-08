c19ab78deaff6ccad331d0eee1aa8aa98846912d
lamots <Stefen.Lamothe@ext.ons.gov.uk>
2022-07-08 15:14:41 +0100

    refactored soc collection

--- cishouseholds/pipeline/pipeline_stages.py
+++ cishouseholds/pipeline/pipeline_stages.py
@@ -441,7 +441,8 @@ def process_soc_data(
     """
     Process soc data and combine result with survey responses data
     """
-    window = Window.partitionBy("work_main_job_title", "work_main_job_role")
+    join_on_columns = ["work_main_job_title", "work_main_job_role"]
+    window = Window.partitionBy(*join_on_columns)
     inconsistances_resolution_df = extract_from_table(inconsistances_resolution_table).withColumnRenamed(
         "Gold SOC2010 code", "resolved_soc_code"
     )
@@ -451,27 +452,21 @@ def process_soc_data(
         inconsistances_resolution_df.withColumnRenamed(
             "standard_occupational_classification_code", "resolved_soc_code"
         ),
-        on=["work_main_job_title", "work_main_job_role"],
+        on=join_on_columns,
         how="left",
     )
-    soc_lookup_df = (
-        soc_lookup_df.withColumn(
-            "standard_occupational_classification_code",
-            F.coalesce(F.col("resolved_soc_code"), F.col("standard_occupational_classification_code")),
-        )
-        .drop("resolved_soc_code")
-        .distinct()
-    )
-
-    duplicate_rows_df = soc_lookup_df.filter(F.count("*").over(window) > 1)
-    soc_lookup_df = soc_lookup_df.filter(F.count("*").over(window) == 1)
+    soc_lookup_df = soc_lookup_df.withColumn(
+        "standard_occupational_classification_code",
+        F.coalesce(F.col("resolved_soc_code"), F.col("standard_occupational_classification_code")),
+    ).drop("resolved_soc_code")
+    filter_condition = F.count(*join_on_columns).over(window) > 1
+    duplicate_rows_df = soc_lookup_df.filter(filter_condition)
+    soc_lookup_df = soc_lookup_df.filter(~filter_condition)
 
     update_table(duplicate_soc_rows_table, duplicate_rows_df, "overwrite", archive=True)
 
     soc_lookup_df = transform_cis_soc_data(soc_lookup_df)
-    survey_responses_df = survey_responses_df.join(
-        soc_lookup_df, on=["work_main_job_title", "work_main_job_role"], how="left"
-    )
+    survey_responses_df = survey_responses_df.join(soc_lookup_df, on=join_on_columns, how="left")
     update_table(survey_responses_df, soc_coded_survey_responses_table, "overwrite")
     update_table(soc_lookup_df, transformed_soc_lookup_table, "overwrite")
 
