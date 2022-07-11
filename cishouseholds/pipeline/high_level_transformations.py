69e5836ba4dea976d3fa40cb2e6ed8953675c0b7
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-07-11 11:19:06 +0100

    added retired regex pattern

--- cishouseholds/pipeline/high_level_transformations.py
+++ cishouseholds/pipeline/high_level_transformations.py
@@ -2285,13 +2285,13 @@ def add_pattern_matching_flags(df: DataFrame) -> DataFrame:
         column_name_to_assign="at_university",
         debug_mode=False,
     )
-
+    # add is-retired flag
     df = assign_regex_match_result(
         df=df,
         columns_to_check_in=["work_main_job_title", "work_main_job_role"],
         positive_regex_pattern=retired_regex_pattern.positive_regex_pattern,
         negative_regex_pattern=retired_regex_pattern.negative_regex_pattern,
-        column_name_to_assign="retired",
+        column_name_to_assign="is_retired",
         debug_mode=False,
     )
 
