61f08c97a7734eb0fe4bffad65f4820e45a3bf83
David Foster <foster.dev999@gmail.com>
2022-07-19 16:33:41 +0100

    Add parameter for tranch strata

--- cishouseholds/weights/design_weights.py
+++ cishouseholds/weights/design_weights.py
@@ -27,7 +27,7 @@ def generate_weights(
     postcode_lookup_df: DataFrame,
     country_lookup_df: DataFrame,
     lsoa_cis_lookup_df: DataFrame,
-    strata_variable: str,
+    tranche_strata_columns: List[str],
     first_run: bool,
 ):
     df = join_and_process_lookups(
@@ -52,10 +52,10 @@ def generate_weights(
         df = assign_tranche_factor(
             df=df,
             column_name_to_assign="tranche_factor",
-            column_to_count=strata_variable,
+            column_to_count="ons_household_id",
             barcode_ref_column="TRANCHE_BARCODE_REF",
             tranche_column="tranche",
-            group_by_columns=["cis_area_code_20", "enrolment_date"],
+            group_by_columns=tranche_strata_columns,
         )
     else:
         df = df.withColumn("tranche_eligible_households", F.lit("No"))
