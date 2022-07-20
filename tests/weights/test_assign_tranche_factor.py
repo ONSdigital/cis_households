20ad27096b109e4d15775721c762aeb5a2588c67
David Foster <foster.dev999@gmail.com>
2022-07-20 14:39:23 +0100

    Fix tranche factor test

--- tests/weights/test_assign_tranche_factor.py
+++ tests/weights/test_assign_tranche_factor.py
@@ -6,15 +6,14 @@ from cishouseholds.weights.derive import assign_tranche_factor
 def test_assign_tranche_factor(spark_session):
     expected_df = spark_session.createDataFrame(
         data=[
-            ("A", "A", "J1", 1, "Yes", 2, 0, None),
-            ("B", "B", "J2", 2, "Yes", 2, 2, 1.0),
-            ("C", "C", "J2", 2, "Yes", 2, 2, 1.0),
-            ("D", "D", "J1", 2, "Yes", 2, 1, 2.0),
-            (None, None, "J1", 1, "No", 0, 0, None),
+            ("A", "J1", 1, "Yes", 2, 0, None),
+            ("B", "J2", 2, "Yes", 2, 2, 1.0),
+            ("C", "J2", 2, "Yes", 2, 2, 1.0),
+            ("D", "J1", 2, "Yes", 2, 1, 2.0),
+            (None, "J1", 1, "No", 0, 0, None),
         ],
         schema="""
             barcode string,
-            barcode_ref string,
             groupby string,
             tranche_number_indicator integer,
             tranche_eligible_households string,
@@ -36,7 +35,7 @@ def test_assign_tranche_factor(spark_session):
     )
     assert_df_equality(
         output_df,
-        expected_df.drop("barcode_ref"),
+        expected_df,
         ignore_nullable=True,
         ignore_row_order=True,
         ignore_column_order=True,
