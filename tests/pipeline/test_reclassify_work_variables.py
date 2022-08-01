5399748587af75fe5127c48e17b1c76c11406aef
Sashikanth Dareddy <sashikanth.r.dareddy@ext.ons.gov.uk>
2022-08-01 14:55:06 +0100

    test cases now read from csv

--- tests/pipeline/test_reclassify_work_variables.py
+++ tests/pipeline/test_reclassify_work_variables.py
@@ -2,114 +2,69 @@ import pandas as pd
 import pytest
 from chispa.dataframe_comparer import assert_df_equality
 from pyspark.sql import functions as F
+from pyspark.sql import types as t
 
 from cishouseholds.pipeline.high_level_transformations import reclassify_work_variables
 
 
+@pytest.fixture
+def load_test_cases():
+    test_data = pd.read_csv("tests/pipeline/test_reclassify_work_variables/test-cases.csv")
+
+    expected_data = test_data.query("record_type=='expected' and row_id==1").drop(columns=["record_type"])
+
+    input_data = test_data.query("record_type=='input' and row_id==1").drop(
+        columns=["record_type"] + [col for col in test_data.columns if col.endswith("edited")]
+    )
+
+    return input_data, expected_data
+
+
 @pytest.mark.integration
-def test_reclassify_work_variables(spark_session):
+def test_reclassify_work_variables(spark_session, load_test_cases):
     """Test that high level work variables reclassification is handled correctly"""
 
-    test_data = [
-        (
-            0,
-            "input",
-            None,
-            None,
-            "WORKS FROM HOME",
-            "ADMINSTRATOR",
-            54,
-            None,
-            "Employed",
-            "Employed and currently working",
-            "Employed and currently working",
-            None,
-            None,
-            None,
-            None,
-        ),
-        (
-            0,
-            "expected",
-            None,
-            None,
-            "ADMIN - WORKS FROM HOME",
-            "ADMINSTRATOR",
-            54,
-            "Working from home",
-            "Employed",
-            "Employed and currently working",
-            "Employed and currently working",
-            True,
-            False,
-            False,
-            False,
-        ),
-        (
-            1,
-            "input",
-            None,
-            None,
-            "ASSISTANT SCHOOL TEACHER",
-            "FURLOWED",
-            70,
-            "OFFICE",
-            "Employed",
-            "Employed and currently not working",
-            "Employed and currently not working",
-            None,
-            None,
-            None,
-            None,
-        ),
-        (
-            1,
-            "expected",
-            None,
-            None,
-            "ASSISTANT SCHOOL TEACHER",
-            "FURLOWED",
-            70,
-            "OFFICE",
-            "Furloughed (temporarily not working)",
-            "Employed and currently not working",
-            "Employed and currently not working",
-            False,
-            True,
-            False,
-            False,
-        ),
-    ]
+    input_data, expected_data = load_test_cases
 
-    schema = [
-        "row_id integer",
-        "record_type string",
-        "main_job string",
-        "main_resp string",
-        "work_main_job_title string",
-        "work_main_job_role string",
-        "age_at_visit integer",
-        "work_location string",
-        "work_status_v0 string",
-        "work_status_v1 string",
-        "work_status_v2 string",
-        "work_location_edited boolean",
-        "work_status_v0_edited boolean",
-        "work_status_v1_edited boolean",
-        "work_status_v2_edited boolean",
-    ]
-
-    expected_df = (
-        spark_session.createDataFrame(test_data, schema=",".join(schema))
-        .filter("record_type=='expected'")
-        .drop("record_type")
+    input_schema = t.StructType(
+        [
+            t.StructField("row_id", t.IntegerType()),
+            t.StructField("rule", t.StringType()),
+            t.StructField("main_job", t.StringType()),
+            t.StructField("main_resp", t.StringType()),
+            t.StructField("work_main_job_title", t.StringType()),
+            t.StructField("work_main_job_role", t.StringType()),
+            t.StructField("age_at_visit", t.IntegerType()),
+            t.StructField("work_location", t.StringType()),
+            t.StructField("work_status_v0", t.StringType()),
+            t.StructField("work_status_v1", t.StringType()),
+            t.StructField("work_status_v2", t.StringType()),
+        ]
     )
 
-    input_df = (
-        spark_session.createDataFrame(test_data, schema=",".join(schema))
-        .filter("record_type=='input'")
-        .drop("record_type", *[col for col in expected_df.columns if col.endswith("edited")])
+    expected_schema = t.StructType(
+        [
+            t.StructField("row_id", t.IntegerType()),
+            t.StructField("rule", t.StringType()),
+            t.StructField("main_job", t.StringType()),
+            t.StructField("main_resp", t.StringType()),
+            t.StructField("work_main_job_title", t.StringType()),
+            t.StructField("work_main_job_role", t.StringType()),
+            t.StructField("age_at_visit", t.IntegerType()),
+            t.StructField("work_location", t.StringType()),
+            t.StructField("work_status_v0", t.StringType()),
+            t.StructField("work_status_v1", t.StringType()),
+            t.StructField("work_status_v2", t.StringType()),
+            t.StructField("work_location_edited", t.BooleanType(), False),
+            t.StructField("work_status_v0_edited", t.BooleanType()),
+            t.StructField("work_status_v1_edited", t.BooleanType()),
+            t.StructField("work_status_v2_edited", t.BooleanType()),
+        ]
     )
+
+    expected_df = spark_session.createDataFrame(expected_data, schema=expected_schema).replace("NaN", None)
+
+    input_df = spark_session.createDataFrame(input_data, schema=input_schema).replace("NaN", None)
 
     actual_df = reclassify_work_variables(input_df, spark_session=spark_session)
 
@@ -117,6 +72,6 @@ def test_reclassify_work_variables(spark_session):
         actual_df,
         expected_df,
         ignore_row_order=False,
-        ignore_column_order=True,
-        ignore_nullable=True,
+        ignore_column_order=False,
+        ignore_nullable=False,
     )
