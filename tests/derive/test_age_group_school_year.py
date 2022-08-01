849bf18da598819c221e1a957c7473df757cc0fe
David Foster <31405412+foster999@users.noreply.github.com>
2022-08-01 16:53:35 +0100

    2395 Edit NI to Northern Ireland (#652)
    * Edit NI to Northern Ireland
    
    * Edit NI to Northern Ireland

--- tests/derive/test_age_group_school_year.py
+++ tests/derive/test_age_group_school_year.py
@@ -7,16 +7,16 @@ def test_assign_age_group_school_year(spark_session):
     expected_df = spark_session.createDataFrame(
         data=[
             ("England", 6, 2, "02-6SY"),
-            ("NI", 9, 5, "02-6SY"),
+            ("Northern Ireland", 9, 5, "02-6SY"),
             ("Scotland", 11, 7, "07SY-11SY"),
             ("Wales", 15, 10, "07SY-11SY"),
             ("Wales", 15, 6, "07SY-11SY"),
             ("Scotland", 15, 6, None),
             ("England", 17, 12, "12SY-24"),
-            ("NI", 18, 13, "12SY-24"),
+            ("Northern Ireland", 18, 13, "12SY-24"),
             ("England", 25, 12, "25-34"),
-            ("NI", 55, 79, "50-69"),
-            ("NI", 88, 1, "70+"),
+            ("Northern Ireland", 55, 79, "50-69"),
+            ("Northern Ireland", 88, 1, "70+"),
         ],
         schema="country string, age integer, school_year integer, output string",
     )
