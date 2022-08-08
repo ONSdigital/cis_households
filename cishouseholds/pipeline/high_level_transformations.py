a7653898bcad3ee3b727549849d6b579015d1419
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-08-08 13:09:20 +0100

    updated test for lab report

--- cishouseholds/pipeline/high_level_transformations.py
+++ cishouseholds/pipeline/high_level_transformations.py
@@ -148,18 +148,22 @@ from cishouseholds.pyspark_utils import get_or_create_spark_session
 from cishouseholds.validate_class import SparkValidate
 
 
-def generate_lab_report(df: DataFrame) -> DataFrame:
+def generate_lab_report(df: DataFrame, current_date=F.current_timestamp()) -> DataFrame:
     """
     Generate lab report of latest 7 days of results
     """
-    df = df.filter(F.date_sub(F.current_timestamp(), 7) < F.col("file_date"))
+    df = df.filter(F.date_sub(current_date, 7) < F.col("file_date"))
     swab_df = df.select("swab_sample_barcode", "swab_taken_datetime", "survey_completed_datetime").filter(
-        ((F.col("swab_taken_datetime").isNull()) & (F.col("survey_completed_datetime").isNull()))
-        | F.col("swab_sample_barcode").isNull()
+        ~(
+            ((F.col("swab_taken_datetime").isNull()) & (F.col("survey_completed_datetime").isNull()))
+            | F.col("swab_sample_barcode").isNull()
+        )
     )
     blood_df = df.select("blood_sample_barcode", "blood_taken_datetime", "survey_completed_datetime").filter(
-        ((F.col("blood_taken_datetime").isNull()) & (F.col("survey_completed_datetime").isNull()))
-        | F.col("blood_sample_barcode").isNull()
+        ~(
+            ((F.col("blood_taken_datetime").isNull()) & (F.col("survey_completed_datetime").isNull()))
+            | F.col("blood_sample_barcode").isNull()
+        )
     )
     return swab_df, blood_df
 
--- tests/pipeline/test_generate_lab_report.py
+++ tests/pipeline/test_generate_lab_report.py
@@ -10,7 +10,9 @@ def test_generate_lab_report(spark_session):
         data=[
             ("2022-05-17", "1", "1", "1", "1", "1"),
             ("2022-08-01", "2", "2", "2", "2", "2"),
-            ("2022-08-02", "3", None, "3", "3", "3"),
+            ("2022-08-11", "3", None, "3", "3", "3"),  # only 1 datetime present
+            ("2022-08-09", None, "4", "4", None, "4"),  # blood only records
+            ("2022-08-11", "5", None, None, None, None),  # no datetime present
         ],
         schema="file_date string, swab_sample_barcode string, blood_taken_datetime string, blood_sample_barcode string, swab_taken_datetime string, survey_completed_datetime string",
     )
@@ -19,11 +21,13 @@ def test_generate_lab_report(spark_session):
         schema="swab_sample_barcode string, swab_taken_datetime string, survey_completed_datetime string",
     )
     expected_blood_df = spark_session.createDataFrame(
-        data=[("2", "2", "2")],
+        data=[("2", "2", "2"), ("3", None, "3"), ("4", "4", "4")],
         schema="blood_sample_barcode string, blood_taken_datetime string, survey_completed_datetime string",
     )
     input_df = input_df.withColumn("file_date", F.to_timestamp(F.col("file_date"), format="yyyy-MM-dd"))
-    output_swab_df, output_blood_df = generate_lab_report(input_df)
+    output_swab_df, output_blood_df = generate_lab_report(
+        input_df, F.to_timestamp(F.lit("2022-08-04"), format="yyyy-MM-dd")
+    )
 
     assert_df_equality(output_swab_df, expected_swab_df, ignore_nullable=True, ignore_row_order=True)
     assert_df_equality(output_blood_df, expected_blood_df, ignore_nullable=True, ignore_row_order=True)
