e1fce14ee58a18b17abe054c4e9ecd979668333f
Ohi Bokth <bokth89@gmail.com>
2022-08-11 09:30:43 +0100

    correction: changed vaccination type from v2 to digital

--- dummy_data_generation/schemas.py
+++ dummy_data_generation/schemas.py
@@ -1398,9 +1398,6 @@ def get_voyager_2_data_description(_, blood_barcodes, swab_barcodes):
                 "Sinovac",
                 "Sputnik",
                 "Valneva",
-                "Or Another vaccine please specify",
-                "I do not know the type",
-                "Or do you not know which one you had?",
                 None,
             ],
         ),
@@ -1532,6 +1529,9 @@ type_of_vaccination = [
     "From a research study/trial",
     "Another vaccine please specify",
     "I don't know the type",
+    "Or Another vaccine please specify",
+    "I do not know the type",
+    "Or do you not know which one you had?",
     None,
 ]
 
