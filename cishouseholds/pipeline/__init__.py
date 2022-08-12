c7335de57ca5340753d4e836691099a2891adfb6
David Foster <31405412+foster999@users.noreply.github.com>
2022-08-12 17:06:37 +0100

    2412 Delete merge and post merge code (#668)
    * Remove merge and post merge code
    
    * Delete unused test
    
    * Remove rpy2 dependency
    
    * Move conftest into tests dir
    
    * Delete unused package level variables
    
    * Fix spelling in test cases

--- cishouseholds/pipeline/__init__.py
+++ cishouseholds/pipeline/__init__.py
@@ -2,10 +2,6 @@ import os
 
 from pyspark.sql import DataFrame
 
-consent_blood_test = "Consent_to_Blood_Test"
-consent_finger_prick_a1_a3 = "Consent_to_Finger_prick_A1_A3"
-consent_extend_study_under_16_b1_b3 = "Consent_to_extend_study_uhfhfnder_16_B1_B3"
-
 
 def custom_checkpoint(self, *args, **kwargs):
     """
