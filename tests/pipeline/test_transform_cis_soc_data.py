c454e72f84359dc3fb3987a09aecb0940954622b
David Foster <31405412+foster999@users.noreply.github.com>
2022-08-12 15:50:41 +0100

    2370 Update work description text cleaning (#667)
    * Update job description cleaning
    
    * Update function calls
    
    * Split hyphen removal
    
    * Update regression outputs
    
    * Update SOC test to assume upper
    
    Co-authored-by: Stefen Lamothe <78305473+slamothe99@users.noreply.github.com>

--- tests/pipeline/test_transform_cis_soc_data.py
+++ tests/pipeline/test_transform_cis_soc_data.py
@@ -1,4 +1,3 @@
-import pytest
 from chispa import assert_df_equality
 
 from cishouseholds.pipeline.high_level_transformations import transform_cis_soc_data
@@ -7,20 +6,20 @@ from cishouseholds.pipeline.high_level_transformations import transform_cis_soc_
 def test_transform_cis_soc_data(spark_session):
     input_df = spark_session.createDataFrame(
         data=[
-            ("job title 1", "job1", 5),
-            ("job title 1", "job1", 5),
-            ("job title 2", "job2", 6),
-            ("job title 2", "job2", 66),
-            ("job title 2", "job2", 77),
-            ("job title 2", "job2", "un"),
-            ("job title 3", "job3", "un"),
-            (None, "job3", "un"),
+            ("JOB TITLE 1", "JOB1", 5),
+            ("JOB TITLE 1", "JOB1", 5),
+            ("JOB TITLE 2", "JOB2", 6),
+            ("JOB TITLE 2", "JOB2", 66),
+            ("JOB TITLE 2", "JOB2", 77),
+            ("JOB TITLE 2", "JOB2", "un"),
+            ("JOB TITLE 3", "JOB3", "un"),
+            (None, "JOB3", "un"),
             (None, None, None),
         ],
         schema="work_main_job_title string, work_main_job_role string, standard_occupational_classification_code string",
     )
     expected_df = spark_session.createDataFrame(
-        data=[("job title 1", "JOB1", 5), ("job title 2", "JOB2", 66), ("job title 2", "JOB2", 77)],
+        data=[("JOB TITLE 1", "JOB1", 5), ("JOB TITLE 2", "JOB2", 66), ("JOB TITLE 2", "JOB2", 77)],
         schema="work_main_job_title string, work_main_job_role string, standard_occupational_classification_code string",
     )
     # test mapping functionality with complete map off
