7ff3315a47b59634ee3b606ca2d26560c60885cb
Sashikanth Dareddy <sashikanth.r.dareddy@ext.ons.gov.uk>
2022-08-22 15:13:08 +0100

    fixed test cases

--- tests/derive/test_flag_records_for_student_rules.py
+++ tests/derive/test_flag_records_for_student_rules.py
@@ -76,20 +76,22 @@ def test_flag_records_for_school_v2_rules(spark_session):
     """Test flag_records_for_school_v2_rules function correctly flags the records"""
 
     test_cases = [
-        (1, False),
-        (5, True),
-        (12, True),
-        (19, False),
+        (1, None, False),
+        (5, 8, True),
+        (12, None, False),
+        (19, None, False),
     ]
 
-    expected_df = spark_session.createDataFrame(test_cases, schema="age_at_visit int, actual_flag boolean")
+    expected_df = spark_session.createDataFrame(
+        test_cases, schema="age_at_visit int, school_year int, actual_flag boolean"
+    )
 
     actual_df = expected_df.drop("actual_flag").withColumn("actual_flag", flag_records_for_school_v2_rules())
 
     assert_df_equality(
         actual_df,
         expected_df,
-        ignore_row_order=True,
+        ignore_row_order=False,
         ignore_column_order=True,
         ignore_nullable=True,
     )
