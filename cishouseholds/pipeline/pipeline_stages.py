e0d3e32235f7023b21fcf9f82137a8c9fca72b36
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-09-22 09:24:06 +0100

    added soc code nulls to uncodeable after join

--- cishouseholds/pipeline/pipeline_stages.py
+++ cishouseholds/pipeline/pipeline_stages.py
@@ -391,6 +391,10 @@ def process_soc_data(
 
     duplicate_rows_df, soc_lookup_df = transform_cis_soc_data(soc_lookup_df, join_on_columns)
     survey_responses_df = survey_responses_df.join(soc_lookup_df, on=join_on_columns, how="left")
+    survey_responses_df = survey_responses_df.withColumn(
+        "standard_occupational_classification_code",
+        F.when(F.col("standard_occupational_classification_code").isNull(), "uncodeable"),
+    )
 
     update_table(duplicate_rows_df, duplicate_soc_rows_table, "overwrite", archive=True)
     update_table(survey_responses_df, soc_coded_survey_responses_table, "overwrite")
