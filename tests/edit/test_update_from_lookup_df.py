dc83d971e8bb75546167648774aee66916e9567c
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-09-22 12:28:28 +0100

    added functionality for id column spec

--- tests/edit/test_update_from_lookup_df.py
+++ tests/edit/test_update_from_lookup_df.py
@@ -7,31 +7,33 @@ def test_update_from_lookup_df(spark_session):
     map_df = spark_session.createDataFrame(
         # All fields are read as string from csv (mixed types)
         data=[
-            ("1", None, "A", "original", "edited"),
-            ("2", None, "B", "1", "2"),
-            ("3", None, "A", "original", "edited"),
-            ("3", None, "B", "10", "20"),
-            ("3", None, "C", None, None),  # Non existent column just prints warning
-            ("5", None, "B", None, None),  # Non existent id should not be joined
+            ("id1", "1", None, "A", "original", "edited"),
+            ("id1", "2", None, "B", "1", "2"),
+            ("id1", "3", None, "A", "original", "edited"),
+            ("id1", "3", None, "B", "10", "20"),
+            ("id1", "3", None, "D", None, None),  # Non existent column just prints warning
+            ("id1", "5", None, "B", None, None),  # Non existent id should not be joined
+            ("id2", "A", None, "C", 0, 1),  # edits multiple rows given same id
         ],
-        schema="""id string, dataset_name string, target_column_name string, old_value string, new_value string""",
+        schema="""id_column_name string, id string, dataset_name string, target_column_name string, old_value string, new_value string""",
     )
+    schema = "id1 string, id2 string, A string, B integer, C integer"
     input_df = spark_session.createDataFrame(
         data=[
-            ("1", "original", None),
-            ("2", None, 1),
-            ("3", "original", 10),
+            ("1", "A", "original", None, None),
+            ("2", "A", None, 1, 0),
+            ("3", "A", "original", 10, 0),
         ],
-        schema="""id string, A string, B integer""",
+        schema=schema,
     )
     expected_df = spark_session.createDataFrame(
         data=[
-            ("1", "edited", None),
-            ("2", None, 2),
-            ("3", "edited", 20),
+            ("1", "A", "edited", None, None),
+            ("2", "A", None, 2, 1),
+            ("3", "A", "edited", 20, 1),
         ],
-        schema="""id string, A string, B integer""",
+        schema=schema,
     )
 
-    output_df = update_from_lookup_df(input_df, map_df, id_column="id")
+    output_df = update_from_lookup_df(input_df, map_df)
     assert_df_equality(expected_df, output_df, ignore_nullable=True, ignore_column_order=True)
