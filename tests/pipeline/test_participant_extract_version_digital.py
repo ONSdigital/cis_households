55934fe84397b800f630e5473fae9db119d5056d
Matthew Maynard <matthew.maynard@ons.gov.uk>
2022-10-20 14:32:51 +0100

    enable integration testing for new input stage

--- tests/pipeline/test_participant_extract_version_digital.py
+++ tests/pipeline/test_participant_extract_version_digital.py
@@ -3,15 +3,15 @@ import pytest
 
 # @pytest.mark.regression
 # @pytest.mark.integration
-# def test_participant_extract_digital_df(participants_digital_ETL_output, regression_test_df):
-#     regression_test_df(
-#         participants_digital_ETL_output.drop("survey_response_source_file"),
-#         "participant_id",
-#         "processed_participants_digital",  # Check
-#     )  # remove source file column, as it varies for our temp dummy data
+def test_participant_extract_digital_df(participants_digital_ETL_output, regression_test_df):
+    regression_test_df(
+        participants_digital_ETL_output.drop("survey_response_source_file"),
+        "participant_id",
+        "processed_participants_digital",  # Check
+    )  # remove source file column, as it varies for our temp dummy data
 
 
 # @pytest.mark.regression
 # @pytest.mark.integration
-# def test_participant_extract_digital_schema(regression_test_df_schema, participants_digital_ETL_output):
-#     regression_test_df_schema(participants_digital_ETL_output, "processed_participants_digital")  # check
+def test_participant_extract_digital_schema(regression_test_df_schema, participants_digital_ETL_output):
+    regression_test_df_schema(participants_digital_ETL_output, "processed_participants_digital")  # check
