05619eb85be1da55e510ada62249ae1bc592df0f
kamila-m <74770530+kamila-m@users.noreply.github.com>
2022-10-24 09:59:59 +0100

    2451 - participant extract input processing (#767)
    * participant extract changes
    
    * set up stage for participant extract processing
    
    requires creation of transformation functions for new stage
    
    * added participant extract high level function
    
    * participant ticket
    
    * enable integration testing for new input stage
    
    * reran regressions
    
    Co-authored-by: Matthew Maynard <matthew.maynard@ons.gov.uk>
    Co-authored-by: izpickle <103201130+izpickle@users.noreply.github.com>

--- cishouseholds/pipeline/high_level_transformations.py
+++ cishouseholds/pipeline/high_level_transformations.py
@@ -164,6 +164,63 @@ from cishouseholds.validate_class import SparkValidate
 # from cishouseholds.pipeline.regex_patterns import healthcare_bin_pattern
 
 
+def transform_participant_extract_digital(df: DataFrame) -> DataFrame:
+    """
+    transform and process participant extract data received from cis digital
+    """
+    col_val_map = {
+        "withdrawn_reason": {
+            "Moving Location": "Moving location",
+            "Bad experience with tester / survey": "Bad experience with interviewer/survey",
+            "Swab / blood process too distressing": "Swab/blood process too distressing",
+            "Do NOT Reinstate": "",
+        },
+        "ethnicity": {
+            "African": "Black,Caribbean,African-African",
+            "Caribbean": "Black,Caribbean,Afro-Caribbean",
+            "Any other Black or African or Carribbean background": "Any other Black background",
+            "Any other Mixed/Multiple background": "Any other Mixed background",
+            "Bangladeshi": "Asian or Asian British-Bangladeshi",
+            "Chinese": "Asian or Asian British-Chinese",
+            "English, Welsh, Scottish, Northern Irish or British": "White-British",
+            "Indian": "Asian or Asian British-Indian",
+            "Irish": "White-Irish",
+            "Pakistani": "Asian or Asian British-Pakistani",
+            "White and Asian": "Mixed-White & Asian",
+            "White and Black African": "Mixed-White & Black African",
+            "White and Black Caribbean": "Mixed-White & Black Caribbean",
+            "Gypsy or Irish Traveller": "White-Gypsy or Irish Traveller",
+            "Arab": "Other ethnic group-Arab",
+        },
+    }
+
+    ethnicity_map = {
+        "White": ["White-British", "White-Irish", "White-Gypsy or Irish Traveller", "Any other white background"],
+        "Asian": [
+            "Asian or Asian British-Indian",
+            "Asian or Asian British-Pakistani",
+            "Asian or Asian British-Bangladeshi",
+            "Asian or Asian British-Chinese",
+            "Any other Asian background",
+        ],
+        "Black": ["Black,Caribbean,African-African", "Black,Caribbean,Afro-Caribbean", "Any other Black background"],
+        "Mixed": [
+            "Mixed-White & Black Caribbean",
+            "Mixed-White & Black African",
+            "Mixed-White & Asian",
+            "Any other Mixed background",
+        ],
+        "Other": ["Other ethnic group-Arab", "Any other ethnic group"],
+    }
+
+    df = assign_column_from_mapped_list_key(
+        df=df, column_name_to_assign="ethnicity_group", reference_column="ethnic_group", map=ethnicity_map
+    )
+
+    df = apply_value_map_multiple_columns(df, col_val_map)
+    return df
+
+
 def transform_cis_soc_data(df: DataFrame, join_on_columns: List[str]) -> DataFrame:
     """
     transform and process cis soc data
