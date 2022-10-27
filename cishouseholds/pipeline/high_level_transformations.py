d3c795e9511c135a77e63a48233f10e90152d563
Stefen Lamothe <78305473+slamothe99@users.noreply.github.com>
2022-10-27 14:53:49 +0100

    2619 - ensure events are retained in fill forward (#774)
    * added handling of null visit date to event fill
    
    * added handling of null visit date to event fill
    
    * not complete
    
    * fix fill forwards event test
    
    * update tests for fill_forward_event
    
    Co-authored-by: izpickle <103201130+izpickle@users.noreply.github.com>
    Co-authored-by: Izzy Pickles <izzy.pickles@ons.gov.uk>

--- cishouseholds/pipeline/high_level_transformations.py
+++ cishouseholds/pipeline/high_level_transformations.py
@@ -2228,6 +2228,7 @@ def fill_forward_events_for_key_columns(df):
         ],
         participant_id_column="participant_id",
         visit_datetime_column="visit_datetime",
+        visit_id_column="visit_id",
     )
     # df_4 = get_or_create_spark_session().createDataFrame(
     #     df_3.rdd, schema=df_3.schema
@@ -2241,6 +2242,7 @@ def fill_forward_events_for_key_columns(df):
         detail_columns=["last_suspected_covid_contact_type"],
         participant_id_column="participant_id",
         visit_datetime_column="visit_datetime",
+        visit_id_column="visit_id",
     )
     # df_6 = get_or_create_spark_session().createDataFrame(
     #     df_5.rdd, schema=df_5.schema
@@ -2253,6 +2255,7 @@ def fill_forward_events_for_key_columns(df):
         detail_columns=["last_covid_contact_type"],
         participant_id_column="participant_id",
         visit_datetime_column="visit_datetime",
+        visit_id_column="visit_id",
     )
     return df
 
