65ccf8152ba0263b8bf9805cf1f40d1fe11457f0
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-11-08 11:24:15 +0000

    added union dependent transofmratins output test

--- tests/pipeline/test_transform_cis_soc_data.py
+++ tests/pipeline/test_transform_cis_soc_data.py
@@ -22,6 +22,12 @@ def test_transform_cis_soc_data(spark_session):
         ],
         schema="work_main_job_title string, work_main_job_role string, standard_occupational_classification_code string, another_column string",
     )
+    resolution_df = spark_session.createDataFrame(
+        data=[
+            ("JOB TITLE 1", "JOB1", 5),
+        ],
+        schema="work_main_job_title string, work_main_job_role string, resolved_soc_code string",
+    )
     expected_df = spark_session.createDataFrame(
         data=[
             ("JOB TITLE 1", "JOB1", 5, "A"),
@@ -40,11 +46,14 @@ def test_transform_cis_soc_data(spark_session):
             ("JOB TITLE 5", "JOB5", "uncodeable", "J", "more specific code available"),
             ("JOB TITLE 5", "JOB5", 7, "K", "more specific code available"),
             (None, None, None, "M", "null job title"),
+            (None, "JOB3", "un", "H", "null job title"),
         ],
         schema="work_main_job_title string, work_main_job_role string, standard_occupational_classification_code string, another_column string, drop_reason string",
     )
     # test mapping functionality with complete map off
-    duplicate_df, output_df = transform_cis_soc_data(input_df, ["work_main_job_role"])
+    duplicate_df, output_df = transform_cis_soc_data(
+        input_df, resolution_df, ["work_main_job_title", "work_main_job_role"]
+    )
 
     assert_df_equality(output_df, expected_df, ignore_nullable=True, ignore_row_order=True, ignore_column_order=True)
     assert_df_equality(
--- tests/pipeline/test_union_dependent_transformations.py
+++ tests/pipeline/test_union_dependent_transformations.py
@@ -1,22 +1,20 @@
 import pytest
 
 from cishouseholds.merge import union_multiple_tables
-from cishouseholds.pipeline.high_level_transformations import add_pattern_matching_flags
 from cishouseholds.pipeline.high_level_transformations import fill_forwards_transformations
 from cishouseholds.pipeline.pipeline_stages import union_dependent_cleaning
 from cishouseholds.pipeline.pipeline_stages import union_dependent_derivations
 
 
-@pytest.mark.integration
-def test_union_dependent_transformations(
+@pytest.fixture
+def union_dependent_transformations_df(
     responses_v0_survey_ETL_output,
     responses_v1_survey_ETL_output,
     responses_v2_survey_ETL_output,
     responses_digital_ETL_output,
 ):
     """
-    Check that pyspark can build a valid plan for union-dependent processing steps, given outputs from input processing.
-    Should highlight any column name reference errors.
+    Create union dependent transformations output dataframe
     """
     df = union_multiple_tables(
         [
@@ -29,3 +27,26 @@ def test_union_dependent_transformations(
     df = fill_forwards_transformations(df)
     df = union_dependent_cleaning(df)
     df = union_dependent_derivations(df)
+    return df
+
+
+@pytest.mark.regression
+@pytest.mark.integration
+def test_union_dependent_transformations_df(
+    union_dependent_transformations_df,
+    regression_test_df,
+):
+    regression_test_df(
+        union_dependent_transformations_df.drop("survey_response_source_file"),
+        "visit_id",
+        "union_dependent_transformations",
+    )  # remove source file column, as it varies for our temp dummy data
+
+
+@pytest.mark.regression
+@pytest.mark.integration
+def test_union_dependent_transformations_schema(
+    union_dependent_transformations_df,
+    regression_test_df_schema,
+):
+    regression_test_df_schema(union_dependent_transformations_df, "union_dependent_transformations")
