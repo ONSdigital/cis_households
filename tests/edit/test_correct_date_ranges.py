5e09957ec332a1bb32639fc16f8ff2f1b2ac52b7
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-11-14 17:02:49 +0000

    fixed test

--- tests/edit/test_correct_date_ranges.py
+++ tests/edit/test_correct_date_ranges.py
@@ -34,7 +34,7 @@ def test_correct_date_ranges(spark_session):
             ("2021-01-01", "2020-01-03", 2,"D"),  # valid
             ("2021-01-02", "2021-01-01", 3,"E"),  # can be shifted by a year
             ("2021-01-02", "2019-09-01", 4,"F"),  # set to 2019
-            ("2021-02-04", "2020-01-03", 5,"G"),  # valid answer pulled from another row
+            ("2021-02-04", "2021-01-03", 5,"G"),  # valid answer pulled from another row
             ("2021-01-01", "2020-01-03", 5,"H"),  # valid answer pulled from another row
             ("2021-01-02", "2020-03-03", 5,"I"),  # valid answer pulled from another row
             ("2021-03-01", "2020-03-03", 5,"J"),  # valid answer pulled from another row
@@ -55,5 +55,7 @@ def test_correct_date_ranges(spark_session):
     output_df = correct_date_ranges_union_dependent(output_df, ["date1"], "id", "visit_date")
 
     output_df = remove_incorrect_dates(output_df, ["date1"], "visit_date", "2019-08-01")
+
+    output_df = output_df.orderBy("key")
 
     assert_df_equality(output_df, dfs[1], ignore_column_order=True, ignore_row_order=True)
