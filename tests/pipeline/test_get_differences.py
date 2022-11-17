5d208b58caca54bdb04f961cc0cf00fb839a4b94
Stefen Lamothe <78305473+slamothe99@users.noreply.github.com>
2022-11-17 11:08:06 +0000

    2696 - extended union dependent transforms test (#795)
    * converted unionbyname in regex to union to increase efficiency
    
    * none
    
    * added union dependent transofmratins output test
    
    * added compare diffs stage
    
    * reverted long test
    
    * reverted long test
    
    * added compare stage
    
    * updated
    
    * fixed get diffs
    
    * removed diff count selection
    
    Co-authored-by: mat-may <103656428+mat-may@users.noreply.github.com>

---
+++ tests/pipeline/test_get_differences.py
@@ -0,0 +1,46 @@
+import pytest
+from chispa import assert_df_equality
+
+from cishouseholds.pipeline.high_level_transformations import get_differences
+
+
+def test_get_differences(spark_session):
+    input_df_base = spark_session.createDataFrame(
+        data=[
+            (1, "A", 1, True),
+            (2, "B", 2, True),
+            (3, "C", 3, True),
+        ],
+        schema="id integer, string string, int integer, bool boolean",
+    )
+    input_df_compare = spark_session.createDataFrame(
+        data=[
+            (1, "A", 1, True),
+            (2, "X", 99, True),
+            (3, "C", 69420, None),
+        ],
+        schema="id integer, string string, int integer, bool boolean",
+    )
+    counts_df = spark_session.createDataFrame(
+        data=[
+            ("string", 1),
+            ("int", 2),
+            ("bool", 1),
+        ],
+        schema="column_name string, difference_count integer",
+    )
+    diffs_df = spark_session.createDataFrame(
+        data=[
+            (2, "string"),
+            (2, "int"),
+            (3, "int"),
+            (3, "bool"),
+        ],
+        schema="id integer, column_name string",
+    )
+    output_df_counts, output_df_diffs = get_differences(input_df_base, input_df_compare, "id")
+
+    assert_df_equality(
+        counts_df, output_df_counts, ignore_nullable=True, ignore_column_order=True, ignore_row_order=True
+    )
+    assert_df_equality(diffs_df, output_df_diffs, ignore_nullable=True, ignore_column_order=True, ignore_row_order=True)
