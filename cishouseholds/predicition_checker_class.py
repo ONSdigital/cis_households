8fd1103b08bd89d82e29e34e43a7651a9b66a35d
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2022-11-29 20:34:06 +0000

    temo

--- cishouseholds/predicition_checker_class.py
+++ cishouseholds/predicition_checker_class.py
@@ -8,19 +8,15 @@ from pyspark.sql import DataFrame
 from cishouseholds.pipeline.config import get_config
 
 
-class PluginMeta(type):
-    def __new__(cls, name, bases, dct):
-        plugin_file_path = Path(dct["prediction_functions_file_path"])
-        module = imp.load_source(plugin_file_path.stem, plugin_file_path)
+class PredictionChecker:
+    def __new__(cls, *args, **kwargs):
+        plugin_file_path = Path(get_config()["prediction_functions_file_path"])
+        module = imp.load_source(plugin_file_path.stem, plugin_file_path.as_posix())
         for name in dir(module):
             function = getattr(module, name)
             if isinstance(function, FunctionType):
-                dct[function.__name__] = function
-        return type.__new__(cls, name, bases, dct)
-
-
-class PredictionChecker(PluginMeta):
-    prediction_functions_file_path = get_config()["prediction_functions_file_path"]
+                setattr(cls, function.__name__, staticmethod(function))
+        return object.__new__(cls)
 
     def __init__(self, base_df: DataFrame, compare_df: DataFrame, unique_id_column: str) -> None:
         base_df = base_df.select(*[c for c in base_df.columns if c in compare_df.columns])
@@ -43,6 +39,7 @@ class PredictionChecker(PluginMeta):
 
     def check_predictions(self):
         prediction_names = self.get_predictions()
+        column_names = [prediction_name.split("__")[0] for prediction_name in prediction_names]
         predictions = {}
         for prediction_name in prediction_names:
             prediction = getattr(self, prediction_name)
@@ -50,8 +47,14 @@ class PredictionChecker(PluginMeta):
 
         self.df = self.df.select(
             *[
-                (F.sum(F.when(prediction, 1).otherwise(0)) / F.sum(F.lit(1)) * 100).alias(prediction_name)
-                for prediction_name, prediction in predictions.items()
+                (
+                    F.sum(F.when(prediction, 1).otherwise(0))
+                    / F.sum(F.when(F.col(column_name) != F.col(f"{column_name}_ref"), 1).otherwise(0))
+                    * 100
+                ).alias(prediction_name)
+                for column_name, prediction_name, prediction in zip(
+                    column_names, predictions.keys(), predictions.values()
+                )
             ]
         )
         self.df = self.df.select(
