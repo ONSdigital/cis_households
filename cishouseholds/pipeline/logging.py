b2544ed6f87623c3f6ebcea3182dc6db6dd0df99
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-01-10 10:12:53 +0000

    add no survey table condition

--- cishouseholds/pipeline/logging.py
+++ cishouseholds/pipeline/logging.py
@@ -20,7 +20,9 @@ class SurveyTableLengths:
         table_lengths_string = "\n".join(
             f"- {table_name}: {table_length}" for table_name, table_length in cls.table_lengths.items()
         )
-        if len(lengths) > 1:
+        if len(lengths) == 0:
+            print("No survey tables found")  # functional
+        elif len(lengths) > 1:
             table_lengths_error = f"All survey tables post union should be the same length,\ninstead here are their lengths:\n{table_lengths_string}"
             print(table_lengths_error)  # functional
             raise ValueError(table_lengths_error)
