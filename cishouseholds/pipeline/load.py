92e1b6eef28a80eededa77ac50df5d421e39a152
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-01-19 13:31:27 +0000

    no cache table_length implementation

--- cishouseholds/pipeline/load.py
+++ cishouseholds/pipeline/load.py
@@ -90,8 +90,8 @@ def extract_from_table(table_name: str, break_lineage: bool = False, alternate_p
 
 def update_table(df: DataFrame, table_name, write_mode, archive=False):
     if table_name in list(SurveyTableLengths.table_lengths.keys()):
-        df.cache()
-        SurveyTableLengths.log_length(table_name, df.count())
+        length_df = df.select("visit_id").distinct()
+        SurveyTableLengths.log_length(table_name, length_df.count())
     df.write.mode(write_mode).saveAsTable(get_full_table_name(table_name))
     if archive:
         now = datetime.strftime(datetime.now(), "%Y%m%d_%H%M%S")
