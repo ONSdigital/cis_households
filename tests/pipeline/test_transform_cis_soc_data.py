93fad91e0e02283848a07cd1a4086320e7425f39
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-01-26 12:53:11 +0000

    fix regression tests

--- tests/pipeline/test_transform_cis_soc_data.py
+++ tests/pipeline/test_transform_cis_soc_data.py
@@ -1,6 +1,6 @@
 from chispa import assert_df_equality
 
-from cishouseholds.pipeline.pre_union_transformations import transform_cis_soc_data
+from cishouseholds.pipeline.lookup_and_regex_transformations import transform_cis_soc_data
 
 
 def test_transform_cis_soc_data(spark_session):
