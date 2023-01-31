80a177aebad271af60f6440e13a3d90315dad6bc
Stefen Lamothe <stefen.lamothe@ext.ons.gov.uk>
2023-01-31 14:01:08 +0000

    fixed table count issue

--- cishouseholds/pipeline/logging.py
+++ cishouseholds/pipeline/logging.py
@@ -5,7 +5,9 @@ from cishouseholds.pipeline.load import get_run_id
 
 
 def check_survey_table_lengths():
-    tables_df = extract_from_table("table_log").filter(F.col("survey_table") & (F.col("run_id") == get_run_id()))
+    tables_df = extract_from_table("table_log").filter(
+        (F.col("survey_table") is True) & (F.col("run_id") == get_run_id())
+    )
     table_names = tables_df.toPandas()["table_name"].to_list()
     lengths = [extract_from_table(table).count() for table in table_names]
     table_lengths_string = "\n".join(
