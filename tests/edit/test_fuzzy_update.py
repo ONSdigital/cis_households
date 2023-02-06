2820795465fea658e756001c61ff2767dcd384e8
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-02-06 13:51:14 +0000

    temp fuzzy_update test

--- tests/edit/test_fuzzy_update.py
+++ tests/edit/test_fuzzy_update.py
@@ -8,29 +8,29 @@ def test_fuzzy_update(spark_session):
     # fmt: off
     input_df = spark_session.createDataFrame(
         data=[
-            (1, "1", "A",  "L", 1, None),
-            (1, "3", "A",  "L", 2, None),
-            (1, "4", None, "L", 3, None),
-            (1, "4", "A", None, 3, "update"),
-            (1, "3", "B", "L", 2, "update"),
-            (1, "5", "A", "L", 2, "update"),
-            (2, "5", "A", "L", 2, "unchanged"),
-            (3, "5", "A", "L", 2, None)
+            (1, "1", "2022-11-12", "A",  "L",  1, None),
+            (1, "3", "2022-11-12", "A",  "L",  2, None),
+            (1, "4", "2022-11-12", None, "L",  3, None),
+            (1, "4", "2022-11-12", "A",  None, 3, "update"),
+            (1, "3", "2022-11-12", "B",  "L",  2, "update"),
+            (1, "5", "2022-11-12", "A",  "L",  2, "update"),
+            (2, "5", "2022-11-12", "A",  "L",  2, "unchanged"),
+            (3, "5", "2022-11-12", "A",  "L",  2, None)
         ],
-        schema="""id integer, A string, B string, C string, D integer, update string""",
+        schema="""id integer, visit_date string, A string, B string, C string, D integer, update string""",
     )
     expected_df = spark_session.createDataFrame(
         data=[
-            (1, "1", "A",  "L",  1, None), # no rows exist with more than 2 matches and a not null val in update col
-            (1, "3", "A",  "L",  2, "update"),
-            (1, "4", None, "L",  3, None), # no rows exist with more than 2 matches and a not null val in update col
-            (1, "4", "A",  None, 3, "update"),
-            (1, "3", "B",  "L",  2, "update"),
-            (1, "5", "A",  "L",  2, "update"),
-            (2, "5", "A", "L", 2, "unchanged"),
-            (3, "5", "A", "L", 2, None)
+            (1, "1", "2022-11-12", "A",  "L",  1, None), # no rows exist with more than 2 matches and a not null val in update col
+            (1, "3", "2022-11-12", "A",  "L",  2, "update"),
+            (1, "4", "2022-11-12", None, "L",  3, None), # no rows exist with more than 2 matches and a not null val in update col
+            (1, "4", "2022-11-12", "A",  None, 3, "update"),
+            (1, "3", "2022-11-12", "B",  "L",  2, "update"),
+            (1, "5", "2022-11-12", "A",  "L",  2, "update"),
+            (2, "5", "2022-11-12", "A",  "L",  2, "unchanged"),
+            (3, "5", "2022-11-12", "A",  "L",  2, None)
         ],
-        schema="""id integer, A string, B string, C string, D integer, update string""",
+        schema="""id integer, visit_date string, A string, B string, C string, D integer, update string""",
     )
     # fmt: on
 
@@ -40,6 +40,7 @@ def test_fuzzy_update(spark_session):
         min_matches=2,
         update_column="update",
         id_column="id",
-        right_df_filter=F.col("id") < 3,
+        visit_date_column="visit_date",
+        filter_out_of_range=False,
     )
-    assert_df_equality(expected_df, output_df, ignore_row_order=True, ignore_column_order=True)
+    assert_df_equality(expected_df, output_df, ignore_row_order=False, ignore_column_order=False)
