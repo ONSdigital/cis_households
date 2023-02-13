265eb765de09db723dc869f45d46f92e277fe6db
Kamila Martyna <kamila.martyna@ons.gov.uk>
2023-02-13 20:53:03 +0000

    creating a new grouping variable

--- tests/derive/test_assign_grouped_variable_from_days_since.py
+++ tests/derive/test_assign_grouped_variable_from_days_since.py
@@ -6,25 +6,25 @@ from cishouseholds.derive import assign_grouped_variable_from_days_since
 def test_assign_grouped_variable_from_days_since(spark_session):
     expected_df = spark_session.createDataFrame(
         data=[
-            ("Yes", 7, "1"),
-            ("Yes", 20, "2"),
-            ("Yes", 30, "3"),
-            ("Yes", 70, "4"),
-            ("Yes", 100, "5"),
-            ("Yes", None, "6"),
-            ("Don't care", 10, "1"),
-            ("No", None, "6"),
+            ("Yes", 7, "0-14"),
+            ("Yes", 20, "15-28"),
+            ("Yes", 30, "29-60"),
+            ("Yes", 70, "61-90"),
+            ("Yes", 100, "91+"),
+            ("Yes", None, "Date not given"),
+            ("Don't care", 10, "0-14"),
+            ("No", None, None),
         ],
-        schema="contact_known_or_suspected_covid string, \
-                contact_known_or_suspected_covid_days_since integer, \
-                contact_known_or_suspected_covid_days_since_group string",
+        schema="think_had_covid string, \
+                days_since_think_had_covid integer, \
+                days_since_think_had_covid_group string",
     )
-    input_df = expected_df.drop("contact_known_or_suspected_covid_days_since_group")
+    input_df = expected_df.drop("days_since_think_had_covid_group")
 
     output_df = assign_grouped_variable_from_days_since(
         df=input_df,
-        binary_reference_column="contact_known_or_suspected_covid",
-        days_since_reference_column="contact_known_or_suspected_covid_days_since",
-        column_name_to_assign="contact_known_or_suspected_covid_days_since_group",
+        binary_reference_column="think_had_covid",
+        days_since_reference_column="days_since_think_had_covid",
+        column_name_to_assign="days_since_think_had_covid_group",
     )
     assert_df_equality(expected_df, output_df, ignore_column_order=True, ignore_row_order=True)
