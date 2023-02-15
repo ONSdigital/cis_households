ad6df8b5710fdd8c7108e03cad3a35f8fdf67391
Izzy Pickles <izzy.pickles@ons.gov.uk>
2023-02-15 14:50:43 +0000

    removed dont know mapping from dig trans

--- cishouseholds/pipeline/version_specific_processing/digital_transformations.py
+++ cishouseholds/pipeline/version_specific_processing/digital_transformations.py
@@ -60,52 +60,6 @@ def digital_responses_preprocessing(df: DataFrame) -> DataFrame:
         ["20-05-2022T21:30:00", "25-05-2022 11:00:00"],
     )
 
-    dont_know_columns = [
-        "work_in_additional_paid_employment",
-        "work_nursing_or_residential_care_home",
-        "work_direct_contact_patients_or_clients",
-        "self_isolating",
-        "illness_lasting_over_12_months",
-        "ever_smoked_regularly",
-        "currently_smokes_or_vapes",
-        "cis_covid_vaccine_type_1",
-        "cis_covid_vaccine_type_2",
-        "cis_covid_vaccine_type_3",
-        "cis_covid_vaccine_type_4",
-        "cis_covid_vaccine_type_5",
-        "cis_covid_vaccine_type_6",
-        "other_household_member_hospital_last_28_days",
-        "other_household_member_care_home_last_28_days",
-        "hours_a_day_with_someone_else_at_home",
-        "physical_contact_under_18_years",
-        "physical_contact_18_to_69_years",
-        "physical_contact_over_70_years",
-        "social_distance_contact_under_18_years",
-        "social_distance_contact_18_to_69_years",
-        "social_distance_contact_over_70_years",
-        "times_hour_or_longer_another_home_last_7_days",
-        "times_hour_or_longer_another_person_your_home_last_7_days",
-        "times_shopping_last_7_days",
-        "times_socialising_last_7_days",
-        "face_covering_work_or_education",
-        "face_covering_other_enclosed_places",
-        "cis_covid_vaccine_type",
-    ]
-    df = assign_raw_copies(df, dont_know_columns)
-    dont_know_mapping_dict = {
-        "Prefer not to say": None,
-        "Don't know": None,
-        "Don't Know": None,
-        "I don't know the type": "Don't know type",
-        "Dont know": None,
-        "Don&#39;t know": None,
-        "Do not know": None,
-        "Don&amp;#39;t Know": None,
-    }
-    df = apply_value_map_multiple_columns(
-        df,
-        {k: dont_know_mapping_dict for k in dont_know_columns},
-    )
     return df
 
 
