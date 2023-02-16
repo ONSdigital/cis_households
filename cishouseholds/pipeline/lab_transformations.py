be992b7128072590b54dcedd9781991700b83853
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-02-16 15:30:33 +0000

    add runtime fixes

--- cishouseholds/pipeline/lab_transformations.py
+++ cishouseholds/pipeline/lab_transformations.py
@@ -1,6 +1,7 @@
 import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
 
+from cishouseholds.merge import union_multiple_tables
 from cishouseholds.pipeline.phm import match_type_blood
 from cishouseholds.pipeline.phm import match_type_swab
 from cishouseholds.pyspark_utils import get_or_create_spark_session
@@ -15,14 +16,27 @@ def lab_transformations(df: DataFrame, blood_lookup_df: DataFrame, swab_lookup_d
 
 def join_blood_lookup_df(df: DataFrame, blood_lookup_df: DataFrame) -> DataFrame:
     """Joins blood lookup to df and runs transformation logic post join"""
-    blood_lookup_df = blood_lookup_df.withColumn(
-        "blood_sample_barcode_survey_missing_lab", F.col("blood_sample_barcode").isNull()
-    )
-    df = df.withColumn("blood_sample_barcode_lab_missing_survey", F.col("blood_sample_barcode").isNull())
+    # Logical condition for filtering records that are unjoinable
+    record_unjoinable = F.col("blood_sample_barcode").isNull()
+
+    # Set columns required for analysis
+    blood_lookup_df = blood_lookup_df.withColumn("blood_sample_barcode_survey_missing_lab", record_unjoinable)
+    df = df.withColumn("blood_sample_barcode_lab_missing_survey", record_unjoinable)
+
+    # Create unjoinable df to streamline processing time
+    unjoinable_df = df.filter(record_unjoinable)
+
+    # Run logic on df containing only joinable records
+    df = df.filter(~record_unjoinable)
     df = df.join(blood_lookup_df, on="blood_sample_barcode", how="fullouter")
+
+    # Join unjoinable records back to df and repartition
+    df = union_multiple_tables([df, unjoinable_df])
     partitions = int(get_or_create_spark_session().sparkContext.getConf().get("spark.sql.shuffle.partitions"))
     partitions = int(partitions / 2)
     df = df.repartition(partitions)
+
+    # Run required transformations
     df = match_type_blood(df)
     joinable = (
         (
@@ -46,10 +60,26 @@ def join_blood_lookup_df(df: DataFrame, blood_lookup_df: DataFrame) -> DataFrame
 
 def join_swab_lookup_df(df: DataFrame, swab_lookup_df: DataFrame) -> DataFrame:
     """Joins swab lookup to df"""
-    swab_lookup_df = swab_lookup_df.withColumn(
-        "swab_sample_barcode_survey_missing_lab", F.col("swab_sample_barcode").isNull()
-    )
+    # Logical condition for filtering records that are unjoinable
+    record_unjoinable = F.col("swab_sample_barcode").isNull()
+
+    # Set columns required for analysis
+    swab_lookup_df = swab_lookup_df.withColumn("swab_sample_barcode_survey_missing_lab", record_unjoinable)
+    df = df.withColumn("swab_sample_barcode_lab_missing_survey", record_unjoinable)
+
+    # Create unjoinable df to streamline processing time
+    unjoinable_df = df.filter(record_unjoinable)
+
+    # Run logic on df containing only joinable records
+    df = df.filter(~record_unjoinable)
     df = df.join(swab_lookup_df, on="swab_sample_barcode", how="fullouter")
-    df = df.withColumn("swab_sample_barcode_lab_missing_survey", F.col("swab_sample_barcode").isNull())
+
+    # Join unjoinable records back to df and repartition
+    df = union_multiple_tables([df, unjoinable_df])
+    partitions = int(get_or_create_spark_session().sparkContext.getConf().get("spark.sql.shuffle.partitions"))
+    partitions = int(partitions / 2)
+    df = df.repartition(partitions)
+
+    # Run required transformations
     df = match_type_swab(df)
     return df
