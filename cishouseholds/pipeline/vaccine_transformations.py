19819e8ed712f06bee38558547e952754dee6e26
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-02-23 13:32:24 +0000

    add assign_order_number function
    unit test currently fails - logic needs review

--- cishouseholds/pipeline/vaccine_transformations.py
+++ cishouseholds/pipeline/vaccine_transformations.py
@@ -4,6 +4,7 @@
 from pyspark.sql import DataFrame
 
 from cishouseholds.derive import assign_max_doses
+from cishouseholds.derive import assign_order_number
 from cishouseholds.derive import assign_pos_1_2
 from cishouseholds.derive import group_participant_within_date_range
 from cishouseholds.edit import update_column_values_from_map
@@ -56,5 +57,11 @@ def preprocesing(df: DataFrame):
         participant_id_column="participant_id",
         num_doses_column="cis_covid_vaccine_number_of_doses",
     )
-
+    df = assign_order_number(
+        df=df,
+        column_name_to_assign="order_number",
+        covid_vaccine_type_column="cis_covid_vaccine_type",
+        max_doses_column="max_doses",
+        pos_1_2_column="pos_1_2",
+    )
     return df
