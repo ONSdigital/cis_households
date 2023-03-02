bd72688aede74bebcfa23d7d00294df205c5c220
Izzy Pickles <izzy.pickles@ons.gov.uk>
2023-03-02 17:22:45 +0000

    updating testing

--- tests/derive/test_assign_columns_from_array.py
+++ tests/derive/test_assign_columns_from_array.py
@@ -8,15 +8,17 @@ def test_assign_columns_from_array(spark_session):
         data=[
             (1, ["a", "b", "c"]),
             (2, ["a", "c"]),
+            (3, ["a b c", "a b", "four"]),
         ],
         schema=["id", "arr"],
     )
     expected_df = spark_session.createDataFrame(
         data=[
-            (1, True, True, True),
-            (2, True, False, True),
+            (1, True, True, True, False, False, False),
+            (2, True, False, True, False, False, False),
+            (3, False, False, False, True, True, True),
         ],
-        schema=["id", "test_a", "test_b", "test_c"],
+        schema=["id", "test_a", "test_b", "test_c", "test_a_b_c", "test_a_b", "test_four"],
     )
     output_df = assign_columns_from_array(
         df=input_df, id_column_name="id", array_column_name="arr", prefix="test", true_false_values=[True, False]
