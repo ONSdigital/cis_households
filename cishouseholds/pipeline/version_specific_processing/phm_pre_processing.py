664e87e1774cc4dcba38133b2c60848be6071da2
Kamila Martyna <kamila.martyna@ons.gov.uk>
2023-03-07 17:23:14 +0000

    added phm_pre_processing.py file

---
+++ cishouseholds/pipeline/version_specific_processing/phm_pre_processing.py
@@ -0,0 +1,5 @@
+phm_pre_mapping = {
+    "work_status_employment": {
+        "Currently working - this includes if you are on sick leave or other leave for less than 4 weeks": "Currently working. This includes if you are on sick or other leave for less than 4 weeks"
+    },
+}
