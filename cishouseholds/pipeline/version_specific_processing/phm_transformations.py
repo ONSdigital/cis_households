15cc71203750fbc0ed399b6d97bca30ab9bd7a7f
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-03-21 15:30:17 +0000

    update phm_transformations
    reran regressions

--- cishouseholds/pipeline/version_specific_processing/phm_transformations.py
+++ cishouseholds/pipeline/version_specific_processing/phm_transformations.py
@@ -8,6 +8,7 @@ from pyspark.sql import DataFrame
 from cishouseholds.derive import assign_column_uniform_value
 from cishouseholds.derive import assign_column_value_from_multiple_column_map
 from cishouseholds.derive import assign_columns_from_array
+from cishouseholds.derive import assign_completion_status
 from cishouseholds.derive import assign_date_from_filename
 from cishouseholds.derive import assign_datetime_from_coalesced_columns_and_log_source
 from cishouseholds.derive import assign_datetime_from_combined_columns
@@ -201,7 +202,7 @@ def pre_processing(df: DataFrame) -> DataFrame:
 
     df = assign_column_uniform_value(df, "survey_response_dataset_major_version", 4)
     # df = generic_processing(df)
-
+    df = df.withColumn("file_date", F.col("survey_completed_datetime"))
     # df = assign_completion_status(df=df, column_name_to_assign="survey_completion_status")
     df = add_prefix(df, column_name_to_update="blood_sample_barcode_user_entered", prefix="BLT")
     df = add_prefix(df, column_name_to_update="swab_sample_barcode_user_entered", prefix="SWT")
@@ -256,34 +257,34 @@ def derive_additional_columns(df: DataFrame) -> DataFrame:
     - phm_covid_vaccine_number_of_doses
     """
     df = assign_any_symptoms(df)
-    df = split_array_columns(df)
-    # map_to_bool_columns_dict = {
-    #     "currently_smokes_or_vapes_description": "",
-    #     "blood_not_taken_could_not_reason": "",
-    #     "transport_shared_outside_household_last_28_days": "",
-    #     "phm_think_had_respiratory_infection_type": "",
-    #     "think_have_covid_any_symptom_list_1": "think_have_covid",
-    #     "think_have_covid_any_symptom_list_2": "think_have_covid",
-    #     "think_have_symptoms_new_or_worse_list_1": "think_have_symptoms",
-    #     "think_have_symptoms_new_or_worse_list_2": "think_have_symptoms",
-    #     "think_have_long_covid_symptom_list_1": "think_have_long_covid",
-    #     "think_have_long_covid_symptom_list_2": "think_have_long_covid",
-    #     "think_have_long_covid_symptom_list_3": "think_have_long_covid",
-    #     "think_had_covid_any_symptom_list_1": "think_had_covid",
-    #     "think_had_covid_any_symptom_list_2": "think_had_covid",
-    #     "think_had_other_infection_symptom_list_1": "think_had_other",
-    #     "think_had_other_infection_symptom_list_2": "think_had_other",
-    #     "think_had_flu_symptom_list_1": "think_had_flu",
-    #     "think_had_flu_symptom_list_2": "think_had_flu",
-    # }
-    # for col_to_map, prefix in map_to_bool_columns_dict.items():
-    #     if ("symptom" in col_to_map) & ("list_" in col_to_map):
-    #         value_column_map = {
-    #             key: prefix + value for key, value in transformation_maps[f"symptoms_list_{col_to_map[-1:]}"].items()
-    #         }
-    #     else:
-    #         value_column_map = transformation_maps[col_to_map]
-    #     df = map_options_to_bool_columns(df, col_to_map, value_column_map, ";")
+    # df = split_array_columns(df)
+    map_to_bool_columns_dict = {
+        "currently_smokes_or_vapes_description": "",
+        "blood_not_taken_could_not_reason": "",
+        "transport_shared_outside_household_last_28_days": "",
+        "phm_think_had_respiratory_infection_type": "",
+        "think_have_covid_any_symptom_list_1": "think_have_covid",
+        "think_have_covid_any_symptom_list_2": "think_have_covid",
+        "think_have_symptoms_new_or_worse_list_1": "think_have_symptoms",
+        "think_have_symptoms_new_or_worse_list_2": "think_have_symptoms",
+        "think_have_long_covid_symptom_list_1": "think_have_long_covid",
+        "think_have_long_covid_symptom_list_2": "think_have_long_covid",
+        "think_have_long_covid_symptom_list_3": "think_have_long_covid",
+        "think_had_covid_any_symptom_list_1": "think_had_covid",
+        "think_had_covid_any_symptom_list_2": "think_had_covid",
+        "think_had_other_infection_symptom_list_1": "think_had_other",
+        "think_had_other_infection_symptom_list_2": "think_had_other",
+        "think_had_flu_symptom_list_1": "think_had_flu",
+        "think_had_flu_symptom_list_2": "think_had_flu",
+    }
+    for col_to_map, prefix in map_to_bool_columns_dict.items():
+        if ("symptom" in col_to_map) & ("list_" in col_to_map):
+            value_column_map = {
+                key: prefix + value for key, value in transformation_maps[f"symptoms_list_{col_to_map[-1:]}"].items()
+            }
+        else:
+            value_column_map = transformation_maps[col_to_map]
+        df = map_options_to_bool_columns(df, col_to_map, value_column_map, ";")
 
     column_list = ["work_status_digital", "work_status_employment", "work_status_unemployment", "work_status_education"]
     df = assign_column_value_from_multiple_column_map(
