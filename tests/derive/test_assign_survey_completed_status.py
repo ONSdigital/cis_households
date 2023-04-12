5b6e8fea9a71a2b5b89715756d6932ab16fe3d59
mat-may <103656428+mat-may@users.noreply.github.com>
2023-04-12 11:47:25 +0100

    CISDP-3197 - update survey_completed_datetime implementation (#1039)
    * update survey_completed_datetime regression
    
    * update unit test
    
    * update survey_start_datetime mapping

--- tests/derive/test_assign_survey_completed_status.py
+++ tests/derive/test_assign_survey_completed_status.py
@@ -8,18 +8,15 @@ from cishouseholds.derive import assign_survey_completed_status
 @pytest.fixture
 def expected_df(spark_session):
     return spark_session.createDataFrame(
+        # fmt: off
         data=[
-            (1, "TRUE", None, "Partially Completed"),  # Flushed
-            (2, "FALSE", None, "Not Completed"),  # Not completed not flushed
-            (
-                3,
-                "TRUE",
-                "2023-03-01 11:30:21",
-                "Partially Completed",
-            ),  # Fushed but has datetime - not expected scenario
+            (1, "true", None, "Partially Completed"),  # Flushed
+            (2, "false", None, "Not Completed"),  # Not completed not flushed
+            (3, "true", "2023-03-01 11:30:21", "Partially Completed",),  # Fushed but has datetime - not expected scenario
             (4, None, None, None),  # no information
-            (5, "FALSE", "2023-04-01 12:30:21", "Completed"),  # Not flushed, datetime present
+            (5, "false", "2023-04-01 12:30:21", "Completed"),  # Not flushed, datetime present
         ],
+        # fmt: on
         schema="id int, flushed string, survey_completed_dt string, status string",
     )
 
