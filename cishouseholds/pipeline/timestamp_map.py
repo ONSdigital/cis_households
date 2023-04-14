02943e6cdb60c87cc15c559d5708cf3753872e2c
izpickle <103201130+izpickle@users.noreply.github.com>
2023-04-14 14:25:46 +0100

    CISDP-3212 adding infection type 'unknown' to logic for deriving think_had_other_infection_symptom_any (#1045)
    * updating infection type string
    
    * adding think_had_unknown to any symptom logic
    
    * adding vars to datetime map
    
    * update any symptoms
    
    * reran regressions
    
    ---------
    
    Co-authored-by: Matthew Maynard <matthew.maynard@ons.gov.uk>
    Co-authored-by: mat-may <103656428+mat-may@users.noreply.github.com>

--- cishouseholds/pipeline/timestamp_map.py
+++ cishouseholds/pipeline/timestamp_map.py
@@ -162,6 +162,7 @@ phm_datetime_map = {
         "survey_start_datetime",
         "participant_completion_window_start_date",
         "participant_completion_window_end_date",
+        "date_of_birth",
     ],
     "yyyy-MM-dd'T'HH:mm:ss'+00:00'": ["survey_completed_datetime"],
     # "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'+00:00'": ["survey_start_datetime"],
