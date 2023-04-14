f141dffadce1ade791dfa2295deb6c7f14ea7ff9
Luis Dibdin <luis.dibdin@ons.gov.uk>
2023-04-14 10:25:44 +0100

    add schema_name to dummy data

--- dummy_data_generation/schemas.py
+++ dummy_data_generation/schemas.py
@@ -3967,4 +3967,5 @@ def get_phm_survey_responses_data_description(_, blood_barcodes, swab_barcodes):
             ],
             weights=[0.9, 0.1],
         ),
+        "schema_name": "cris_0001",
     }
