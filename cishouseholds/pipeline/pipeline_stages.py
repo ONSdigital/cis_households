c5f4b64b44500676b89b4059ee53c38b7608605e
Izzy Pickles <izzy.pickles@ons.gov.uk>
2023-04-20 15:55:07 +0100

    CISDP-3207 filling forward work variables

--- cishouseholds/pipeline/pipeline_stages.py
+++ cishouseholds/pipeline/pipeline_stages.py
@@ -579,7 +579,6 @@ def union_historical_visit_transformations(tables_to_process: List, output_surve
     df = union_multiple_tables(df_list)
     for transformation in transformations:
         df = transformations_dict[transformation](df)
-    # df = df.filter(F.col("filter_survey_response_dataset_major_version")) > 3
     update_table(df, output_survey_table, "overwrite", survey_table=True)
     return {"output_survey_table": output_survey_table}
 
@@ -688,13 +687,15 @@ def execute_job_transformations(
 ):
     """
     Runs job transformations on the input survey table and produces output table
+    Then drops historical survey responses
     """
     df = extract_from_table(input_survey_table)
     # soc_lookup_df = extract_from_table(soc_lookup_table)
     # job_lookup_df = None
     # if check_table_exists(job_lookup_table):
     #     job_lookup_df = extract_from_table(job_lookup_table, break_lineage=True)
-    df, job_lookup_df = job_transformations(df=df)
+    df = job_transformations(df=df)
+    df = df.filter(F.col("survey_response_dataset_major_version") > 3)
     update_table(df, output_survey_table, "overwrite", survey_table=True)
     # update_table(job_lookup_df, job_lookup_table, "overwrite")
     return {"output_survey_table": output_survey_table}
