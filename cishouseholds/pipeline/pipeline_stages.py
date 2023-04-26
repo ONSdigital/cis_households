092d7074de2429b745fd2bd7c609c9a527c53245
Izzy Pickles <izzy.pickles@ons.gov.uk>
2023-04-26 09:26:34 +0100

    removing dataset filter on job transformations

--- cishouseholds/pipeline/pipeline_stages.py
+++ cishouseholds/pipeline/pipeline_stages.py
@@ -703,7 +703,7 @@ def execute_job_transformations(
     # if check_table_exists(job_lookup_table):
     #     job_lookup_df = extract_from_table(job_lookup_table, break_lineage=True)
     df = job_transformations(df=df)
-    df = df.filter(F.col("survey_response_dataset_major_version") > 3)
+    # df = df.filter(F.col("survey_response_dataset_major_version") > 3)
     update_table(df, output_survey_table, "overwrite", survey_table=True)
     # update_table(job_lookup_df, job_lookup_table, "overwrite")
     return {"output_survey_table": output_survey_table}
