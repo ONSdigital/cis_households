b72dbc2f8bf4fee8e0b7bb513c1b0a3765f2169d
Matthew Maynard <matthew.maynard@ons.gov.uk>
2023-05-04 11:46:20 +0100

    add assign_completion_status function call

--- cishouseholds/pipeline/version_specific_processing/phm_transformations.py
+++ cishouseholds/pipeline/version_specific_processing/phm_transformations.py
@@ -7,6 +7,7 @@ from pyspark.sql import DataFrame
 
 from cishouseholds.derive import assign_column_uniform_value
 from cishouseholds.derive import assign_column_value_from_multiple_column_map
+from cishouseholds.derive import assign_completion_status
 from cishouseholds.derive import assign_date_from_filename
 from cishouseholds.derive import assign_datetime_from_combined_columns
 from cishouseholds.derive import assign_raw_copies
@@ -210,6 +211,7 @@ def pre_processing(df: DataFrame) -> DataFrame:
     )
 
     df = assign_column_uniform_value(df, "survey_response_dataset_major_version", 4)
+    df = assign_completion_status(df, column_name_to_assign="survey_completion_status")
     df = add_prefix(df, column_name_to_update="blood_sample_barcode_user_entered", prefix="BLT")
     df = add_prefix(df, column_name_to_update="swab_sample_barcode_user_entered", prefix="SWT")
     return df
